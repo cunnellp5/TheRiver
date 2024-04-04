fdda71e7d94ce6156004827066fa1c65d9b08cde
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-04 12:04:48 -0600

    data

---
+++ src/lib/data/marketing.ts
@@ -0,0 +1,64 @@
+export const introduction = {
+	content:
+		'The River is a rising Pop/R&B/Hip-Hop artist based in Denver, Colorado. Known for mesmerizing vocal styling & sultry vibes, her work highlights a unique perspective and passionate messaging. A lifelong poet, the pieces soon grew from poetry into raps and songs. Her music is known for its expressive lyricism matched with wicked beats. The River works with producers Sammy Seeds, Alex Newton, and Scot Stewart & looks forward to touring with her pieces and sharing her art with the world',
+	socials: [
+		{
+			platform: 'TikTok',
+			handle: '@theriversings'
+		},
+		{
+			platform: 'YouTube',
+			handle: '@theriver'
+		},
+		{
+			platform: 'Instagram',
+			handle: '@the.river.sings'
+		}
+	]
+};
+
+export const blogReferences = [
+	{
+		content: {
+			message: 'Ever since releasing her first song in June 2021, she has had her foot on the gas.',
+			author: 'Victória Santiago'
+		},
+		link: {
+			url: 'https://ratingsgamemusic.com/2022/01/08/the-river-delivers-angry-verses-on-hours/',
+			title: 'Victória Santiago [Ratings Game Music Blogger]'
+		}
+	},
+	{
+		content: {
+			message:
+				"On Hours: The production here is sublime and crisp as each instrument stands out with clarity while The River's commanding vocals cut through the mix with ease.",
+			author: 'TAYO ODUTOLA'
+		},
+		link: {
+			url: 'https://earmilk.com/2022/01/10/the-rivers-hours-digs-deep-into-the-dynamics-of-failed-relationships/',
+			title: 'Earmilk'
+		}
+	},
+	{
+		content: {
+			message:
+				"The vocals are confidently delivered, imbued with the effortless coolness that comes with being insanely naturally gifted. Plus, the arrangement and production create the most infectious groove you'll ever hear.",
+			author: 'Alyce Lindberg'
+		},
+		link: {
+			url: 'https://musicfashionblog.com/it-aint-that-deep-the-river/',
+			title: 'Read more'
+		}
+	},
+	{
+		content: {
+			message:
+				"Somewhere in the lyrics there is peace and truth. The River's latest single “Hours” facing the reality of love, respect, and emotions. Over and acoustic melody the rain pours over the lyrical expressions of facing your feelings.",
+			author: 'Kreadiv Kesh'
+		},
+		link: {
+			url: 'EXPIRED_URL', // https://www.laonlock.com/reviews/1/11/2022/the-river-hours-review
+			title: 'Kreadiv Kesh [LA On Lock Blogger]'
+		}
+	}
+];
