e926b6e2632b2a1da960b84af9b0b153a86d4d37
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-04 12:05:19 -0600

    style contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,14 +1,39 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		document.title = 'Contact | The River Sings';
+	});
+</script>
+
 <main>
 	<h1>Contact</h1>
 	<div></div>
 	<p>theriverrunsfast@gmail.com</p>
 </main>
 
+<section></section>
+
 <style>
 	main {
 		display: flex;
 		flex-direction: row;
 		align-items: center;
+		justify-content: center;
+	}
+
+	section {
+		left: 50%; /* Center the section horizontally */
+		top: 200px;
+		height: var(--size-15);
+		width: var(--size-15);
+		position: fixed;
+		z-index: -1;
+		background-image: var(--gradient-22), var(--noise-3);
+		background-size: 100%;
+		border-radius: var(--radius-blob-1);
+		transform: translateX(-50%);
+		opacity: 0.4;
 	}
 
 	h1 {
@@ -20,6 +45,7 @@
 		border-left: 1.8px solid rgb(207, 207, 207);
 		height: var(--size-7);
 	}
+
 	p {
 		font-weight: 800;
 		font-size: var(--size-4);
