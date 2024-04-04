054db07d382ad11292a07789298359da95275f37
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-03 18:59:48 -0600

    main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,55 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<title>The River</title>
+
+<div id="video-background">
+	<video autoplay loop muted>
+		<source src="/jungle-river.mp4" type="video/mp4" />
+		<track kind="captions" srclang="en" label="English" />
+	</video>
+	<div id="video-tint"></div>
+</div>
+
+<div id="image-container">
+	<img src="/Logo_White.png" alt="The River Logo" srcset="" />
+</div>
+
+<style>
+	#video-background {
+		position: fixed;
+		right: 0;
+		bottom: 0;
+		min-width: 100%;
+		min-height: 100%;
+		width: auto;
+		height: auto;
+		z-index: -100;
+	}
+
+	#video-tint {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background: rgba(0, 0, 0, 0.5); /* Black tint with 50% opacity */
+	}
+
+	#image-container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh; /* Full height of the viewport */
+	}
+
+	img {
+		width: 70%;
+	}
+
+	video {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
+</style>
