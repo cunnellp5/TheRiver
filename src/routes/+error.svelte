bebb127b5fb875f0d6b0f72b034d0ca3a9e85f06
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-04 12:11:28 -0600

    error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,28 @@
+<main>
+	<h1>500</h1>
+	<div></div>
+	<p>Page not found</p>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+	}
+
+	div {
+		margin-inline: 1rem;
+		border-left: 1px solid var(--text-1);
+		height: var(--size-7);
+	}
+
+	p {
+		letter-spacing: var(--size-1);
+		font-weight: 800;
+		color: var(--text-2);
+		font-size: var(--size-3);
+		text-transform: uppercase;
+	}
+</style>
