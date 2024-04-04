8e8a8de04a6fbe222daafee67fa5a3e7735322ad
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-04 09:33:04 -0600

    add footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import Circle from 'lucide-svelte/icons/circle';
+</script>
+
+<footer>
+	<p>🌊 TheRiverSings &copy {new Date().getFullYear()}</p>
+	<p>socials</p>
+
+	<ul>
+		<li class="light">
+			<Circle />
+		</li>
+		<li class="dark">
+			<Circle fill="#FFFFFF" />
+		</li>
+	</ul>
+</footer>
+
+<style>
+	footer {
+		display: grid;
+		grid-template-columns: auto auto auto;
+		justify-content: space-between;
+		padding-block: var(--size-7);
+		border-top: 1px solid var(--border);
+	}
+
+	p {
+		color: var(--text-2);
+	}
+
+	.dark {
+		display: none;
+	}
+</style>
