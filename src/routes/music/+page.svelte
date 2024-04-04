618b62d4e3ea73509822bd5c681ef776df6677fa
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-03 18:59:56 -0600

    quick routes

---
+++ src/routes/music/+page.svelte
@@ -0,0 +1,6 @@
+<h1>music page</h1>
+<div>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ipsa odit expedita ducimus quod sit
+	fugiat ab ad dolores temporibus ex quam, numquam quia quibusdam excepturi. Aliquam veritatis ut
+	aliquid.
+</div>
