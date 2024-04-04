891b37b4d296202b31c4fc36bd3c78027fe2db35
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-04 09:32:46 -0600

    basic contact page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,27 @@
+<main>
+	<h1>Contact</h1>
+	<div></div>
+	<p>theriverrunsfast@gmail.com</p>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+	}
+
+	h1 {
+		text-transform: uppercase;
+	}
+
+	div {
+		margin-inline: 1rem;
+		border-left: 1.8px solid rgb(207, 207, 207);
+		height: var(--size-7);
+	}
+	p {
+		font-weight: 800;
+		font-size: var(--size-4);
+	}
+</style>
