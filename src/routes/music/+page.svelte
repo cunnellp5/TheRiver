36111bd29f1552fc313d054739faabd3207f2a14
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-04 12:05:35 -0600

    general updates

--- src/routes/music/+page.svelte
+++ src/routes/music/+page.svelte
@@ -1,6 +1,17 @@
+<script lang="ts">
+</script>
+
 <h1>music page</h1>
 <div>
 	Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ipsa odit expedita ducimus quod sit
 	fugiat ab ad dolores temporibus ex quam, numquam quia quibusdam excepturi. Aliquam veritatis ut
 	aliquid.
 </div>
+
+<style>
+	div {
+		background: white;
+		-webkit-mask: var(--mask-edge-drip-bottom);
+		mask: var(--mask-edge-drip-bottom);
+	}
+</style>
--- src/routes/producers/+page.svelte
+++ src/routes/producers/+page.svelte
@@ -1,1 +1,64 @@
-<h1>producers page</h1>
+<main>
+	<h1>Producers</h1>
+
+	<p>Click one of the links then downalod from google drive.</p>
+	<p>I would love to hear what you create!</p>
+
+	<ul>
+		<li>
+			<h4>
+				<a
+					href="https://drive.google.com/drive/folders/1zMLDz-1iEerzgr4eu_bZ30GzVUBgr3cr"
+					target="_blank"
+				>
+					#1 Tides
+				</a>
+			</h4>
+		</li>
+		<li>
+			<h4>
+				<a
+					href="https://drive.google.com/drive/folders/1WfE2Cz68NtzI2qMJoDcnNsmgepSRzbUZ"
+					target="_blank"
+				>
+					#2 Security & Passion
+				</a>
+			</h4>
+		</li>
+	</ul>
+
+	<p>Email me the audio track or tag me on instagram, TikTok, or youtube.</p>
+
+	<div>
+		<p>Tag me:</p>
+		<ul>
+			<li>instagram @the.river.sings</li>
+			<li>TikTok @theriversings</li>
+			<li>youtube @theriversings</li>
+		</ul>
+	</div>
+	theriverrunsfast@gmail.com
+</main>
+
+<style>
+	h1,
+	ul {
+		padding-block: var(--size-7);
+	}
+
+	p {
+		line-height: var(--size-7);
+	}
+
+	a {
+		color: var(--text-1);
+		text-decoration: none;
+	}
+
+	a:hover {
+		background: var(--gradient-3) fixed; /* Change to a different gradient on hover */
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
+		background-clip: text;
+	}
+</style>
