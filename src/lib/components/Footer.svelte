a7d6232c124613641e2a6ca38baed8afbded4513
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-04 16:10:31 -0600

    update footer with ideas

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -5,30 +5,50 @@
 
 <footer>
 	<div>
-		<p>The River Sings</p>
-		<div class="line-spacer"></div>
+		<!-- <a href="/">
+			<img src="/Logo_White.png" alt="The River Logo White" />
+		</a> -->
+		<!-- <div class="line-spacer"></div> -->
 		<small> &copy {new Date().getFullYear()}</small>
 	</div>
 
-	<Socials />
+	<div class="socials">
+		<a href="/">
+			<img src="/Logo_White.png" alt="The River Logo White" />
+		</a>
+		<Socials />
+	</div>
 
-	<ToggleTheme />
+	<div class="toggler">
+		<ToggleTheme />
+	</div>
 </footer>
 
 <style>
 	footer {
 		display: grid;
-		grid-template-columns: auto auto auto;
+		grid-template-columns: 1fr 1fr 1fr;
 		justify-content: space-between;
 		padding-block: var(--size-7);
 		border-top: 1px solid var(--border);
 	}
 
+	.toggler {
+		justify-content: end;
+	}
+
+	.socials {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		gap: var(--size-7);
+	}
+
 	p {
 		display: flex;
-		gap: var(--size-2);
-		color: var(--text-2);
-		font-family: var(--font-serif);
+		font-family: var(--font-mono);
+		font-variant: small-caps;
+		letter-spacing: var(--size-2);
 	}
 
 	div {
@@ -36,9 +56,14 @@
 		align-items: center;
 	}
 
-	.line-spacer {
+	/* .line-spacer {
 		margin-inline: var(--size-2);
 		border-left: 0.5px solid rgb(207, 207, 207);
 		height: var(--size-3);
+	} */
+
+	img {
+		width: var(--size-10);
+		border-radius: 0;
 	}
 </style>
