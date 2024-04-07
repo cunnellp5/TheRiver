8e7828b31f00722f63c9129465be92ed7788829e
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-07 12:01:33 -0600

    begin adding global typings

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,6 +2,10 @@
 // for information about these interfaces
 declare global {
 	namespace App {
+		interface EventHandlers {
+			preventDefault: () => void;
+			stopPropagation: () => void;
+		}
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
@@ -10,4 +14,4 @@ declare global {
 	}
 }
 
-export {};
+export { EventHandlers };
