648611de36d4246cb36ea24c3bee1a03ed266db1
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-07 16:52:56 -0600

    add error handling if cloudinary url breaks

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,13 @@
+import { error } from '@sveltejs/kit';
 import { env } from '$env/dynamic/private';
+import type { PageServerLoad } from './$types';
 
-export const load = () => {
-	return {
-		videoURL: env.VIDEO_URL
-	};
+export const load: PageServerLoad = async () => {
+	const videoURL = env.VIDEO_URL;
+
+	if (!videoURL) {
+		throw error(404, 'Video not found');
+	}
+
+	return { videoURL };
 };
