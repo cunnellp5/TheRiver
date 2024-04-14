2440eecbb90e8c3ba8ac47571136f5c01c719934
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-14 11:49:21 -0600

    update socials layout

--- src/lib/components/Socials.svelte
+++ src/lib/components/Socials.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
-	import Spotify from '$lib/components/svgs/socials/Spotify.svelte';
 	import Etsy from '$lib/components/svgs/socials/Etsy.svelte';
-	import LinkTree from '$lib/components/svgs/socials/LinkTree.svelte';
 	import Facebook from '$lib/components/svgs/socials/Facebook.svelte';
 	import Instagram from '$lib/components/svgs/socials/Instagram.svelte';
+	import LinkTree from '$lib/components/svgs/socials/LinkTree.svelte';
+	import Spotify from '$lib/components/svgs/socials/Spotify.svelte';
 	import Youtube from '$lib/components/svgs/socials/Youtube.svelte';
+	import LogoR from '$lib/components/svgs/logos/LogoR2.svelte';
 </script>
 
 <ul>
 	<li><Facebook /></li>
 	<li><Youtube /></li>
 	<li><Instagram /></li>
+	<li class="logo-link">
+		<a href="/">
+			<LogoR />
+		</a>
+	</li>
 	<li><LinkTree /></li>
 	<li><Spotify /></li>
 	<li><Etsy /></li>
@@ -25,10 +31,24 @@
 	}
 
 	li {
+		display: flex;
 		margin-inline: var(--size-1);
+		border: 1px solid var(--gray-9);
+		justify-content: center;
+		align-items: center;
+		border-radius: var(--radius-round);
+		width: var(--size-8);
 	}
 
 	li:hover {
 		cursor: pointer;
+	}
+
+	.logo-link {
+		border: none;
+		width: var(--size-8);
+		& path {
+			fill: var(--indigo-4);
+		}
 	}
 </style>
