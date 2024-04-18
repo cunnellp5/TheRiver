28517bcefa1a25a93da719de6febb0df328a3880
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-18 13:28:36 -0600

    add delete and get one route with error handling

--- src/routes/api/posts/[slug]/+server.ts
+++ src/routes/api/posts/[slug]/+server.ts
@@ -1,12 +1,34 @@
 import db from '$lib/server/database';
-import { json } from '@sveltejs/kit';
+import { json, error } from '@sveltejs/kit';
 
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async ({ params }) => {
-	const post = await db.post.findUnique({
-		where: { slug: params.slug }
-	});
+	let post;
+
+	try {
+		post = await db.post.findUnique({
+			where: { slug: params.slug }
+		});
+	} catch (err: unknown | Error) {
+		error(500, (err as Error).message);
+	}
+
+	if (!post) {
+		error(404, 'Post not found');
+	}
 
 	return json(post);
+};
+
+export const DELETE: RequestHandler = async ({ params }): Promise<Response> => {
+	try {
+		await db.post.delete({
+			where: { slug: params.slug }
+		});
+	} catch (err: unknown | Error) {
+		error(500, (err as Error).message);
+	}
+
+	return json({ status: 204 });
 };
