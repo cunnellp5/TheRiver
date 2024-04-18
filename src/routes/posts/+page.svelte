016f638d4dc0e7c9877ea79ca3d2aa4bf520c23e
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-18 13:29:26 -0600

    update posts page ui

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -1,52 +1,108 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-	import SquareArrowOurUpRight from 'lucide-svelte/icons/square-arrow-out-up-right';
+	import { enhance } from '$app/forms';
 	import formatDate from '$lib/utils/formatDate';
+	import Pencil from 'lucide-svelte/icons/pencil';
+	import Check from 'lucide-svelte/icons/check';
+	import Plus from 'lucide-svelte/icons/plus';
+	import SquareArrowOurUpRight from 'lucide-svelte/icons/square-arrow-out-up-right';
+	import Trash from 'lucide-svelte/icons/trash';
+	import type { PageData, SubmitFunction } from './$types';
 
+	export let form;
 	export let data: PageData;
+	let deleteFrom: HTMLFormElement;
+	let hideSuccessMessage = false;
+
+	function rmNote() {
+		hideSuccessMessage = true;
+	}
 
 	$: ({ posts, isAdmin } = data);
+	$: console.log(form);
 </script>
 
 <main>
 	<section>
-		<header>
-			<h1>The River Blog</h1>
-
-			<p>Showing {posts.length} posts.</p>
-		</header>
-
-		<ul>
-			{#each posts as { slug, title, tags, createdAt }}
-				<li class="card">
-					<div>
-						<h5>{title}</h5>
-						<date>{formatDate(new Date(createdAt))}</date>
-					</div>
-					<p class="description">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-						molestias, voluptatum, animi quia suscipit fugiat, consectetur earum
-						ex officia laboriosam et inventore tempora sapiente! Eum accusantium
-						deserunt iure quis sapiente?
-					</p>
-					<div class="tags">
-						{#each tags as tag}
-							<span class="badge"># {tag}</span>
-						{/each}
-					</div>
-					<a class="blogLink" href="/posts/{slug}">
-						read more
-						<SquareArrowOurUpRight size="10" />
-					</a>
+		{#if posts.length > 0}
+			<header>
+				<div class="titleWrapper">
+					<h1>The River Blog</h1>
 					{#if isAdmin}
-						<div class="actionsGroup">
-							<button type="button">Edit</button>
-							<button type="button">Delete</button>
-						</div>
+						<button class="create">
+							<a href="/posts/new"><Plus /></a>
+						</button>
 					{/if}
-				</li>
-			{/each}
-		</ul>
+				</div>
+
+				<p>Showing {posts.length} posts.</p>
+
+				{#if form?.success}
+					<div class="card" class:hidden={hideSuccessMessage}>
+						<div class="titleWrapper-sub">
+							<h5>{form.deletedTitle}</h5>
+							<button class="delete block" on:click={rmNote}><Trash /></button>
+						</div>
+						<p class="success-form-message"><Check />{form.message}</p>
+					</div>
+				{/if}
+			</header>
+			<ul>
+				{#each posts as { slug, title, tags, createdAt, content }}
+					<li class="cardWrapper">
+						<div class="card">
+							<div>
+								<h5>
+									<a href="/posts/{slug}">
+										{title}
+									</a>
+								</h5>
+								<date>{formatDate(new Date(createdAt))}</date>
+							</div>
+							<p class="description">
+								{content?.substring(0, 100) + ' ...'}
+							</p>
+							<div class="tags">
+								{#each tags as tag}
+									<span class="badge"># {tag}</span>
+								{/each}
+							</div>
+							<a class="blogLink" href="/posts/{slug}">
+								read more
+								<SquareArrowOurUpRight size="10" />
+							</a>
+						</div>
+						{#if isAdmin}
+							<div class="actionsGroup">
+								<button class="edit" type="button"><Pencil /></button>
+								<form
+									method="POST"
+									action="?/deletePost"
+									use:enhance={({ cancel }) => {
+										if (confirm('Are you sure you want to delete this post?')) {
+											return async ({ update }) => {
+												return update();
+											};
+										} else {
+											cancel();
+										}
+									}}
+								>
+									<input type="hidden" name="slug" id="slug" value={slug} />
+									<button class="delete" type="submit">
+										<Trash />
+									</button>
+								</form>
+							</div>
+						{/if}
+					</li>
+				{/each}
+			</ul>
+		{:else}
+			<div class="noPostsWrapper">
+				<h1>The River Blog</h1>
+				<p>No posts found.</p>
+			</div>
+		{/if}
 	</section>
 </main>
 
@@ -76,15 +132,44 @@
 		color: var(--text-2);
 	}
 	button {
-		padding: var(--size-1) var(--size-4);
 		font-size: var(--font-size-0);
+	}
+	a:hover {
+		text-decoration: none;
+	}
+	.hidden {
+		display: none;
+	}
+	.success-form-message {
+		display: flex;
+		gap: var(--size-2);
+		color: var(--green-4);
+	}
+	.noPostsWrapper {
+		/* height: 100vh; */
+		margin-block: var(--size-content-1);
+	}
+	.titleWrapper {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		gap: var(--size-4);
+	}
+	.titleWrapper-sub {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+	.titleWrapper-sub button {
+		flex-grow: 0;
+		flex-shrink: 0;
 	}
 	.actionsGroup {
 		display: flex;
-		flex-direction: row;
+		flex-direction: column;
 		gap: var(--size-2);
-		justify-content: end;
-		margin-block-start: var(--size-4);
+		justify-content: center;
+		align-items: center;
 	}
 	.description {
 		margin-block: var(--size-4);
@@ -94,12 +179,15 @@
 		box-shadow: var(--shadow-2);
 		padding: var(--size-4);
 	}
+	.cardWrapper {
+		display: grid;
+		grid-template-columns: 4fr 0.5fr;
+	}
 	.blogLink {
 		display: flex;
 		justify-content: end;
 		gap: var(--size-1);
 		color: var(--link);
-		text-decoration: none;
 		font-size: var(--font-size-0);
 		margin-block: var(--size-2);
 	}
@@ -111,5 +199,15 @@
 		border-radius: var(--radius-3);
 		font-size: var(--font-size-0);
 		margin-inline-end: var(--size-1);
+	}
+
+	.create {
+		background-color: hsl(var(--purple-4-hsl) / 50%);
+	}
+	.edit {
+		background-color: hsl(var(--brown-3-hsl) / 50%);
+	}
+	.delete {
+		background-color: hsl(var(--red-4-hsl) / 50%);
 	}
 </style>
