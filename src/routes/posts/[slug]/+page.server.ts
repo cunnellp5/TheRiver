3bd0268471d957653fb0a7b61096469bf402ee8f
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-18 17:01:17 -0600

    adding form actions to single post

--- src/routes/posts/[slug]/+page.server.ts
+++ src/routes/posts/[slug]/+page.server.ts
@@ -1,16 +1,64 @@
-import { error } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
+import { error, fail, redirect } from '@sveltejs/kit';
+import type { PageServerLoad, Actions } from './$types';
+import slugify from '$lib/utils/slugify';
 
-export const load: PageServerLoad = async ({ fetch, params }) => {
+export const load: PageServerLoad = async ({ fetch, params, locals }) => {
 	const response = await fetch('/api/posts/' + params.slug);
 
 	const post = await response.json();
 
 	if (!post) {
-		// maybe redirect instead? but that might be confusing
 		error(404, 'Post not found');
 	}
 
-	// return json(post);
-	return { post };
+	return {
+		post,
+		isAdmin: locals?.user?.isAdmin
+	};
+};
+
+export const actions: Actions = {
+	default: async ({ fetch, request, params }) => {
+		const slug = params.slug;
+
+		const formData = await request.formData();
+		const title = formData.get('title')?.toString();
+		const content = formData.get('content');
+		const tags = formData.get('tagInput')?.toString();
+
+		if (!slug) {
+			error(400, 'Invalid slug');
+		}
+
+		if (!title) {
+			console.log(title, 'nowooo');
+			return fail(400, { message: 'Invalid title' });
+		}
+
+		if (!content) {
+			return fail(400, { message: 'Add content' });
+		}
+
+		const response = await fetch(`/api/posts/${slug}`, {
+			method: 'PATCH',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				title,
+				content,
+				tags: tags?.split(',').map((tag: string) => tag.trim()),
+				slug: slugify(title)
+			})
+		});
+
+		if (response.ok) {
+			const post = await response.json();
+
+			return redirect(302, `/posts/${post.slug}`);
+		} else {
+			const errorMessage = await response.json();
+			error(response.status, errorMessage);
+		}
+	}
 };
