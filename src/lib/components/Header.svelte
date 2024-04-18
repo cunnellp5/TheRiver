08855ea8a3b454d5f67c746d235565b9c3863385
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-17 21:21:09 -0600

    add aria current to navigation

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -39,19 +39,34 @@
 		</a>
 
 		<ul class="links">
-			<li class:current={$page.url.pathname == '/music'}>
+			<li
+				class:current={$page.url.pathname == '/music'}
+				aria-current={$page.url.pathname == '/music'}
+			>
 				<a href="/music"> Music </a>
 			</li>
-			<li class:current={$page.url.pathname == '/services'}>
+			<li
+				class:current={$page.url.pathname == '/services'}
+				aria-current={$page.url.pathname == '/services'}
+			>
 				<a href="/services"> services </a>
 			</li>
-			<li class:current={$page.url.pathname == '/shop'}>
+			<li
+				class:current={$page.url.pathname == '/shop'}
+				aria-current={$page.url.pathname == '/shop'}
+			>
 				<a href="/shop"> Merch </a>
 			</li>
-			<li class:current={$page.url.pathname == '/posts'}>
+			<li
+				class:current={$page.url.pathname == '/posts'}
+				aria-current={$page.url.pathname == '/posts'}
+			>
 				<a href="/posts"> Blog </a>
 			</li>
-			<li class:current={$page.url.pathname == '/contact'}>
+			<li
+				class:current={$page.url.pathname == '/contact'}
+				aria-current={$page.url.pathname == '/contact'}
+			>
 				<a href="/contact"> Contact </a>
 			</li>
 
