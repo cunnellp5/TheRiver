72a8a7ffb5b7a6d9c82be1274adc39ef49554a68
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-19 10:52:15 -0600

    rm username leftovers

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -2,9 +2,9 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
-	console.log(event.locals, 'whatishappenind');
 	if (!event.locals.user) redirect(302, '/auth/login');
+
 	return {
-		username: event.locals.user.username
+		isAdmin: event.locals.user.isAdmin
 	};
 };
