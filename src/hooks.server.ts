09130ef8de20b8fb7c43e5a09e4a16a9ee13eb60
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-19 10:51:07 -0600

    playing with auth hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -15,6 +15,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 
 	if (session && session.fresh) {
 		const sessionCookie = lucia.createSessionCookie(session.id);
+
 		event.cookies.set(sessionCookie.name, sessionCookie.value, {
 			path: '.',
 			...sessionCookie.attributes
@@ -23,6 +24,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 
 	if (!session) {
 		const sessionCookie = lucia.createBlankSessionCookie();
+
 		event.cookies.set(sessionCookie.name, sessionCookie.value, {
 			path: '.',
 			...sessionCookie.attributes
@@ -30,6 +32,8 @@ export const handle: Handle = async ({ event, resolve }) => {
 	}
 
 	event.locals.user = user;
+
 	event.locals.session = session;
+
 	return resolve(event);
 };
