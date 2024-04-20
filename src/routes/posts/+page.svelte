0f79a00e6b4752e7ab566554cca634df87a7928e
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-20 15:19:17 -0600

    building smaller components

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
-	import formatDate from '$lib/utils/formatDate';
 	import Check from 'lucide-svelte/icons/check';
-	import Pencil from 'lucide-svelte/icons/pencil';
-	import SquareArrowOurUpRight from 'lucide-svelte/icons/square-arrow-out-up-right';
 	import Trash from 'lucide-svelte/icons/trash';
 	import { fly, slide } from 'svelte/transition';
 	import type { ActionData, PageData } from './$types';
+	import BlogCard from './components/BlogCard.svelte';
 
 	export let form: ActionData;
 	export let data: PageData;
@@ -37,51 +34,7 @@
 			<ul>
 				{#each posts as { slug, title, tags, createdAt, content }}
 					<li in:fly={{ y: 20 }} out:slide class="cardWrapper">
-						<div class="card">
-							<div>
-								<h5>
-									<a href="/posts/{slug}">
-										{title}
-									</a>
-								</h5>
-								<date>{formatDate(new Date(createdAt))}</date>
-							</div>
-							<p class="description">
-								{`${content?.substring(0, 100)}...`}
-							</p>
-							<div>
-								{#each tags as tag}
-									<span class="badge"># {tag}</span>
-								{/each}
-							</div>
-							<a class="blogLink" href="/posts/{slug}">
-								read more
-								<SquareArrowOurUpRight size="10" />
-							</a>
-						</div>
-						{#if isAdmin}
-							<div class="actionsGroup">
-								<a href="/posts/{slug}">
-									<button class="edit" type="button">
-										<Pencil />
-									</button>
-								</a>
-								<form
-									method="POST"
-									action="?/deletePost"
-									use:enhance={({ cancel }) => {
-										if (confirm('Are you sure you want to delete this post?')) {
-											return async ({ update }) => update();
-										}
-										cancel();
-									}}>
-									<input type="hidden" name="slug" id="slug" value={slug} />
-									<button class="delete" type="submit">
-										<Trash />
-									</button>
-								</form>
-							</div>
-						{/if}
+						<BlogCard {title} {tags} {content} {createdAt} {slug} {isAdmin} />
 					</li>
 				{/each}
 			</ul>
@@ -94,27 +47,7 @@
 </main>
 
 <style>
-	main {
-		width: 100%;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-	}
-	date {
-		color: var(--stone-9);
-		font-size: var(--font-size-0);
-	}
-	p {
-		line-height: var(--font-lineheight-0);
-		font-family: var(--font-mono);
-		color: var(--text-2);
-	}
-	button {
-		font-size: var(--font-size-0);
-	}
-	a:hover {
-		text-decoration: none;
-	}
+	/* CLASSES */
 	.success-form-message {
 		display: flex;
 		gap: var(--size-2);
@@ -132,50 +65,19 @@
 		flex-grow: 0;
 		flex-shrink: 0;
 	}
-	.actionsGroup {
-		display: flex;
-		flex-direction: column;
-		gap: var(--size-2);
-		justify-content: center;
-		align-items: center;
-	}
-	.description {
-		margin-block: var(--size-4);
-		font-size: var(--font-size-2);
-		font-family: var(--font-serif);
-		letter-spacing: var(--font-letterspacing-2);
-	}
 	.card {
 		margin-block: var(--size-4);
 		box-shadow: var(--shadow-2);
 		padding: var(--size-4);
 	}
-	.cardWrapper {
-		display: grid;
-		grid-template-columns: 4fr 0.5fr;
-	}
-	.blogLink {
-		display: flex;
-		justify-content: end;
-		gap: var(--size-1);
-		color: var(--link);
-		font-size: var(--font-size-0);
-		margin-block: var(--size-2);
-	}
-	.badge {
-		padding-inline: var(--size-2);
-		padding-block: var(--size-1);
-		background: hsl(var(--gray-8-hsl) / 50%);
-		color: hsl(var(--pink-2-hsl) / 50%);
-		border-radius: var(--radius-3);
-		font-size: var(--font-size-0);
-		margin-inline-end: var(--size-1);
-	}
 
-	.edit {
-		background-color: hsl(var(--brown-3-hsl) / 50%);
+	ul {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+		/* grid-template-columns: repeat(auto-fill); */
+		/* gap: 20px; */
 	}
-	.delete {
-		background-color: hsl(var(--red-4-hsl) / 50%);
-	}
+	/* ul li:nth-child(odd) {
+		grid-row: span 2;
+	} */
 </style>
---
+++ src/routes/posts/components/BlogCard.svelte
@@ -0,0 +1,117 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import formatDate from '$lib/utils/formatDate';
+	import Pencil from 'lucide-svelte/icons/pencil';
+	import SquareArrowOurUpRight from 'lucide-svelte/icons/square-arrow-out-up-right';
+	import Trash from 'lucide-svelte/icons/trash';
+
+	export let title: string;
+	export let tags: string[];
+	export let content: string | null;
+	export let createdAt: Date;
+	export let slug: string;
+	export let isAdmin: boolean | undefined;
+</script>
+
+<div class="card">
+	<div>
+		<div>
+			<h5>
+				<a href="/posts/{slug}">
+					{title}
+				</a>
+			</h5>
+			<date>{formatDate(new Date(createdAt))}</date>
+		</div>
+		<p class="description">
+			{`${content?.substring(0, 100)}...`}
+		</p>
+		<div>
+			{#each tags as tag}
+				<span class="badge"># {tag}</span>
+			{/each}
+		</div>
+		<a class="blogLink" href="/posts/{slug}">
+			read more
+			<SquareArrowOurUpRight size="10" />
+		</a>
+	</div>
+	{#if isAdmin}
+		<div class="actionsGroup">
+			<a href="/posts/{slug}">
+				<button class="edit" type="button">
+					<Pencil />
+				</button>
+			</a>
+			<form
+				method="POST"
+				action="?/deletePost"
+				use:enhance={({ cancel }) => {
+					if (confirm('Are you sure you want to delete this post?')) {
+						return async ({ update }) => update();
+					}
+					cancel();
+				}}>
+				<input type="hidden" name="slug" id="slug" value={slug} />
+				<button class="delete" type="submit">
+					<Trash />
+				</button>
+			</form>
+		</div>
+	{/if}
+</div>
+
+<style>
+	date {
+		color: var(--stone-9);
+		font-size: var(--font-size-0);
+	}
+	p {
+		line-height: var(--font-lineheight-0);
+		font-family: var(--font-mono);
+		color: var(--text-2);
+	}
+	a:hover {
+		text-decoration: none;
+	}
+
+	/* CLASSES */
+	.card {
+		margin-block: var(--size-4);
+		box-shadow: var(--shadow-2);
+		padding: var(--size-4);
+	}
+	.description {
+		margin-block: var(--size-4);
+		font-size: var(--font-size-2);
+		font-family: var(--font-serif);
+		letter-spacing: var(--font-letterspacing-2);
+	}
+	.badge {
+		padding-inline: var(--size-2);
+		padding-block: var(--size-1);
+		background: hsl(var(--gray-8-hsl) / 50%);
+		color: hsl(var(--pink-2-hsl) / 50%);
+		border-radius: var(--radius-3);
+		font-size: var(--font-size-0);
+		margin-inline-end: var(--size-1);
+	}
+	.blogLink {
+		display: flex;
+		justify-content: end;
+		gap: var(--size-1);
+		color: var(--link);
+		font-size: var(--font-size-0);
+		margin-block: var(--size-2);
+	}
+	.actionsGroup {
+		display: flex;
+		flex-direction: row;
+		gap: var(--size-2);
+		justify-content: end;
+		align-self: end;
+	}
+	.delete {
+		background-color: hsl(var(--red-5-hsl) / 80%);
+	}
+</style>
