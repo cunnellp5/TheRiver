1e09ecfac7114403edb0332cde1a3368157e6e2c
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-19 19:07:45 -0600

    clean up unused pages

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -1,23 +1,14 @@
 <main>
-	<section>
-		<h1>THE RIVER BEAUTY</h1>
-		<p>🚧 Under construction</p>
-	</section>
+	<h1>THE RIVER BEAUTY</h1>
+	<p>🚧 Under construction</p>
 </main>
 
 <style>
 	main {
 		display: flex;
 		align-items: center;
-		flex-direction: row;
+		flex-direction: column;
 		justify-content: center;
 		height: 100vh;
-	}
-
-	section {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
 	}
 </style>
--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -3,39 +3,20 @@
 </script>
 
 <main>
-	<div class="hero">
-		<h1>THE RIVER CREATES</h1>
-		<div class="arrow">Coming soon!</div>
-	</div>
+	<h1>THE RIVER CREATES</h1>
+	<div class="animation">Coming spoon!</div>
 </main>
 
 <style>
-	h1 {
-		margin-block: var(--size-7);
-	}
-
 	main {
-		margin: 0 auto;
-	}
-
-	h1 {
-		margin-bottom: 0.5rem;
-		/* text-transform: uppercase; */
-	}
-
-	p {
-		margin-top: 0;
-	}
-
-	.hero {
-		height: calc(100vh - var(--size-8));
 		display: flex;
-		flex-direction: column;
 		align-items: center;
+		flex-direction: column;
 		justify-content: center;
+		height: 100vh;
 	}
 
-	.arrow {
+	.animation {
 		margin-block: var(--size-4);
 		animation: var(--animation-float);
 		animation-timing-function: var(--ease-elastic-in-out-3);
