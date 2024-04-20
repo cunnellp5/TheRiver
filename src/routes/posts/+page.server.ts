3ffa0760991f29d8dc3598b1b4156bc030b223b6
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-19 19:06:40 -0600

    add create post

--- src/routes/posts/+page.server.ts
+++ src/routes/posts/+page.server.ts
@@ -2,6 +2,7 @@ import { error, fail } from '@sveltejs/kit';
 import type { Post } from '@prisma/client';
 import type { PageServerLoad } from '../$types';
 import type { Actions } from './$types';
+import db from '$lib/server/database';
 
 export const load: PageServerLoad = async ({ fetch, locals }) => {
 	const response = await fetch('/api/posts');
@@ -20,6 +21,38 @@ export const load: PageServerLoad = async ({ fetch, locals }) => {
 };
 
 export const actions: Actions = {
+	default: async ({ request }) => {
+		const formData = await request.formData();
+		const title = formData.get('title').trim();
+		const content = formData.get('content');
+		const tags = formData.get('tagInput');
+
+		if (!title) {
+			return fail(400, { message: 'Invalid title' });
+		}
+
+		if (!content) {
+			return fail(400, { message: 'Add content' });
+		}
+
+		const slugified = slugify(title);
+
+		const post = await db.post.create({
+			data: {
+				title,
+				content,
+				tags: tags?.split(',').map((tag: string) => tag.trim()),
+				slug: slugified
+			}
+		});
+
+		return {
+			success: true,
+			message: 'Post created',
+			createdTitle: post.title
+		};
+	},
+
 	deletePost: async ({ fetch, request }) => {
 		const formData = await request.formData();
 		const slug = formData.get('slug');
