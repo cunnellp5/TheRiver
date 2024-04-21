7193f02c93d4cd77eacfe44ac2591a2e2c6ef951
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-21 11:56:44 -0600

    throw actual error and handle that instead of redirecting

--- src/routes/admin/+layout.server.ts
+++ src/routes/admin/+layout.server.ts
@@ -1,9 +1,11 @@
-import { redirect } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from '../$types';
 
 // prevents any unauthorized access to ANY of the admin panel
 export const load: PageServerLoad = async (event) => {
-	if (!event.locals.user) redirect(302, '/error');
+	if (!event.locals.user) {
+		return error(400, 'Bad request');
+	}
 
 	return {
 		email: event.locals.user.email
