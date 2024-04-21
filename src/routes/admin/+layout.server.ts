1701b6d148dc6101942de119d762299995cc77e0
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-21 11:44:14 -0600

    format

--- src/routes/admin/+layout.server.ts
+++ src/routes/admin/+layout.server.ts
@@ -3,7 +3,8 @@ import type { PageServerLoad } from '../$types';
 
 // prevents any unauthorized access to ANY of the admin panel
 export const load: PageServerLoad = async (event) => {
-	if (!event.locals.user) redirect(302, '/');
+	if (!event.locals.user) redirect(302, '/error');
+
 	return {
 		email: event.locals.user.email
 	};
