94b772b41b72446decf85a575bf10e175e884db2
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-20 20:48:21 -0600

    move quillConfig to utility

---
+++ src/lib/utils/QuillConfig.ts
@@ -0,0 +1,13 @@
+export default {
+	modules: {
+		toolbar: [
+			[{ header: 1 }, { header: 2 }, 'blockquote', 'link', 'image', 'video'],
+			['bold', 'italic', 'underline', 'strike'],
+			[{ list: 'ordered' }, { list: 'bullet' }],
+			[{ align: [] }],
+			['clean']
+		]
+	},
+	theme: 'snow',
+	placeholder: `Write about it...`
+};
