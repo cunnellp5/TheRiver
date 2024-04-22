8772d24dc7f74f1f9b35196bf418a77353bdde72
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-21 21:58:35 -0600

    update contact page a little more

--- src/routes/(app)/contact/+page.svelte
+++ src/routes/(app)/contact/+page.svelte
@@ -1,5 +1,16 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import Copy from 'lucide-svelte/icons/copy';
+	import Send from 'lucide-svelte/icons/send';
+
+	let email = 'theriverrunsfast@gmail.com';
+	let copied = false;
+
+	function copyEmail() {
+		navigator.clipboard.writeText(email);
+		copied = true;
+		setTimeout(() => (copied = false), 2000); // Reset after 2 seconds
+	}
 
 	onMount(() => {
 		document.title = 'Contact | The River Sings';
@@ -10,6 +21,18 @@
 	<section>
 		<h1>Contact</h1>
 		<p>theriverrunsfast@gmail.com</p>
+		<section class="buttons">
+			<button on:click={copyEmail}>
+				{#if copied}
+					Copied!
+				{:else}
+					<Copy />
+				{/if}
+			</button>
+			<a href="mailto:{email}">
+				<button> <Send /> </button>
+			</a>
+		</section>
 	</section>
 </main>
 
@@ -40,7 +63,14 @@
 	} */
 
 	p {
-		font-family: monospace;
-		letter-spacing: var(--font-letterspacing-1);
+		/* font-family: monospace; */
+		letter-spacing: var(--font-letterspacing-3);
+	}
+
+	.buttons {
+		display: flex;
+		flex-direction: row;
+		gap: var(--size-4);
+		margin-block-start: var(--size-2);
 	}
 </style>
