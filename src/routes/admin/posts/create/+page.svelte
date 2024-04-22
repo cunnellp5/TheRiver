ac9433fe58a4a73e99d26fcc2da6ac4667a59768
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-21 21:18:06 -0600

    render form

--- src/routes/admin/posts/create/+page.svelte
+++ src/routes/admin/posts/create/+page.svelte
@@ -1,1 +1,16 @@
-<h1>creating in admin portal</h1>
+<script lang="ts">
+	import PostsForm from '$lib/components/ui/posts/PostsForm.svelte';
+	import type { ActionData } from '../../../$types';
+	export let form: ActionData;
+</script>
+
+<section>
+	<PostsForm {form} />
+</section>
+
+<style>
+	section {
+		display: flex;
+		justify-content: center;
+	}
+</style>
