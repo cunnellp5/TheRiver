36fa83541db4b2fed71e00e332131a64ab253157
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-25 17:26:36 -0600

    table pocs

---
+++ src/lib/components/ui/Table.svelte
@@ -0,0 +1,44 @@
+<!-- Table.svelte -->
+<script lang="ts">
+	export let columns = [];
+	export let data = [];
+</script>
+
+<table>
+	<thead>
+		<tr>
+			{#each columns as column (column)}
+				<th>{column}</th>
+			{/each}
+		</tr>
+	</thead>
+	<tbody>
+		{#each data as row (row)}
+			<tr>
+				{#each Object.values(row) as cell (cell)}
+					<td>{cell}</td>
+				{/each}
+				<td>
+					<button class="primary">Book now</button>
+				</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
+
+<style>
+	/* Add your table styles here */
+	table {
+		box-shadow: var(--shadow-2);
+		/* border-collapse: collapse; */
+		width: 100%;
+		font-size: var(--size-6);
+	}
+	th {
+		color: var(--link);
+	}
+
+	button {
+		padding-block: 0;
+	}
+</style>
