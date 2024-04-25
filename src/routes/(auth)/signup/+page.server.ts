71e505f7b3e39d9098acdfbdad238bedd55180e3
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-25 15:08:17 -0600

    move config stuff out

--- src/routes/(auth)/signup/+page.server.ts
+++ src/routes/(auth)/signup/+page.server.ts
@@ -2,76 +2,11 @@ import { lucia } from '$lib/server/auth';
 import { error, fail, redirect } from '@sveltejs/kit';
 import { Argon2id } from 'oslo/password';
 import db from '$lib/server/database';
-import { boolean, email as emailV, minLength, object, parse, string, ValiError } from 'valibot';
+import { parse, ValiError } from 'valibot';
+import { SignUpSchema } from '$lib/test/ValibotSchemas/SignUpSchema';
+import type { SignUpValidator } from '$lib/test/ValibotSchemas/SignUpSchema';
+
 import type { Actions, PageServerLoad } from './$types';
-
-// Create login schema with email and password
-const SignUpSchema = object({
-	email: string('Your email must be a string', [
-		emailV('Please enter a valid email.'),
-		minLength(1, 'Please enter your email.')
-	]),
-	password: string('Your password must be a string.', [
-		minLength(1, 'Please enter your password.'),
-		minLength(8, 'Your password must have 8 characters or more.')
-	]),
-	firstName: string('Your first name must be a string.', [
-		minLength(1, 'Please enter your first name.')
-	]),
-	lastName: string('Your last name must be a string.', [
-		minLength(1, 'Please enter your last name.')
-	]),
-	isSubscribed: boolean('Your subscription must be a boolean.'),
-	confirm: string('Your confirmation must be a string.', [
-		minLength(1, 'Please enter your confirmation.')
-	])
-});
-
-// Infer output TypeScript type of signup schema
-type SignUpValidator = Output<typeof SignUpSchema>;
-
-async function validateEmail(email: string) {
-	try {
-		await db.user.findUnique({
-			where: { email: email.toString() }
-		});
-		return null;
-	} catch (err) {
-		return fail(400, { message: 'That email is already used' });
-	}
-}
-
-function validateSignUpForm({
-	email,
-	password,
-	firstName,
-	lastName,
-	isSubscribed,
-	confirm
-}: {
-	email: string;
-	password: string;
-	firstName: string;
-	lastName: string;
-	isSubscribed: string;
-	confirm: string;
-}) {
-	try {
-		parse(SignUpSchema, {
-			email,
-			password,
-			firstName,
-			lastName,
-			isSubscribed: isSubscribed === 'on',
-			confirm
-		}) as SignUpValidator;
-	} catch (err) {
-		const errors = err as ValiError;
-		throw fail(400, {
-			message: errors.message
-		});
-	}
-}
 
 export const load: PageServerLoad = async (event) => {
 	if (event.locals.session) redirect(302, '/login');
@@ -99,19 +34,31 @@ export const actions: Actions = {
 		)
 			return fail(400, { message: 'Please fill out all fields' });
 
-		console.log(formData, 'calling default function in test!');
 		// validates with valibot
-		await validateSignUpForm({
-			email,
-			password,
-			firstName,
-			lastName,
-			isSubscribed,
-			confirm
-		});
+		try {
+			parse(SignUpSchema, {
+				email,
+				password,
+				firstName,
+				lastName,
+				isSubscribed: isSubscribed === 'on',
+				confirm
+			}) as SignUpValidator;
+		} catch (err) {
+			const errors = err as ValiError;
+			return fail(400, {
+				message: errors.message
+			});
+		}
 
 		// Check db for existing email
-		await validateEmail(email);
+		try {
+			await db.user.findUnique({
+				where: { email: email.toString() }
+			});
+		} catch (err) {
+			return fail(400, { message: 'That email is already used' });
+		}
 
 		let hashedPassword = null;
 
@@ -138,11 +85,14 @@ export const actions: Actions = {
 			// inner set
 			try {
 				const session = await lucia.createSession(newUser.id, {});
+
 				const sessionCookie = lucia.createSessionCookie(session.id);
+
 				cookies.set(sessionCookie.name, sessionCookie.value, {
 					path: '.',
 					...sessionCookie.attributes
 				});
+
 				throw redirect(302, '/dashboard');
 			} catch {
 				throw error(500, { message: 'Failed to create session' });
