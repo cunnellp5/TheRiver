5ad02b98dda73890c230ef7067d8b3f3d124af84
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-25 12:16:05 -0600

    test config

--- svelte.config.js
+++ svelte.config.js
@@ -15,6 +15,15 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
+	},
+
+	test: {
+		// Jest like globals
+		globals: true,
+		environment: 'jsdom',
+		include: ['src/**/*.{test,spec}.ts'],
+		// Extend jest-dom matchers
+		setupFiles: ['./setupTest.ts']
 	}
 };
 
