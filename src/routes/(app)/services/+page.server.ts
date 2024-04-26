3f71ebe5bd9030b78f33cb91aa086149f4ed6d9c
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-26 08:29:06 -0600

    server service

---
+++ src/routes/(app)/services/+page.server.ts
@@ -0,0 +1,15 @@
+import db from '$lib/server/database';
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async () => {
+	try {
+		const services = await db.service.findMany();
+
+		if (!services) return error(404, 'No services found');
+
+		return { services };
+	} catch (err) {
+		return error(500, err.message);
+	}
+};
