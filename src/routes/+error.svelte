6c0efffac8df0840b3ff99abbdf820a4c9675b75
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-25 19:40:01 -0600

    artsy error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,19 +5,38 @@
 
 <main>
 	{#if $page.status === 404}
-		<h1>You must be lost.</h1>
-		<a href="/">Go Home</a>
+		<h1>The River</h1>
+		<section>
+			<div>
+				<h3>Creates</h3>
+				<h2>&nbsp;&nbsp;Sings</h2>
+				<h4>Beauty</h4>
+				<a href="/">
+					<h5>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Home</h5>
+				</a>
+			</div>
+		</section>
 	{:else if $page.status === 500}
 		<h1>We weren't expecting that</h1>
 		<p>We're investigating the issue.</p>
 	{:else}
 		<h1>{$page.status}</h1>
 		<p>{$page.error?.message}</p>
-		<a href="/">Go home</a>
+		<a href="/">Home</a>
 	{/if}
 </main>
 
 <style>
+	h1 {
+		font-size: var(--size-15);
+		/* letter-spacing: var(--size-8); */
+	}
+	section {
+		position: absolute;
+		/* flex-direction: row; */
+		justify-content: start;
+	}
+
 	main {
 		display: flex;
 		flex-direction: column;
