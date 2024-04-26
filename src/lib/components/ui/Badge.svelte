d4ae285fe794ba2ffad811c3221c3a4a2034c2e9
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-26 16:16:21 -0600

    badge styled component

---
+++ src/lib/components/ui/Badge.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let tag: string;
+	export let prefix: string | null = null;
+</script>
+
+{#if prefix}
+	<span class="badge">{prefix} {tag}</span>
+{:else}
+	<span class="badge">
+		{tag}
+		<slot></slot>
+	</span>
+{/if}
+
+<style>
+	.badge {
+		margin-inline-end: var(--size-1);
+		border-radius: var(--radius-3);
+		background: var(--border);
+		padding-inline: var(--size-2);
+		padding-block: var(--size-1);
+		color: hsl(var(--gray-6-hsl));
+		font-size: var(--font-size-0);
+		letter-spacing: var(--font-letterspacing-1);
+	}
+</style>
