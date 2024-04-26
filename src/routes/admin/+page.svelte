519d20e36075559032be517c53f16e77745592ab
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-26 11:31:20 -0600

    descriptions

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,9 +1,16 @@
 <h2>Description</h2>
 
 <p>This is how you can change the contents of your website.</p>
+<p>
+	The App folder shows all the things you can change that you see in the nav bar [Posts, Services,
+	etc...]
+</p>
+<p>The Admin folder shows config you can change, including user data</p>
 
 <style>
 	p {
 		margin-block: var(--size-4);
+		font-weight: var(--font-weight-1);
+		line-height: var(--font-lineheight-1);
 	}
 </style>
