22b130858060a1c79550dff14f7201d7b5dae022
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-27 18:28:04 -0600

    generic dropdown

---
+++ src/lib/components/ui/Dropdown.svelte
@@ -0,0 +1,42 @@
+<script>
+	let isOpen = true;
+
+	function toggle() {
+		isOpen = !isOpen;
+	}
+</script>
+
+<a class="dropdown" on:click={toggle}>
+	<div class="parent-menu">
+		<div class="arrow">{isOpen ? '▼' : '►'}</div>
+		<slot name="label"></slot>
+	</div>
+</a>
+
+{#if isOpen}
+	<div class="child-menu">
+		<slot></slot>
+	</div>
+{/if}
+
+<style>
+	a {
+		color: var(--gray-7);
+		user-select: none;
+	}
+	.parent-menu {
+		display: flex;
+		/* justify-content: center; */
+		align-items: center;
+		/* padding: var(--size-3); */
+	}
+	.child-menu {
+		transition: transform 0.3s ease;
+		margin-left: var(--size-3);
+		border-left: 1px solid var(--link);
+	}
+	.arrow {
+		margin-inline: var(--size-2);
+		font-size: var(--size-2);
+	}
+</style>
