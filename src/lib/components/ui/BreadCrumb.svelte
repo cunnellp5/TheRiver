87bfc7e5d9787f6bbffeae4be94ed38300f56565
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-28 09:59:43 -0600

    breadcrumb ui

---
+++ src/lib/components/ui/BreadCrumb.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let crumbs: { name: string; link: string }[] = [];
+</script>
+
+{#each crumbs as { name, link }, index (link)}
+	<span>
+		{#if index < crumbs.length - 1}
+			<a href={link}>{name}</a>
+		{:else}
+			{name}
+		{/if}
+		{#if index < crumbs.length - 1}
+			&rsaquo;
+		{/if}
+	</span>
+{/each}
