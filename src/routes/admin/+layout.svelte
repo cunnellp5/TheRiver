bb7cf56bb6cad41c87a5755d7c82c02ea96b8bb2
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-28 10:07:27 -0600

    admin nav layout

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,27 +1,64 @@
 <script lang="ts">
+	import BreadCrumb from '$lib/components/ui/BreadCrumb.svelte';
 	import Dropdown from '$lib/components/ui/Dropdown.svelte';
 	// eslint-disable-next-line import/no-unresolved
+	import { browser } from '$app/environment';
+	// eslint-disable-next-line import/no-unresolved
 	import { page } from '$app/stores';
+
+	let crumbs: { name: string; link: string }[] = [];
+
+	$: if (browser) {
+		const pathnames = $page.url.pathname.split('/').filter(Boolean);
+		crumbs = pathnames.map((name, index, arr) => ({
+			name: name.charAt(0).toUpperCase() + name.slice(1),
+			link: `/${arr.slice(0, index + 1).join('/')}`
+		}));
+	}
 </script>
 
 <header>
 	<h1>ADMIN PORTAL</h1>
+	<div>
+		<BreadCrumb {crumbs}></BreadCrumb>
+	</div>
 </header>
 <main>
 	<aside>
 		<nav class="sidenav">
 			<a class:current={$page.url.pathname === '/admin'} href="/admin/"> Home </a>
 			<Dropdown>
-				<div class="dropLabel" slot="label">App</div>
-				<a class:current={$page.url.pathname.includes('/admin/services')} href="/admin/services">
-					Services
-				</a>
-				<!-- <a class:current={$page.url.pathname.includes('/admin/services')} href="/admin/services">
-					Services
-				</a> -->
+				<div
+					class="dropLabel"
+					class:currentParent={$page.url.pathname.includes('/admin/posts')}
+					slot="label">
+					App
+				</div>
+
 				<a class:current={$page.url.pathname.includes('/admin/posts')} href="/admin/posts">
 					Posts
 				</a>
+				<Dropdown>
+					<div
+						class="dropLabel"
+						class:currentParent={$page.url.pathname.includes('/admin/services')}
+						slot="label">
+						Services
+					</div>
+					<a class:current={$page.url.pathname === '/admin/services'} href="/admin/services">
+						Jobs
+					</a>
+					<a
+						class:current={$page.url.pathname.includes('/admin/services/bookings')}
+						href="/admin/services/bookings">
+						Bookings
+					</a>
+					<a
+						class:current={$page.url.pathname.includes('/admin/services/time-slots')}
+						href="/admin/services/time-slots">
+						Time slots
+					</a>
+				</Dropdown>
 			</Dropdown>
 			<Dropdown>
 				<div class="dropLabel" slot="label">Admin</div>
@@ -48,6 +85,7 @@
 	}
 	header {
 		display: flex;
+		flex-direction: column;
 		margin-block: var(--size-5);
 		padding-block: var(--size-5);
 		/* background-image: var(--gradient-16); */
@@ -76,19 +114,28 @@
 		padding: var(--size-3);
 		text-decoration: none;
 	}
-	.sidenav a:hover,
-	.dropLabel:hover {
+	.sidenav a:hover {
 		cursor: pointer;
-		background-color: var(--border);
 		/* color: #f1f1f1; */
+		background-color: var(--border);
 	}
+
+	.sidenav a {
+	}
+
 	.current {
-		/* background-color: var(--gray-9); */
+		/* border-right: 1px solid var(--link); */
+		border-left: 1px solid var(--link);
+		background-color: var(--surface-3);
 		/* color: var(--gray-1); */
 		color: var(--link);
 	}
+	.currentParent {
+		border-bottom: 1px solid var(--gray-6);
+	}
 
 	.dropLabel {
+		cursor: pointer;
 		width: 100%;
 	}
 </style>
