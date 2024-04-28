a88e936969b6d1bb953295f59afd4189855c58d3
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-28 10:28:21 -0600

    table styles

--- src/lib/components/ui/Table.svelte
+++ src/lib/components/ui/Table.svelte
@@ -7,7 +7,6 @@
 <table>
 	<thead class="surface-2">
 		<tr>
-			<th></th>
 			{#each columns as column (column)}
 				<th>
 					<h6>
@@ -20,7 +19,6 @@
 	<tbody class="">
 		{#each data as row (row)}
 			<tr>
-				<td></td>
 				{#each Object.values(row) as cell (cell)}
 					{#if cell === 'AVAILABLE'}
 						<td>available</td>
@@ -32,7 +30,7 @@
 						<td>{cell}</td>
 					{/if}
 				{/each}
-				<td>
+				<td class="actions">
 					<button class="primary">Book now</button>
 				</td>
 			</tr>
@@ -41,61 +39,47 @@
 </table>
 
 <style>
-	/* Add your table styles here */
-	table {
-		box-shadow: var(--shadow-2);
-		border: none;
-		border-collapse: collapse;
-		width: 100%;
-		font-weight: var(--font-weight-2);
-		font-size: var(--size-4);
-	}
-	th {
-		color: var(--text-2);
-		text-align: left;
-	}
-	table,
-	th,
-	td {
-		/* border-right: 1px solid var(--text-2); */
-		background-color: var(--table);
-		/* background-color: unset; */
-		text-align: left;
-	}
-	tbody > tr {
-		border-top: 1px solid var(--text-2);
-	}
-	thead,
-	th {
-		background-image: var(--table);
-		/* background-image: linear-gradient(#111, #111, #191919); */
-	}
-	tbody,
-	th {
-		font-weight: var(--font-weight-7);
-		/* font-size: var(--size-5); */
-		/* text-transform: uppercase; */
-	}
-	thead,
-	th {
-		font-weight: var(--font-weight-5);
-		/* font-size: var(--size-5); */
-		text-transform: uppercase;
-	}
-
 	button {
-		/* padding-block: 0; */
 		font-weight: var(--font-weight-9);
 	}
-
-	/* .table-body {
-		background: hsl(var(--gray-7-hsl) / 70%);
-	} */
-
 	h6 {
 		background: var(--gradient-22) fixed;
 		-webkit-background-clip: text;
 		-webkit-text-fill-color: transparent;
 		background-clip: text;
+	}
+
+	table {
+		box-shadow: var(--shadow-2);
+		border: none;
+		border-collapse: collapse;
+		background-color: var(--table);
+		width: 100%;
+		font-weight: var(--font-weight-2);
+		font-size: var(--size-4);
+	}
+	tbody {
+		font-weight: var(--font-weight-7);
+	}
+	thead {
+		font-weight: var(--font-weight-1);
+		text-transform: uppercase;
+	}
+	th {
+		background-image: var(--table);
+		background-color: var(--table);
+		color: var(--text-2);
+		font-weight: var(--font-weight-7);
+		text-align: left;
+	}
+	tbody > tr {
+		border-top: 1px solid var(--text-2);
+	}
+
+	td {
+		background-color: var(--table);
+		&:not(.actions) {
+			text-align: left;
+		}
 	}
 </style>
