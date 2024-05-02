2104eba5080933bbc09a4a2ec926107dec395ba0
cunnellp5 <philip.cunnell@colorado.edu>
2024-05-02 11:04:59 -0600

    test toast work

--- src/routes/admin/services/booking/+page.svelte
+++ src/routes/admin/services/booking/+page.svelte
@@ -2,13 +2,20 @@
 	import Calendar from '$lib/components/ui/Calendar.svelte';
 	// import Calendar from '@event-calendar/core';
 	// import TimeGrid from '@event-calendar/time-grid';
+	import Toasts from '$lib/components/ui/Toasts.svelte';
+	import { addToast } from '$lib/stores/toast';
 
 	const today = new Date();
 </script>
 
+<Toasts />
 <!-- <button on:click={updateOptions}>Change slot duration</button> -->
 <!-- <button on:click={invokeMethod}>Refetch events</button> -->
 
+<button
+	on:click={() => addToast({ message: 'hello', type: 'info', dismissible: true, timeout: 5000 })}>
+	Add toast
+</button>
 <div class="">
 	<Calendar {today} month={today.getMonth()} year={today.getFullYear()} />
 </div>
