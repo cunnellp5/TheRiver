a4288b55ac4df59cd4f64d4fec4b462109f4c044
cunnellp5 <philip.cunnell@colorado.edu>
2024-05-05 17:02:30 -0600

    show data

--- src/routes/admin/services/time-slots/+page.svelte
+++ src/routes/admin/services/time-slots/+page.svelte
@@ -1,1 +1,36 @@
+<script>
+	import Calendar from '$lib/components/ui/Calendar.svelte';
+	import formatHours from '$lib/utils/formatHours';
+
+	export let data;
+	const { timeSlots } = data;
+</script>
+
 <h4>Services <span>&rsaquo;</span> time slots</h4>
+
+<Calendar {timeSlots} />
+
+<ul>
+	{#each timeSlots as timeSlot}
+		<li class="surface-4">
+			<p>
+				start: {formatHours(timeSlot.startTime)}
+			</p>
+			<p>
+				end: {formatHours(timeSlot.endTime)}
+			</p>
+			<p>
+				Availability: {timeSlot.availability}
+			</p>
+			<p>
+				day: {timeSlot.day}
+			</p>
+			<p>
+				ServiceId {timeSlot.serviceId}
+			</p>
+		</li>
+	{/each}
+</ul>
+
+<style>
+</style>
