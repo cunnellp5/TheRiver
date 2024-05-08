e948f07ef32a23bd57e155f7245714201bde96ee
cunnellp5 <philip.cunnell@colorado.edu>
2024-05-07 22:24:05 -0600

    hello email template

---
+++ src/lib/emails/Hello.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { Button, Hr, Html, Text } from 'svelte-email';
+	import { WEBSITE_URL } from '$lib/data/WEBSITE_URL';
+
+	export let name = 'you are amazing!';
+</script>
+
+<Html lang="en">
+	<Text>
+		Hello, {name}!
+	</Text>
+	<Hr />
+	<Button href={WEBSITE_URL}>Visit TheRiver</Button>
+</Html>
