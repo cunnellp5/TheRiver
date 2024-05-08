0ce3e138704127ae338b3e72b2da146e75c094aa
cunnellp5 <philip.cunnell@colorado.edu>
2024-05-08 13:18:45 -0600

    renames

--- src/lib/utils/ValibotSchemas/Email.ts
+++
@@ -1,7 +0,0 @@
-import * as valibot from 'valibot';
-
-export const Email = valibot.string([
-	valibot.minLength(1, 'Please enter your email.'),
-	valibot.email('The email is badly formatted.'),
-	valibot.maxLength(30, 'Your email is too long.')
-]);
