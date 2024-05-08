83c30ccfab19b63d2a8bc03888251c026ab5be63
cunnellp5 <philip.cunnell@colorado.edu>
2024-05-07 22:28:53 -0600

    reset endpoint works!

---
+++ src/routes/(auth)/reset/+page.server.ts
@@ -0,0 +1,51 @@
+import { lucia } from '$lib/server/auth';
+import { fail, redirect, success } from '@sveltejs/kit';
+import { Argon2id } from 'oslo/password';
+import db from '$lib/server/database';
+import type { Actions } from './$types';
+
+// const ERROR_MESSAGE = 'Invalid credentials';
+
+export const actions: Actions = {
+	default: async ({ request, fetch }) => {
+		const formData = await request.formData();
+		const email = formData.get('email');
+
+		// const user = await db(email);
+		// if (!user) {
+		// 	return res.status(404).send('User not found');
+		// }
+
+		let user = null;
+
+		try {
+			user = db.user.findFirst({
+				where: { email: email.toString() }
+			});
+
+			if (!user) {
+				throw new Error('should trickle down and throw success');
+			}
+		} catch (error) {
+			return success(
+				200,
+				'An email was sent to the email address provided. Please check your email.'
+			);
+			// return fail(404, 'If the email exists, an email was sent');
+		}
+
+		try {
+			await fetch('api/emails/hello', {
+				method: 'POST',
+				body: JSON.stringify({ email }),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+		} catch (error) {
+			console.log(error, 'error from fetch');
+		}
+
+		console.log(email, 'default action nhit');
+	}
+};
