063bbaa8d61f2d4e87b25f600e59c3b7285c269c
cunnellp5 <philip.cunnell@colorado.edu>
2024-05-14 21:02:45 -0600

    chiseling away at styles

--- src/routes/(app)/services/booking/+page.svelte
+++ src/routes/(app)/services/booking/+page.svelte
@@ -14,7 +14,12 @@
 </section>
 
 <style>
-	section {
+	aside {
+		& .month {
+			width: 20%;
+			/* height: 50%; */
+		}
+		/* height: var(--size-4); */
 		/* display: grid; */
 		/* grid-template-columns: 1fr 0.1fr; */
 		/* width: 50vw; */
