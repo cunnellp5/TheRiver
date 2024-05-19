d84f9598fb0b88adcc0f0df35fdb37d5c7e52493
cunnellp5 <philip.cunnell@colorado.edu>
2024-05-18 22:24:00 -0600

    add form for admin

--- src/routes/admin/services/time-slots/+page.svelte
+++ src/routes/admin/services/time-slots/+page.svelte
@@ -1,14 +1,21 @@
 <script>
 	import Calendar from '$lib/components/ui/Calendar.svelte';
 	import formatHours from '$lib/utils/formatHours';
+	// eslint-disable-next-line import/no-unresolved
+	import { enhance } from '$app/forms';
 
 	export let data;
 	const { timeSlots } = data;
+
+	let day = new Date().toLocaleDateString('en-CA');
+	let startTime = '09:00';
+	let endTime = '10:00';
+	let availability = 'AVAILABLE';
 </script>
 
 <h4>Services <span>&rsaquo;</span> time slots</h4>
 
-<Calendar {timeSlots} />
+<Calendar />
 
 <ul>
 	{#each timeSlots as timeSlot}
@@ -32,5 +39,52 @@
 	{/each}
 </ul>
 
+<form action="?/add" method="POST" class="form surface-4" use:enhance>
+	<div class="form-group">
+		<label for="day">Day:</label>
+		<input type="date" name="day" id="day" bind:value={day} required />
+	</div>
+	<div class="form-group">
+		<label for="startTime">Start Time:</label>
+		<input type="time" name="startTime" id="startTime" bind:value={startTime} step="900" required />
+	</div>
+	<div class="form-group">
+		<label for="endTime">End Time:</label>
+		<input type="time" name="endTime" id="endTime" bind:value={endTime} step="900" required />
+	</div>
+	<div class="form-group available">
+		<label for="availability">Availability:</label>
+		<select name="availability" id="availability" bind:value={availability} required>
+			<option value="AVAILABLE">AVAILABLE</option>
+			<option value="PENDING">PENDING</option>
+			<option value="UNAVAILABLE">UNAVAILABLE</option>
+		</select>
+	</div>
+	<button type="submit">Add Time Slot</button>
+</form>
+
 <style>
+	.form {
+		margin: 0 auto;
+		border-radius: 5px;
+		padding: var(--size-4);
+		width: 100%;
+		max-width: 600px;
+	}
+
+	.form-group {
+		margin-bottom: var(--size-4);
+	}
+
+	.form-group label {
+		display: block;
+		margin-bottom: var(--size-2);
+	}
+
+	.form-group input {
+		border: 1px solid var(--stone-10);
+		border-radius: 5px;
+		padding: var(--size-2);
+		width: 100%;
+	}
 </style>
