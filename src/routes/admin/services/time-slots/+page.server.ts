19a332ea4d6fbdc86d2f900be5423e78e0cb6a21
cunnellp5 <philip.cunnell@colorado.edu>
2024-05-18 22:23:37 -0600

    create time-slot db insert

--- src/routes/admin/services/time-slots/+page.server.ts
+++ src/routes/admin/services/time-slots/+page.server.ts
@@ -1,5 +1,5 @@
 import db from '$lib/server/database';
-import { error } from '@sveltejs/kit';
+import { error, type Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
@@ -12,5 +12,34 @@ export const load: PageServerLoad = async (event) => {
 		return { timeSlots };
 	} catch (err) {
 		return error(500, 'Internal Server Error');
+	}
+};
+
+export const actions: Actions = {
+	add: async ({ request }) => {
+		const form = await request.formData();
+
+		// TODO validate with valibot
+
+		const day = `${form.get('day')}T00:00:00.000Z`;
+
+		const timeSlot = {
+			availability: form.get('availability'),
+			day,
+			endTime: `${day.split('T')[0]}T${form.get('endTime')}:00.000Z`,
+			startTime: `${day.split('T')[0]}T${form.get('startTime')}:00.000Z`
+		};
+
+		if (!timeSlot) {
+			return error(400, 'Bad Request');
+		}
+
+		try {
+			await db.timeSlot.create({ data: timeSlot });
+			return { status: 201 };
+		} catch (err) {
+			// TODO add sentry
+			return error(500, 'Internal Server Error');
+		}
 	}
 };
