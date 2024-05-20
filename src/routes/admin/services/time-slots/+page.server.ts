33edbb5bfed614c32bf85dcc11253e81479221ff
cunnellp5 <philip.cunnell@colorado.edu>
2024-05-19 22:46:23 -0600

    trying to make the ui better

--- src/routes/admin/services/time-slots/+page.server.ts
+++ src/routes/admin/services/time-slots/+page.server.ts
@@ -2,6 +2,49 @@ import db from '$lib/server/database';
 import { error, type Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
+function generateTimeSlots(
+	day,
+	startOfDay,
+	endOfDay,
+	lunchBreakStart,
+	lunchBreakEnd,
+	interval,
+	otherBreaks
+) {
+	const slots = [];
+	let currentTime = new Date(`${day}T${startOfDay}:00`);
+	const endTime = new Date(`${day}T${endOfDay}:00`);
+	const lunchStart = new Date(`${day}T${lunchBreakStart}:00`);
+	const lunchEnd = new Date(`${day}T${lunchBreakEnd}:00`);
+
+	while (currentTime < endTime) {
+		const nextTime = new Date(currentTime.getTime() + interval * 60000);
+
+		if (
+			(currentTime >= lunchStart && currentTime < lunchEnd) ||
+			otherBreaks.some(
+				([breakStart, breakEnd]: [string, string]) =>
+					currentTime >= new Date(`${day}T${breakStart}:00`) &&
+					currentTime < new Date(`${day}T${breakEnd}:00`)
+			)
+		) {
+			currentTime = nextTime;
+			continue;
+		}
+
+		slots.push({
+			day: new Date(day).toISOString().split('T')[0],
+			startTime: currentTime,
+			endTime: nextTime,
+			availability: true
+		});
+
+		currentTime = nextTime;
+	}
+
+	return slots;
+}
+
 export const load: PageServerLoad = async (event) => {
 	if (!event.locals.session || !event.locals.user) {
 		return error(404, 'Not found');
@@ -22,31 +65,52 @@ export const load: PageServerLoad = async (event) => {
 export const actions: Actions = {
 	add: async ({ request }) => {
 		const form = await request.formData();
+		const selectedDates = form.getAll('selectedDates');
+		const startOfDay = form.get('startOfDay');
+		const endOfDay = form.get('endOfDay');
+		const lunchBreakStart = form.get('lunchBreakStart');
+		const lunchBreakEnd = form.get('lunchBreakEnd');
+		const timeSlotInterval = Number(form.get('timeSlotInterval'));
+		const otherBreaks = form
+			.get('otherBreaks')
+			?.toString()
+			.split(',')
+			.map((breakTime) =>
+				breakTime
+					.trim()
+					.split('-')
+					.map((time) => `1970-01-01T${time}:00Z`)
+			);
 
-		// Validate input
-		const availability = form.get('availability');
-		const day = form.get('day');
-		const startTime = form.get('startTime');
-		const endTime = form.get('endTime');
+		const generatedSlots = selectedDates.flatMap((date) =>
+			generateTimeSlots(
+				date,
+				startOfDay,
+				endOfDay,
+				lunchBreakStart,
+				lunchBreakEnd,
+				timeSlotInterval,
+				otherBreaks
+			)
+		);
 
-		if (!availability || !day || !startTime || !endTime) {
-			return error(400, 'Bad Request: Missing required fields');
-		}
+		console.log(generatedSlots, 'generated slots');
 
-		// Create time slot object
-		const dayString = `${day}T00:00:00.000Z`;
-		const timeSlot = {
-			availability: availability === 'true', // Assuming availability is a boolean
-			day: new Date(dayString),
-			startTime: new Date(`${day}T${startTime}:00.000Z`),
-			endTime: new Date(`${day}T${endTime}:00.000Z`)
-		};
 		try {
-			await db.timeSlot.create({ data: timeSlot });
+			await db.timeSlot.createMany({
+				data: generatedSlots.map((slot) => ({
+					day: slot.day,
+					startTime: slot.startTime,
+					endTime: slot.endTime,
+					availability: slot.availability,
+					duration: slot.endTime.getTime() - slot.startTime.getTime()
+					// Add any other fields required by your timeSlot table schema
+				}))
+			});
 			return { status: 201 };
 		} catch (err) {
-			// TODO add sentry
-			return error(500, 'Internal Server Error');
+			console.error(err);
+			return { status: 500, message: 'Internal Server Error' };
 		}
 	}
 };
--- src/routes/admin/services/time-slots/+page.svelte
+++ src/routes/admin/services/time-slots/+page.svelte
@@ -1,88 +1,128 @@
 <script lang="ts">
 	import Calendar from '$lib/components/ui/Calendar.svelte';
-	import formatHours from '$lib/utils/formatHours';
-	// eslint-disable-next-line import/no-unresolved
 	import { enhance } from '$app/forms';
+	import { onMount } from 'svelte';
 
 	export let data;
-	const { timeSlots } = data;
+	const { timeSlots, blackoutDays } = data;
 
-	let day = new Date().toLocaleDateString('en-CA');
-	let startTime = '09:00';
-	let endTime = '10:00';
-	let availability = 'AVAILABLE';
-
+	let selectedDates = [];
 	let startOfDay = '09:00';
 	let endOfDay = '18:00';
 	let timeSlotInterval = 15;
-	let lunchBreakEnd = '13:00';
 	let lunchBreakStart = '12:00';
-	let timeSlotVisual: string[];
+	let lunchBreakEnd = '13:00';
+	let otherBreaks = '';
+	let timeSlotVisual = [];
 
-	$: {
-		const start = startOfDay;
-		const end = endOfDay;
-		const interval = timeSlotInterval;
+	function handleDateSelection(dates) {
+		selectedDates = dates.map((date) => new Date(date).toLocaleDateString('en-CA'));
+		updateTimeSlots();
+	}
 
-		const startTimeV = new Date(`1970-01-01T${start}:00`);
-		const endTimeV = new Date(`1970-01-01T${end}:00`);
+	function updateTimeSlots() {
+		const startTime = new Date(`1970-01-01T${startOfDay}:00`);
+		const endTime = new Date(`1970-01-01T${endOfDay}:00`);
 
 		timeSlotVisual = [];
+		for (
+			let time = new Date(startTime);
+			time < endTime;
+			time.setMinutes(time.getMinutes() + timeSlotInterval)
+		) {
+			const formattedTime = time.toISOString().slice(11, 16);
+			const isLunchBreak =
+				new Date(`1970-01-01T${lunchBreakStart}:00`) <= time &&
+				time < new Date(`1970-01-01T${lunchBreakEnd}:00`);
+			const isOtherBreak = otherBreaks.split(',').some((b) => {
+				const [start, end] = b
+					.trim()
+					.split('-')
+					.map((t) => new Date(`1970-01-01T${t}:00`));
+				return start <= time && time < end;
+			});
 
-		for (let time = startTimeV; time <= endTimeV; time.setMinutes(time.getMinutes() + interval)) {
-			timeSlotVisual.push(time.toISOString().slice(11, 16));
+			if (!isLunchBreak && !isOtherBreak) {
+				timeSlotVisual.push(formattedTime);
+			}
 		}
 	}
+
+	onMount(() => {
+		enhance(document.querySelector('form'));
+		updateTimeSlots();
+	});
 </script>
 
-<h4>Services <span>&rsaquo;</span> time slots</h4>
+<div class="container">
+	<h4>Services <span>&rsaquo;</span> Time Slots</h4>
 
-<Calendar />
+	<Calendar on:select={handleDateSelection} />
 
-<h2>Existing Time Slots</h2>
-<table>
-	<thead>
-		<tr>
-			<th>Day</th>
-			<th>Start Time</th>
-			<th>End Time</th>
-			<th>Availability</th>
-		</tr>
-	</thead>
-	<tbody>
-		{#each timeSlots as slot}
+	<h2>Existing Time Slots</h2>
+	<table>
+		<thead>
 			<tr>
-				<td>{new Date(slot.day).toLocaleDateString()}</td>
-				<td>{new Date(slot.startTime).toLocaleTimeString()}</td>
-				<td>{new Date(slot.endTime).toLocaleTimeString()}</td>
-				<td>{slot.availability ? 'Available' : 'Unavailable'}</td>
+				<th>Day</th>
+				<th>Start Time</th>
+				<th>End Time</th>
+				<th>Availability</th>
 			</tr>
-		{/each}
-	</tbody>
-</table>
+		</thead>
+		<tbody>
+			{#each timeSlots as slot}
+				<tr>
+					<td>{new Date(slot.day).toLocaleDateString()}</td>
+					<td
+						>{new Date(slot.startTime).toLocaleTimeString([], {
+							hour: '2-digit',
+							minute: '2-digit'
+						})}</td>
+					<td
+						>{new Date(slot.endTime).toLocaleTimeString([], {
+							hour: '2-digit',
+							minute: '2-digit'
+						})}</td>
+					<td>{slot.availability ? 'Available' : 'Unavailable'}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
 
-<form action="?/add" method="POST" class="form surface-4" use:enhance>
-	<div class="form-group">
-		<label for="day">Day:</label>
-		<input type="date" name="day" id="day" bind:value={day} required />
-	</div>
-	<div class="form-group">
-		<label for="startOfDay">Start of Day:</label>
-		<input
-			type="time"
-			name="startOfDay"
-			id="startOfDay"
-			bind:value={startOfDay}
-			step="900"
-			required />
-	</div>
+	<form action="?/add" method="POST" class="form" use:enhance>
+		<div class="form-group">
+			<label for="selectedDates">Selected Dates:</label>
+			<input type="hidden" name="selectedDates" value={selectedDates.join(',')} />
+			<ul>
+				{#each selectedDates as date}
+					<li>{date}</li>
+				{/each}
+			</ul>
+		</div>
+		<div class="form-group">
+			<label for="startOfDay">Start of Day:</label>
+			<input
+				type="time"
+				name="startOfDay"
+				id="startOfDay"
+				bind:value={startOfDay}
+				step="900"
+				required
+				on:change={updateTimeSlots} />
+		</div>
 
-	<div class="form-group">
-		<label for="endOfDay">End of Day:</label>
-		<input type="time" name="endOfDay" id="endOfDay" bind:value={endOfDay} step="900" required />
-	</div>
+		<div class="form-group">
+			<label for="endOfDay">End of Day:</label>
+			<input
+				type="time"
+				name="endOfDay"
+				id="endOfDay"
+				bind:value={endOfDay}
+				step="900"
+				required
+				on:change={updateTimeSlots} />
+		</div>
 
-	<div class="lunchWrapper">
 		<div class="form-group">
 			<label for="lunchBreakStart">Lunch Break Start:</label>
 			<input
@@ -90,7 +130,8 @@
 				name="lunchBreakStart"
 				id="lunchBreakStart"
 				bind:value={lunchBreakStart}
-				required />
+				required
+				on:change={updateTimeSlots} />
 		</div>
 
 		<div class="form-group">
@@ -100,63 +141,145 @@
 				name="lunchBreakEnd"
 				id="lunchBreakEnd"
 				bind:value={lunchBreakEnd}
-				required />
+				required
+				on:change={updateTimeSlots} />
 		</div>
+
+		<div class="form-group">
+			<label for="timeSlotInterval">Time Slot Interval (15 minutes recommended):</label>
+			<input
+				type="number"
+				name="timeSlotInterval"
+				id="timeSlotInterval"
+				bind:value={timeSlotInterval}
+				required
+				min="15"
+				step="15"
+				on:change={updateTimeSlots} />
+		</div>
+
+		<div class="form-group">
+			<label for="otherBreaks">Other Breaks (comma separated times in HH:mm-HH:mm format):</label>
+			<input
+				type="text"
+				name="otherBreaks"
+				id="otherBreaks"
+				bind:value={otherBreaks}
+				on:change={updateTimeSlots} />
+		</div>
+
+		<button type="submit">Generate Time Slots</button>
+	</form>
+
+	<div class="time-slot-visual">
+		<h3>Time Slot Preview</h3>
+		{#each timeSlotVisual as timeSlot (timeSlot)}
+			<div class="time-slot">{timeSlot}</div>
+		{/each}
 	</div>
-
-	<div class="form-group">
-		<label for="timeSlotInterval">Time Slot Interval (15 minutes recommended):</label>
-		<input
-			type="number"
-			name="timeSlotInterval"
-			id="timeSlotInterval"
-			bind:value={timeSlotInterval}
-			required
-			min="15"
-			step="15" />
-	</div>
-
-	<div class="form-group">
-		<label for="otherBreaks">Other Breaks (comma separated times in HH:mm-HH:mm format):</label>
-		<input type="text" name="otherBreaks" id="otherBreaks" />
-	</div>
-
-	<button type="submit">Generate Time Slots</button>
-</form>
-
-<div class="time-slot-visual">
-	{#each timeSlotVisual as timeSlot (timeSlot)}
-		<div class="time-slot">{timeSlot}</div>
-	{/each}
 </div>
 
 <style>
-	.form {
-		margin: 0 auto;
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		border-radius: 10px;
+		background-color: #1a1a1a;
+		padding: 2rem;
+		color: #f0f0f0;
+	}
+
+	h4 {
+		margin-bottom: 1rem;
+	}
+
+	table {
+		margin-top: 1rem;
+		border-collapse: collapse;
+		width: 100%;
+	}
+
+	th,
+	td {
+		border: 1px solid #555;
+		padding: 0.5rem;
+		text-align: left;
+	}
+
+	th {
+		background-color: #333;
+	}
+
+	.time-slot-visual {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 0.5rem;
+		margin-top: 1rem;
+	}
+
+	.time-slot {
+		border: 1px solid #555;
 		border-radius: 5px;
-		padding: var(--size-4);
+		background-color: #333;
+		padding: 0.5rem;
+	}
+
+	.form {
+		margin-top: 2rem;
+		border-radius: 10px;
+		background-color: #2a2a2a;
+		padding: 1rem;
 		width: 100%;
 		max-width: 600px;
 	}
 
 	.form-group {
-		margin-bottom: var(--size-4);
+		margin-bottom: 1rem;
 	}
 
 	.form-group label {
 		display: block;
-		margin-bottom: var(--size-2);
+		margin-bottom: 0.5rem;
 	}
 
-	.form-group input {
-		border: 1px solid var(--stone-10);
+	.form-group input,
+	.form-group select {
+		border: 1px solid #555;
 		border-radius: 5px;
-		padding: var(--size-2);
+		background-color: #3a3a3a;
+		padding: 0.5rem;
+		width: 100%;
+		color: #f0f0f0;
+	}
+
+	.form-group input[type='time'] {
+		display: inline-block;
+		margin-right: 1rem;
+		width: calc(50% - 1rem);
+	}
+
+	.form-group input[type='time']:last-child {
+		margin-right: 0;
+	}
+
+	.form-group input[type='number'] {
 		width: 100%;
 	}
 
-	.lunchWrapper {
-		display: flex;
-		justify-content: space-evenly;
+	button {
+		display: block;
+		cursor: pointer;
+		border: none;
+		border-radius: 5px;
+		background-color: #4a90e2;
+		padding: 0.75rem;
+		width: 100%;
+		color: #fff;
+		font-size: 1rem;
+	}
+
+	button:hover {
+		background-color: #357ab8;
 	}
 </style>
