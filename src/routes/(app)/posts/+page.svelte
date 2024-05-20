a120c26facebf575561e64345f0ee55e2379dd77
cunnellp5 <philip.cunnell@colorado.edu>
2024-05-20 13:59:49 -0600

    clean up

--- src/routes/(app)/posts/+page.svelte
+++ src/routes/(app)/posts/+page.svelte
@@ -5,12 +5,6 @@
 
 	export let data: PageData;
 	const { posts } = data;
-	// let toggleClass = false;
-
-	// function toggler() {
-	// 	toggleClass = !toggleClass;
-	// 	return toggleClass;
-	// }
 </script>
 
 <main>
@@ -29,12 +23,7 @@
 			</div>
 		{/if}
 	</section>
-	<!-- {#if toggleClass} -->
-	<!-- <div class:showMe={toggleClass} class="single">test</div> -->
-	<!-- {/if} -->
 </main>
-
-<!-- This section should be using the perspective thing to show single posts, and keep the list of posts in another column, so that the single post view is basically emulated with fewer steps: https://hackernoon.com/how-to-build-a-masonry-layout-using-css -->
 
 <style>
 	ul {
@@ -42,11 +31,9 @@
 		flex-direction: column;
 		gap: var(--size-4);
 	}
-
 	.noPostsWrapper {
 		margin-block: var(--size-content-1);
 	}
-
 	@media (max-width: 768px) {
 		main {
 			margin-inline: 0;
