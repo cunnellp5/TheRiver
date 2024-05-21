35a9d34bb41507279712dea019bdb1dcae1a3098
cunnellp5 <philip.cunnell@colorado.edu>
2024-05-21 14:28:20 -0600

    tweaks

--- src/routes/(app)/contact/+page.svelte
+++ src/routes/(app)/contact/+page.svelte
@@ -21,56 +21,78 @@
 	});
 </script>
 
-<main>
+<h1>Information</h1>
+<p>TODO add a map</p>
+<div class="main-page-wrapper">
+	<!-- TODO put this video in cloudinary -->
+	<video autoplay muted loop playsinline class="background-video">
+		<source src="water.mp4" type="video/mp4" />
+		Your browser does not support HTML5 video.
+	</video>
 	<section>
-		<!-- <video autoplay muted loop playsinline class="background-video">
-			<source src="water.mp4" type="video/mp4" />
-			Your browser does not support HTML5 video.
-		</video> -->
-		<h1>Contact</h1>
-		<p>theriverrunsfast@gmail.com</p>
-		<aside class="buttons">
-			<button on:click={copyEmail}>
-				{#if copied}
-					<a>
-						<Check size="36" />
+		<div>
+			<h2>The River</h2>
+			<p>Located in Denver, CO</p>
+		</div>
+		<div>
+			<h2>NEWSLETTER</h2>
+			<form action="">
+				<label for="email">Sign up to receive emails from The River</label>
+				<div class="newsletter-form">
+					<input type="email" name="email" placeholder="test@example.com" />
+					<button title="Send email">
+						<a>
+							<Send />
+						</a>
+					</button>
+				</div>
+			</form>
+		</div>
+		<div>
+			<h2>CONTACT</h2>
+			<p>theriverrunsfast@gmail.com</p>
+			<aside class="buttons">
+				<button on:click={copyEmail} title="Copy email to clipboard">
+					{#if copied}
+						<a>
+							<Check size="24" />
+						</a>
+					{:else}
+						<a>
+							<Copy size="24" />
+						</a>
+					{/if}
+				</button>
+				<button title="Send email">
+					<a href="mailto:{email}">
+						<Send size="24" />
 					</a>
-				{:else}
-					<a>
-						<Copy size="36" />
-					</a>
-				{/if}
-			</button>
-			<button>
-				<a href="mailto:{email}">
-					<Send size="36" />
-				</a>
-			</button>
-		</aside>
+				</button>
+			</aside>
+		</div>
 	</section>
-</main>
+</div>
 
 <style>
-	main {
-		display: flex;
-		flex-direction: column;
-		/* justify-content: center; */
-		align-items: center;
-		margin-block: var(--size-9);
-		/* height: 100vh; */
-	}
-
 	section {
 		display: flex;
-		/* position: relative; */
 		flex-direction: column;
-		/* justify-content: center; */
+		justify-content: center;
 		/* align-items: center; */
-		/* box-shadow: var(--shadow-1); */
-		/* padding: var(--size-7); */
+		gap: var(--size-8);
+		margin-inline-start: var(--size-4);
+		margin: 0 auto;
+		border-radius: var(--radius-1);
+		background-color: hsl(var(--cyan-6-hsl) / 85%);
+		padding: var(--size-4);
+		width: fit-content;
+		/* color: black; */
 	}
-
-	h1 {
+	section > div {
+		margin-inline: var(--size-8);
+	}
+	h1,
+	h2 {
 		text-transform: uppercase;
 	}
 
@@ -78,18 +100,50 @@
 		letter-spacing: var(--font-letterspacing-3);
 	}
 
+	video {
+		box-shadow: var(--shadow-1);
+		border-radius: var(--radius-4);
+		/* inline-size: var(--size-5); */
+	}
+	/* .main-page-wrapper {
+		display: flex;
+		flex-direction: row-reverse;
+		justify-content: center;
+	} */
 	.buttons {
 		display: flex;
-		flex-direction: column;
+		flex-direction: row;
 		gap: var(--size-4);
-		margin-block-start: var(--size-6);
+		margin-block-start: var(--size-2);
 	}
-	/* 
-	.background-video {
+
+	/* .background-video {
 		position: relative;
+		z-index: -1;
+		object-fit: cover;
+	} */
+
+	.main-page-wrapper {
+		position: relative;
+	}
+
+	.background-video {
+		position: absolute;
+		top: 0;
+		left: 0;
 		z-index: -1;
 		width: 100%;
 		height: 100%;
 		object-fit: cover;
-	} */
+	}
+
+	section {
+		position: relative;
+	}
+
+	.newsletter-form {
+		display: flex;
+		gap: var(--size-2);
+		margin-block-start: var(--size-2);
+	}
 </style>
