c7a5c0d3836aa982ddd4ee0b81c39c1779e3a45c
cunnellp5 <philip.cunnell@colorado.edu>
2024-05-26 21:46:15 -0600

    get some calendar help!

--- src/routes/admin/services/time-slots/+page.svelte
+++ src/routes/admin/services/time-slots/+page.svelte
@@ -1,223 +1,104 @@
 <script lang="ts">
-	import Calendar from '$lib/components/ui/Calendar.svelte';
-	import { enhance } from '$app/forms';
-	import { onMount } from 'svelte';
-	import { writable } from 'svelte/store';
+	import Calendar from '@event-calendar/core';
+	import DayGrid from '@event-calendar/day-grid';
+	import List from '@event-calendar/list';
+	import TimeGrid from '@event-calendar/time-grid';
+	import Interaction from '@event-calendar/interaction';
+	import '@event-calendar/core/index.css';
+	import { theme } from '$lib/stores/theme';
+
+	$: currentTheme = $theme;
+
+	const plugins = [DayGrid, List, TimeGrid, Interaction];
+	const options = {
+		events: [
+			// your list of events
+		],
+		// slotDuration: '00:15do:00',
+		slotLabelFormat: {
+			hour: 'numeric',
+			minute: '2-digit',
+			meridiem: 'short'
+		},
+		headerToolbar: {
+			start: 'title dayGridMonth,timeGridWeek,timeGridDay listYear,listMonth,listWeek,listDay',
+			center: '',
+			end: 'today prev,next'
+		},
+		buttonText: {
+			listDay: 'listDay',
+			listYear: 'listYear',
+			listMonth: 'listMonth',
+			listWeek: 'listWeek',
+			dayGridMonth: 'month',
+			timeGridWeek: 'week',
+			timeGridDay: 'day',
+			today: 'today'
+		}
+	};
+	// import Calendar from '$lib/components/ui/Calendar.svelte';
+	// import { enhance } from '$app/forms';
+	// import { onMount } from 'svelte';
+	// import { writable } from 'svelte/store';
+
+	const STEP_SIZE = 900; // this is in ms, so 900 is 15 minutes
 
 	export let data;
 	const { timeSlots, blackoutDays } = data;
 
 	const days = ['Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat', 'Sun'];
-	let availability = writable([]);
+	// let availability = writable([]);
 
-	function addAvailability(day, startTime, endTime) {
-		availability.update((current) => [...current, { day, startTime, endTime }]);
-	}
-	let selectedDates = [];
-	let startOfDay = '09:00';
-	let endOfDay = '18:00';
-	let startTime = '09:00';
-	let endTime = '18:00';
+	// function addAvailability(day, startTime, endTime) {
+	// 	availability.update((current) => [...current, { day, startTime, endTime }]);
+	// }
+	// let selectedDates = [];
+	// let startOfDay = '09:00';
+	// let endOfDay = '18:00';
+	// let startTime = '09:00';
+	// let endTime = '18:00';
 
-	function handleDateSelection(dates) {
-		selectedDates = dates.map((date) => new Date(date).toLocaleDateString('en-CA'));
-	}
+	// function handleDateSelection(dates) {
+	// 	selectedDates = dates.map((date) => new Date(date).toLocaleDateString('en-CA'));
+	// }
+
+	const availability = days.map((day) => ({
+		day,
+		startOfDay: '09:00',
+		endOfDay: '18:00',
+		breaks: [],
+		buffer: 15 // default buffer time in minutes
+	}));
+
+	const addBreak = (index: number) => {
+		availability[index].breaks.push({ start: '', end: '' });
+		console.log(availability);
+	};
+
+	const removeBreak = (dayIndex: number, breakIndex: number) => {
+		availability[dayIndex].breaks.splice(breakIndex, 1);
+		console.log(availability);
+	};
 </script>
 
-<div class="container">
-	<h4>Services <span>&rsaquo;</span> Time Slots</h4>
+<div
+	class="container ec-dark"
+	class:ec-dark={currentTheme === 'dark'}
+	class:ec-light={currentTheme === 'light'}>
+	<h4>Schedule</h4>
 
-	<Calendar />
+	<p>This generates time slots on the backend in 15 min chunks</p>
 
-	<!-- <h2>Existing Time Slots</h2>
-	<table>
-		<thead>
-			<tr>
-				<th>Day</th>
-				<th>Start Time</th>
-				<th>End Time</th>
-				<th>Availability</th>
-			</tr>
-		</thead>
-		<tbody>
-			{#each timeSlots as slot}
-				<tr>
-					<td>{new Date(slot.day).toLocaleDateString()}</td>
-					<td
-						>{new Date(slot.startTime).toLocaleTimeString([], {
-							hour: '2-digit',
-							minute: '2-digit'
-						})}</td>
-					<td
-						>{new Date(slot.endTime).toLocaleTimeString([], {
-							hour: '2-digit',
-							minute: '2-digit'
-						})}</td>
-					<td>{slot.availability ? 'Available' : 'Unavailable'}</td>
-				</tr>
-			{/each}
-		</tbody>
-	</table> -->
+	<pre>
+		1 - select a day or many days
+		2 - for each of those days, select the start and end time
+		3 - add breaks if needed
+		4 - add buffer time between each time slot
+	</pre>
 
-	<form action="?/add" method="POST" class="form" use:enhance>
-		<div class="form-group">
-			<label for="selectedDates">Selected Dates:</label>
-			<input type="hidden" name="selectedDates" value={selectedDates.join(',')} />
-			<ul>
-				{#each selectedDates as date}
-					<li>{date}</li>
-				{/each}
-			</ul>
-		</div>
-		<div class="form-group">
-			<label for="startOfDay">Start of Day:</label>
-			<input
-				type="time"
-				name="startOfDay"
-				id="startOfDay"
-				bind:value={startOfDay}
-				step="900"
-				required />
-		</div>
-
-		<div class="form-group">
-			<label for="endOfDay">End of Day:</label>
-			<input type="time" name="endOfDay" id="endOfDay" bind:value={endOfDay} step="900" required />
-		</div>
-
-		{#each days as day}
-			<div class="form-group days-forms-wrapper">
-				<label>{day}</label>
-				<input type="time" bind:value={startTime} step="900" placeholder="Start Time" />
-				<input type="time" bind:value={endTime} step="900" placeholder="End Time" />
-				<button type="button" on:click={() => addAvailability(day, startTime.value, endTime.value)}>
-					Add
-				</button>
-			</div>
-		{/each}
-
-		<button type="submit">Generate Time Slots</button>
-	</form>
+	<Calendar {plugins} {options} />
+	<!-- <form action="?/add" method="POST" class="form" use:enhance></form> -->
 </div>
 
 <style>
-	.container {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		border-radius: 10px;
-		background-color: #1a1a1a;
-		padding: 2rem;
-		color: #f0f0f0;
-	}
-
-	h4 {
-		margin-bottom: 1rem;
-	}
-
-	.days-forms-wrapper {
-		display: flex;
-		flex-direction: row;
-		align-items: center;
-		/* & div {
-			display: flex;
-			flex-direction: row;
-		} */
-		& label {
-			width: 20%;
-		}
-		& button {
-			width: 20%;
-		}
-	}
-
-	table {
-		margin-top: 1rem;
-		border-collapse: collapse;
-		width: 100%;
-	}
-
-	th,
-	td {
-		border: 1px solid #555;
-		padding: 0.5rem;
-		text-align: left;
-	}
-
-	th {
-		background-color: #333;
-	}
-
-	.time-slot-visual {
-		display: flex;
-		flex-direction: column;
-		flex-wrap: wrap;
-		gap: 0.5rem;
-		margin-top: 1rem;
-	}
-
-	.time-slot {
-		border: 1px solid #555;
-		border-radius: 5px;
-		background-color: #333;
-		padding: 0.5rem;
-	}
-
-	.form {
-		margin-top: 2rem;
-		border-radius: 10px;
-		background-color: #2a2a2a;
-		padding: 1rem;
-		width: 100%;
-		max-width: 600px;
-	}
-
-	.form-group {
-		margin-bottom: 1rem;
-	}
-
-	.form-group label {
-		display: block;
-		margin-bottom: 0.5rem;
-	}
-
-	.form-group input,
-	.form-group select {
-		border: 1px solid #555;
-		border-radius: 5px;
-		background-color: #3a3a3a;
-		padding: 0.5rem;
-		width: 100%;
-		color: #f0f0f0;
-	}
-
-	.form-group input[type='time'] {
-		display: inline-block;
-		margin-right: 1rem;
-		width: calc(50% - 1rem);
-	}
-
-	.form-group input[type='time']:last-child {
-		margin-right: 0;
-	}
-
-	.form-group input[type='number'] {
-		width: 100%;
-	}
-
-	button {
-		display: block;
-		cursor: pointer;
-		border: none;
-		border-radius: 5px;
-		background-color: #4a90e2;
-		padding: 0.75rem;
-		width: 100%;
-		color: #fff;
-		font-size: 1rem;
-	}
-
-	button:hover {
-		background-color: #357ab8;
-	}
 </style>
