61331fc12b4891f87413c004e29bd9c04fda09e8
cunnellp5 <philip.cunnell@colorado.edu>
2024-06-04 17:22:02 -0600

    render events and update modal form

--- src/routes/admin/services/time-slots/+page.svelte
+++ src/routes/admin/services/time-slots/+page.svelte
@@ -7,39 +7,59 @@
 	import Interaction from '@event-calendar/interaction';
 	import List from '@event-calendar/list';
 	import TimeGrid from '@event-calendar/time-grid';
-	import { createEvents, configOptions } from './calendarConfig';
+	import { configOptions } from './calendarConfig';
+	import { ViewIcon } from 'lucide-svelte';
 
 	export let data;
 
 	const { timeSlots, blackoutDays } = data;
 
-	$: showModal = false;
+	let eventStart: string = '';
+	let eventEnd: string = '';
+	let eventTitle: string = '';
+
+	let eventInfo;
+
+	let showModal = false;
+	let ec: Calendar;
 
 	const toggleModal = () => {
 		showModal = !showModal;
 	};
 
-	let ec: Calendar;
+	function handleClose() {
+		showModal = false;
+
+		eventStart = eventInfo.startStr;
+		eventEnd = eventInfo.endStr;
+
+		console.log(eventStart, 'test');
+
+		// adds event to calendar, vars are being updated from modal inputs
+		ec.addEvent({
+			start: eventStart,
+			end: eventEnd,
+			display: 'auto',
+			resourceId: 1,
+			title: eventTitle,
+			editable: true,
+			durationEditable: true,
+			startEditable: true,
+			constraint: 'businessHours',
+			extendedProps: {
+				availability: true
+			}
+		});
+	}
 
 	const plugins = [DayGrid, List, TimeGrid, Interaction];
 
 	const options = {
 		...configOptions,
 		select(info) {
-			ec.addEvent({
-				start: info.startStr,
-				end: info.endStr,
-				display: 'auto',
-				resourceId: 1,
-				title: 'Available',
-				editable: true,
-				durationEditable: true,
-				startEditable: true,
-				constraint: 'businessHours',
-				extendedProps: {
-					availability: true
-				}
-			});
+			eventStart = info.startStr;
+			eventEnd = info.endStr;
+			eventInfo = { ...info };
 			console.log(info, 'select');
 			// render modal
 			toggleModal();
@@ -63,15 +83,61 @@
 	};
 </script>
 
-<Modal bind:showModal overrideButtons={true}>
-	<h2 slot="header">Modal test</h2>
+<Modal bind:showModal on:close={handleClose} buttonText="Save">
+	<h2 slot="header">Save availability</h2>
 
-	<div slot="buttons" class="buttons">
-		<button
-			on:click={() => {
-				showModal = false;
-			}}>close</button>
+	<div class="form-group">
+		<label for="eventTitle">Title</label>
+		<input
+			placeholder="Title of event"
+			name="eventTitle"
+			type="text"
+			id="eventTitle"
+			bind:value={eventTitle} />
 	</div>
+	<!-- <div>
+			<label for="eventStart">Start</label>
+			<input type="text" id="eventStart" bind:value={eventStart} />
+		</div>
+
+		<div>
+			<label for="eventEnd">End</label>
+			<input type="text" id="eventEnd" bind:value={eventEnd} />
+		</div> -->
+
+	<!-- <div>
+			<span>repeat for:</span>
+			<div class="weekdays">
+				<div class="day">
+					<label for="monday">Monday</label>
+					<input type="checkbox" id="monday" />
+				</div>
+				<div class="day">
+					<label for="tuesday">Tuesday</label>
+					<input type="checkbox" id="tuesday" />
+				</div>
+				<div class="day">
+					<label for="wednesday">Wednesday</label>
+					<input type="checkbox" id="wednesday" />
+				</div>
+				<div class="day">
+					<label for="thursday">Thursday</label>
+					<input type="checkbox" id="thursday" />
+				</div>
+				<div class="day">
+					<label for="friday">Friday</label>
+					<input type="checkbox" id="friday" />
+				</div>
+				<div class="day">
+					<label for="saturday">Saturday</label>
+					<input type="checkbox" id="saturday" />
+				</div>
+				<div class="day">
+					<label for="sunday">Sunday</label>
+					<input type="checkbox" id="sunday" />
+				</div>
+			</div>
+		</div> -->
 </Modal>
 
 <div class="container" class:ec-dark={$theme === 'dark'} class:ec-light={$theme === 'light'}>
@@ -91,6 +157,8 @@
 	<!-- <form action="?/add" method="POST" class="form" use:enhance></form> -->
 </div>
 
+<!-- deleting https://github.com/vkurko/calendar/discussions/91 -->
+
 <style>
 	.calendar-wrapper {
 		max-width: 70vw;
@@ -101,6 +169,14 @@
 			background-clip: unset;
 			color: var(--text-1);
 			font-size: var(--font-size-1);
+		}
+	}
+
+	.form-group {
+		display: flex;
+		flex-direction: column;
+		& label {
+			margin-bottom: var(--size-1);
 		}
 	}
 </style>
