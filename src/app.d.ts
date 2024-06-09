a6e0dfb55b7c8a3f09aa6da9cd7604d73fdd2a1b
cunnellp5 <philip.cunnell@colorado.edu>
2024-06-09 08:36:08 -0600

    adding types and making them available to frontend TS files

--- src/app.d.ts
+++ src/app.d.ts
@@ -11,5 +11,40 @@ declare global {
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
+
+		interface Info {
+			start: Date;
+			end: Date;
+			startStr: string; // ISO8601 string representation of the start date
+			endStr: string; // ISO8601 string representation of the end date
+			allDay: boolean;
+			jsEvent: Event;
+			view: View;
+			resource: Resource;
+		}
+
+		interface View {
+			type:
+				| 'dayGridMonth'
+				| 'dayGridWeek'
+				| 'dayGridDay'
+				| 'timeGridWeek'
+				| 'timeGridDay'
+				| 'listWeek'
+				| 'listDay'
+				| 'listMonth';
+			title: string;
+			currentStart: Date;
+			currentEnd: Date;
+			activeStart: Date;
+			activeEnd: Date;
+		}
+
+		interface Resource {
+			id: string;
+			title: string;
+			eventBackgroundColor: string;
+			eventTextColor: string;
+		}
 	}
 }
---
+++ src/lib/types.ts
@@ -0,0 +1,3 @@
+export type Info = App.Info;
+export type View = App.View;
+export type Resource = App.Resource;
