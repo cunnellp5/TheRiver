815564b05b02664eeb52e3ce93b5fdd33741577f
cunnellp5 <philip.cunnell@colorado.edu>
2024-07-05 19:40:39 -0600

    render users and conditionally color the admin col

--- src/routes/admin/users/+page.svelte
+++ src/routes/admin/users/+page.svelte
@@ -1,1 +1,53 @@
-<h2>view all users here</h2>
+<script lang="ts">
+	export let data;
+
+	const { users } = data;
+</script>
+
+<h3>All users</h3>
+
+<table>
+	<thead>
+		<tr>
+			<th>count</th>
+			<th>Email</th>
+			<th>firstName</th>
+			<th>lastName</th>
+			<th>isAdmin</th>
+			<th>isSubscribed</th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each users as user, id}
+			<tr>
+				<td>{id + 1}</td>
+				<td>{user.email}</td>
+				<td>{user.firstName}</td>
+				<td>{user.lastName}</td>
+				<td class:admin={user.isAdmin} class:defaultUser={!user.isAdmin}>{user.isAdmin}</td>
+				<td>{user.isSubscribed}</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
+
+<style>
+	table {
+		width: 100%;
+	}
+
+	td {
+		padding: 0.5rem;
+	}
+
+	tr:nth-child(even) {
+		background-color: #f9f9f9;
+	}
+
+	.defaultUser {
+		background-color: var(--teal-12);
+	}
+	.admin {
+		background-color: var(--pink-12);
+	}
+</style>
