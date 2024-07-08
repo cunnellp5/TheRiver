53a1a918e72550a2f9a8347fc75691286928ab48
cunnellp5 <philip.cunnell@colorado.edu>
2024-07-08 10:18:42 -0600

    update seeds

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -94,12 +94,54 @@ async function main() {
 
 	await db.timeSlot.create({
 		data: {
-			startTime: new Date('2024-05-05T13:00:00Z'),
-			endTime: new Date('2024-05-05T14:00:00Z'),
-			day: new Date('2024-05-05T19:00:00.000Z'),
+			startTime: new Date('2024-07-07T13:00:00Z'),
+			endTime: new Date('2024-07-07T14:00:00Z'),
+			day: new Date('2024-07-07T19:00:00.000Z'),
 			available: true,
 			serviceId: 1
 		}
+	});
+
+	await db.article.createMany({
+		data: [
+			{
+				articleImgUrl: 'https://res.cloudinary.com/dswpu3qez/image/upload/v1707412331/sample.jpg',
+				contentMessage:
+					'Ever since releasing her first song in June 2021, she has had her foot on the gas.',
+				contentAuthor: 'Victória Santiago',
+				linkUrl:
+					'https://ratingsgamemusic.com/2022/01/08/the-river-delivers-angry-verses-on-hours/',
+				linkTitle: 'Victória Santiago [Ratings Game Music Blogger]'
+			},
+			{
+				articleImgUrl:
+					'https://res.cloudinary.com/dswpu3qez/image/upload/v1707412341/samples/people/jazz.jpg',
+				contentMessage:
+					"On Hours: The production here is sublime and crisp as each instrument stands out with clarity while The River's commanding vocals cut through the mix with ease.",
+				contentAuthor: 'TAYO ODUTOLA',
+				linkUrl:
+					'https://earmilk.com/2022/01/10/the-rivers-hours-digs-deep-into-the-dynamics-of-failed-relationships/',
+				linkTitle: 'Earmilk'
+			},
+			{
+				articleImgUrl:
+					'https://res.cloudinary.com/dswpu3qez/image/upload/v1707412345/samples/ecommerce/accessories-bag.jpg',
+				contentMessage:
+					"The vocals are confidently delivered, imbued with the effortless coolness that comes with being insanely naturally gifted. Plus, the arrangement and production create the most infectious groove you'll ever hear.",
+				contentAuthor: 'Alyce Lindberg',
+				linkUrl: 'https://musicfashionblog.com/it-aint-that-deep-the-river/',
+				linkTitle: 'Read more'
+			},
+			{
+				articleImgUrl:
+					'https://res.cloudinary.com/dswpu3qez/image/upload/v1707412364/samples/man-portrait.jpg',
+				contentMessage:
+					"Somewhere in the lyrics there is peace and truth. The River's latest single “Hours” facing the reality of love, respect, and emotions. Over and acoustic melody the rain pours over the lyrical expressions of facing your feelings.",
+				contentAuthor: 'Kreadiv Kesh',
+				linkUrl: 'EXPIRED_URL', // https://www.laonlock.com/reviews/1/11/2022/the-river-hours-review
+				linkTitle: 'Kreadiv Kesh [LA On Lock Blogger]'
+			}
+		]
 	});
 }
 
