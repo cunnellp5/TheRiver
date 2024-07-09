ca31f93434638c97d5ee689ed0c8f05b4b7d1b7b
cunnellp5 <philip.cunnell@colorado.edu>
2024-07-09 17:22:54 -0600

    render cards in admin portal

--- src/routes/admin/homepage/articles/+page.svelte
+++ src/routes/admin/homepage/articles/+page.svelte
@@ -1,1 +1,29 @@
+<script lang="ts">
+	import Card from '$lib/components/ui/Card.svelte';
+
+	export let data;
+
+	const { articles } = data;
+</script>
+
 <pre>this page CRUDs homepage articles</pre>
+
+<section>
+	{#each articles as article}
+		<Card
+			articleImage={article.articleImgUrl}
+			articleTitle={article.linkTitle}
+			message={article.contentMessage}
+			link={article.linkUrl}>
+		</Card>
+	{/each}
+</section>
+
+<style>
+	section {
+		display: flex;
+		grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+		flex-flow: row wrap;
+		gap: var(--size-6);
+	}
+</style>
