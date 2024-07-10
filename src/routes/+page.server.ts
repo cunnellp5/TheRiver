399575bd4a832be25b55a755d8dd3c9c06ba26b7
cunnellp5 <philip.cunnell@colorado.edu>
2024-07-10 10:08:15 -0600

    pull articles from db

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,22 @@
 import { error } from '@sveltejs/kit';
+import db from '$lib/server/database';
 import { env } from '$env/dynamic/private';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
 	// TODO fetch url from cloudinary figure out media
 	const videoURL = env.VIDEO_URL;
+	let articles = null;
 
 	if (!videoURL) {
 		error(404, 'Video not found');
 	}
 
-	return { videoURL };
+	try {
+		articles = await db.article.findMany();
+	} catch (err) {
+		return error(500, 'Internal Server Error');
+	}
+
+	return { videoURL, articles };
 };
