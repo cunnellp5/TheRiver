a022a05789aa29545289bea5daa0a7278edbc019
cunnellp5 <philip.cunnell@colorado.edu>
2024-07-12 17:25:08 -0600

    create dummy pages

---
+++ src/routes/admin/homepage/articles/add/+page.svelte
@@ -0,0 +1,5 @@
+<h4>put an article CREATE form here</h4>
+
+<form method="post" action="?/createArticle">
+	<button>Create Article</button>
+</form>
---
+++ src/routes/admin/homepage/articles/edit/+page.svelte
@@ -0,0 +1,5 @@
+<h4>put an article EDIT form here dood</h4>
+
+<form method="post" action="?/createArticle">
+	<button>Create Article</button>
+</form>
