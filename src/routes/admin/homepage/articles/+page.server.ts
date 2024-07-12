cc002b0f7640f2a911783f1d42914bcaa2a45aba
cunnellp5 <philip.cunnell@colorado.edu>
2024-07-12 17:24:51 -0600

    scaffold out an action for creating a new article

--- src/routes/admin/homepage/articles/+page.server.ts
+++ src/routes/admin/homepage/articles/+page.server.ts
@@ -1,6 +1,6 @@
-import { error } from '@sveltejs/kit';
+import { error, redirect, fail } from '@sveltejs/kit';
 import db from '$lib/server/database';
-import type { PageServerLoad } from './$types';
+import type { PageServerLoad, Actions } from './$types';
 
 export const load: PageServerLoad = async (event) => {
 	if (!event.locals.session || !event.locals.user) {
@@ -12,5 +12,37 @@ export const load: PageServerLoad = async (event) => {
 		return { articles };
 	} catch (err) {
 		return error(500, 'Internal Server Error');
+	}
+};
+
+export const actions: Actions = {
+	createArticle: async (event) => {
+		console.log(event);
+		// if (!event.locals.session) {
+		// 	redirect(302, '/login');
+		// }
+		const formData = await event.request.formData();
+		console.log(formData, 'formdatum');
+		// const title = formData.get('title');
+		// const url = formData.get('url');
+		// // TODO figure out how to upload icons or svgs?
+
+		// if (typeof title !== 'string' || title.length < 3 || title.length > 255) {
+		// 	return fail(400, {
+		// 		message: 'Invalid title'
+		// 	});
+		// }
+
+		// const link = await db.article.create({
+		// 	data: {
+		// 		title,
+		// 		url: url?.toString() || ''
+		// 	}
+		// });
+
+		// return {
+		// 	message: `Link to ${link.title} created`,
+		// 	socialLink: link
+		// };
 	}
 };
