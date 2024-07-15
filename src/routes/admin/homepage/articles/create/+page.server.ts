4e9313ad5db4b4ac89c0806dc210effffc272977
cunnellp5 <philip.cunnell@colorado.edu>
2024-07-15 10:28:17 -0600

    submit form set up

---
+++ src/routes/admin/homepage/articles/create/+page.server.ts
@@ -0,0 +1,25 @@
+export const actions: Actions = {
+	default: async (event) => {
+		console.log('SUBMITTED ARTICLE');
+
+		// const submitForm = async () => {
+		// 	// Example POST request to your API endpoint
+		// 	const response = await fetch('/api/articles', {
+		// 		method: 'POST',
+		// 		headers: {
+		// 			'Content-Type': 'application/json'
+		// 		},
+		// 		body: JSON.stringify(article)
+		// 	});
+
+		// 	if (response.ok) {
+		// 		// Handle success response
+		// 		console.log('Article submitted successfully');
+		// 		// Optionally, clear the form or redirect the user
+		// 	} else {
+		// 		// Handle error response
+		// 		console.error('Failed to submit article');
+		// 	}
+		// };
+	}
+};
