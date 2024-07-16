891ff11721f8df0acd0566afb7d751f9549dc383
cunnellp5 <philip.cunnell@colorado.edu>
2024-07-16 07:49:03 -0600

    pass data to backend and save in db

--- src/routes/admin/homepage/articles/create/+page.server.ts
+++ src/routes/admin/homepage/articles/create/+page.server.ts
@@ -1,6 +1,42 @@
+import { error, redirect, fail } from '@sveltejs/kit';
+import db from '$lib/server/database';
+import type { Actions } from './$types';
+
 export const actions: Actions = {
 	default: async (event) => {
-		console.log('SUBMITTED ARTICLE');
+		if (!event.locals.session) {
+			redirect(302, '/login');
+		}
+
+		const formData = await event.request.formData();
+
+		const article = {
+			articleImgUrl: formData.get('articleImgUrl')
+				? JSON.stringify(formData.get('articleImgUrl'))
+				: '',
+			contentMessage: formData.get('contentMessage')
+				? JSON.stringify(formData.get('contentMessage'))
+				: '',
+			contentAuthor: formData.get('contentAuthor')
+				? JSON.stringify(formData.get('contentAuthor'))
+				: '',
+			linkUrl: formData.get('linkUrl') ? JSON.stringify(formData.get('linkUrl')) : '',
+			linkTitle: formData.get('linkTitle') ? JSON.stringify(formData.get('linkTitle')) : ''
+		};
+
+		console.log(article, 'ARTICLE');
+		// TODO handle validation errors
+		// TODO ensure the fall back values render something other than a skeleton loader (probably want to avoid empty strings)
+
+		await db.article.create({
+			data: {
+				articleImgUrl: article.articleImgUrl,
+				contentMessage: article.contentMessage,
+				contentAuthor: article.contentAuthor,
+				linkUrl: article.linkUrl,
+				linkTitle: article.linkTitle
+			}
+		});
 
 		// const submitForm = async () => {
 		// 	// Example POST request to your API endpoint
