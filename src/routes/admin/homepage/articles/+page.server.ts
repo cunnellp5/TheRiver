77688e09cc7f03faf332b95162418f893d524b3f
cunnellp5 <philip.cunnell@colorado.edu>
2024-07-16 08:47:50 -0600

    add better error handling and messaging plus notes for testing

--- src/routes/admin/homepage/articles/+page.server.ts
+++ src/routes/admin/homepage/articles/+page.server.ts
@@ -17,26 +17,38 @@ export const load: PageServerLoad = async (event) => {
 
 export const actions: Actions = {
 	deleteArticle: async (event) => {
+		// TODO test me
 		if (!event.locals.session) {
-			redirect(302, '/login');
+			throw new Error('Unauthorized access');
 		}
 
 		const formData = await event.request.formData();
 		const articleId = Number(formData.get('articleId'));
 
-		// TODO add delete message in UI
-		if (!articleId) {
+		// TODO test me
+		if (Number.isNaN(articleId) || articleId <= 0) {
 			return fail(400, {
 				message: 'Invalid article id'
 			});
 		}
 
-		await db.article.delete({
-			where: {
-				id: articleId
-			}
-		});
+		try {
+			await db.article.delete({
+				where: {
+					id: articleId
+				}
+			});
 
-		return { message: 'Article deleted' };
+			return {
+				success: true,
+				message: 'Article deleted'
+			};
+		} catch (err) {
+			// TODO test me
+			// log the error with some logger (sentry.io?)
+			return fail(500, {
+				message: 'An error occurred while deleting the article'
+			});
+		}
 	}
 };
