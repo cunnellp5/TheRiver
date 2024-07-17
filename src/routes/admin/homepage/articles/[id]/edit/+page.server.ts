ecdc469ce812332006ecf0fb62b4126337b98172
cunnellp5 <philip.cunnell@colorado.edu>
2024-07-17 14:34:01 -0600

    outline logic for edit form on server

--- src/routes/admin/homepage/articles/[id]/edit/+page.server.ts
+++ src/routes/admin/homepage/articles/[id]/edit/+page.server.ts
@@ -23,3 +23,55 @@ export const load: PageServerLoad = async (event) => {
 		return error(500, 'Internal Server Error');
 	}
 };
+
+// TODO FINISH THIS
+export const actions: Actions = {
+	updateArticle: async (event) => {
+		// TODO test me
+		if (!event.locals.session) {
+			throw new Error('Unauthorized access');
+		}
+
+		const formData = await event.request.formData();
+		const articleId = Number(formData.get('articleId'));
+		const linkTitle = formData.get('linkTitle') as string;
+		const contentMessage = formData.get('contentMessage') as string;
+
+		// TODO test me
+		if (Number.isNaN(articleId) || articleId <= 0) {
+			return fail(400, {
+				message: 'Invalid article id'
+			});
+		}
+
+		// TODO test me
+		if (!title || !content) {
+			return fail(400, {
+				message: 'Title and content are required'
+			});
+		}
+
+		try {
+			await db.article.update({
+				where: {
+					id: articleId
+				},
+				data: {
+					linkTitle,
+					contentMessage
+				}
+			});
+
+			return {
+				success: true,
+				message: 'Article updated'
+			};
+		} catch (err) {
+			// TODO test me
+			// log the error with some logger (sentry.io?)
+			return fail(500, {
+				message: 'An error occurred while updating the article'
+			});
+		}
+	}
+};
