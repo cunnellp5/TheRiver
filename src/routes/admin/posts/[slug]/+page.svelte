dcbca3d28fa978d3a89d107e7f2002c607ce7583
cunnellp5 <philip.cunnell@colorado.edu>
2024-07-25 09:30:06 -0600

    posts: - render appropriate blog post - show err if necessary - styles

--- src/routes/admin/posts/[slug]/+page.svelte
+++ src/routes/admin/posts/[slug]/+page.svelte
@@ -8,40 +8,50 @@
 	// eslint-disable-next-line import/no-unresolved
 	import { page } from '$app/stores';
 	import EditDeleteActions from '../components/EditDeleteActions.svelte';
+	// eslint-disable-next-line import/no-unresolved
+	import { browser } from '$app/environment';
 
 	export let data: PageData;
 	let quill: Quill | null;
 	let reader: string | HTMLElement;
+	let quillError = '';
 
-	$: post = data.posts.find((p) => p.slug === $page.params.slug) || {
-		title: '',
-		content: '',
-		tags: [],
-		createdAt: new Date(),
-		slug: ''
-	};
+	function findPost(slug: string) {
+		return data.posts.find((p) => p.slug === slug) || undefined;
+	}
 
-	onMount(async () => {
+	let post = findPost($page.params.slug); // initial post
+
+	async function setQuillData() {
+		if (!browser) return;
+
 		try {
 			const { default: Quill } = await import('quill');
-
 			quill = new Quill(reader, QuillConfigReadonly);
 
-			const quillData = await quillContentInit(post.content);
+			const quillData = await quillContentInit(post ? post.content : 'No content found');
 
 			quill.setContents(quillData);
-		} catch (error) {
-			// TODO: unable to load quill, provide some fall back
+		} catch (err) {
+			// eslint-disable-next-line no-console
+			console.error('Failed to set Quill data:', err);
+			quillError = 'Failed to load the editor. Please try again later.';
 		}
+	}
+
+	$: {
+		post = findPost($page.params.slug);
+		setQuillData();
+	}
+
+	onMount(async () => {
+		setQuillData();
 	});
 </script>
 
-<main>
-	<section>
+<section>
+	{#if post}
 		<hgroup>
-			<div class="btnWrapper">
-				<EditDeleteActions slug={post.slug} redirect={true} />
-			</div>
 			<div class="headerAction">
 				<h1 id={post.slug}>{post.title}</h1>
 			</div>
@@ -56,14 +66,17 @@
 		<div class="reader-wrapper">
 			<div bind:this={reader} />
 		</div>
-	</section>
-</main>
+		<div class="btnWrapper">
+			<EditDeleteActions slug={post?.slug || $page.params.slug} redirect={true} />
+		</div>
+	{:else if quillError}
+		<p>Quill broken try again</p>
+	{:else}
+		<p>Post not found</p>
+	{/if}
+</section>
 
 <style>
-	main {
-		display: grid;
-		grid-template-columns: 80% 20%;
-	}
 	date {
 		color: var(--gray-7);
 		font-size: var(--font-size-0);
@@ -99,9 +112,17 @@
 	}
 	.btnWrapper {
 		display: flex;
+		flex-direction: column;
 		gap: var(--size-2);
+		width: 100%;
 		& button {
-			width: var(--size-12);
+			width: 100%;
+		}
+		& .edit {
+			background-color: var(--update);
+		}
+		& .delete {
+			background-color: var(--delete);
 		}
 	}
 </style>
