54930511473fa004f0619306cde2ee292ec5bf82
cunnellp5 <philip.cunnell@colorado.edu>
2024-07-27 22:26:19 -0600

    basic styles and set up for working with youtube api

--- src/routes/admin/homepage/videos/+page.svelte
+++ src/routes/admin/homepage/videos/+page.svelte
@@ -1,1 +1,16 @@
+<script>
+	import { enhance } from '$app/forms';
+	// import type { ActionData } from './$types';
+
+	export let form;
+</script>
+
 <pre>video crud on homepage</pre>
+
+{#if form}
+	<pre>{JSON.stringify(form, null, 2)}</pre>
+{/if}
+
+<form method="POST" use:enhance>
+	<button type="submit">fetch youtube</button>
+</form>
