6af24b8de1a01c0374d356418cb7906889f2a800
cunnellp5 <philip.cunnell@colorado.edu>
2024-07-29 11:51:29 -0600

    encourage admin button of fetching video data

--- src/routes/admin/homepage/videos/+page.svelte
+++ src/routes/admin/homepage/videos/+page.svelte
@@ -2,8 +2,12 @@
 	// eslint-disable-next-line import/no-unresolved
 	import { enhance } from '$app/forms';
 
+	export let data;
 	export let form;
+
 	let loading = false;
+
+	$: ({ videos } = data);
 </script>
 
 <ul>
@@ -11,9 +15,33 @@
 	<li>The <em>/music/videos</em> section will then have the latest videos</li>
 </ul>
 
-{#if form}
-	<pre>{JSON.stringify(form, null, 2)}</pre>
-{/if}
+<div class="video-data-wrapper">
+	{#if form}
+		<div>
+			<h4>youtube data</h4>
+			<pre>{JSON.stringify(form, null, 2)}</pre>
+			<form method="POST" action="?/updateDb">
+				<input type="hidden" name="iframes" value={form} />
+				<button class="primary" type="submit">Update the DB</button>
+			</form>
+		</div>
+	{/if}
+
+	<ul class="videos-from-db">
+		{#if videos.length === 0}
+			<li>No videos in the db</li>
+		{:else}
+			<h4>DB data</h4>
+			{#each videos as { id, iframe, updatedAt, createdAt } (id)}
+				<li>
+					<p>{iframe}</p>
+					<p>{updatedAt}</p>
+					<p>{createdAt}</p>
+				</li>
+			{/each}
+		{/if}
+	</ul>
+</div>
 
 {#if loading}
 	<div class="loader">Loading...</div>
@@ -21,6 +49,7 @@
 
 <form
 	method="POST"
+	action="?/getLatestYoutubeVideos"
 	use:enhance={({ cancel }) => {
 		loading = true;
 		// eslint-disable-next-line no-alert, no-restricted-globals
@@ -33,10 +62,11 @@
 		loading = false;
 		return cancel();
 	}}>
-	<button class="secondary" type="submit">Update DB with latest youtube videos</button>
+	<button class="secondary" type="submit">Get latest youtube videos</button>
 </form>
 
 <style>
+	/* ELEMENTS */
 	li {
 		margin-block: var(--size-3);
 		font-size: var(--font-size-1);
@@ -51,5 +81,18 @@
 	button {
 		margin-block: var(--size-7);
 		font-weight: var(--font-weight-6);
+	}
+
+	/* CLASSES */
+	.video-data-wrapper {
+		display: flex;
+		flex-direction: row;
+		/* align-items: center; */
+	}
+	.videos-from-db {
+		margin: var(--size-7);
+		& li {
+			list-style-type: none;
+		}
 	}
 </style>
