6bcd2b43e8e98f76a6fdc8c06f3af496fa68b693
cunnellp5 <philip.cunnell@colorado.edu>
2024-07-28 21:02:00 -0600

    fetch user data instead of relying on session

--- src/routes/(userDashboard)/dashboard/+page.server.ts
+++ src/routes/(userDashboard)/dashboard/+page.server.ts
@@ -1,10 +1,19 @@
 import { error } from '@sveltejs/kit';
+import db from '$lib/server/database';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
-	if (!event.locals.session) {
+	if (!event.locals.session || !event.locals.user) {
 		return error(404, 'Not found');
 	}
 
-	return { user: event.locals.user };
+	try {
+		const user = await db.user.findUnique({
+			where: { id: event.locals.user.id }
+		});
+
+		return { user };
+	} catch (err) {
+		return error(404, 'Not found');
+	}
 };
