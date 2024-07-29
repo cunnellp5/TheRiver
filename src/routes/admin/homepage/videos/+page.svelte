e111290c7236c0121f6efd524ea36817b7f7541e
cunnellp5 <philip.cunnell@colorado.edu>
2024-07-29 10:21:29 -0600

    make the ui a little more interactive

--- src/routes/admin/homepage/videos/+page.svelte
+++ src/routes/admin/homepage/videos/+page.svelte
@@ -1,16 +1,55 @@
 <script>
+	// eslint-disable-next-line import/no-unresolved
 	import { enhance } from '$app/forms';
-	// import type { ActionData } from './$types';
 
 	export let form;
+	let loading = false;
 </script>
 
-<pre>video crud on homepage</pre>
+<ul>
+	<li>Clicking the button below will fetch the latest youtube videos and update the DB</li>
+	<li>The <em>/music/videos</em> section will then have the latest videos</li>
+</ul>
 
 {#if form}
 	<pre>{JSON.stringify(form, null, 2)}</pre>
 {/if}
 
-<form method="POST" use:enhance>
-	<button type="submit">fetch youtube</button>
+{#if loading}
+	<div class="loader">Loading...</div>
+{/if}
+
+<form
+	method="POST"
+	use:enhance={({ cancel }) => {
+		loading = true;
+		// eslint-disable-next-line no-alert, no-restricted-globals
+		if (confirm('This updates our DB with the latest youtube videos. Are you sure?')) {
+			return async ({ update }) =>
+				update().finally(() => {
+					loading = false;
+				});
+		}
+		loading = false;
+		return cancel();
+	}}>
+	<button class="secondary" type="submit">Update DB with latest youtube videos</button>
 </form>
+
+<style>
+	li {
+		margin-block: var(--size-3);
+		font-size: var(--font-size-1);
+		list-style-type: disc;
+	}
+	pre {
+		border: 1px solid var(--border);
+		padding: var(--size-3);
+		max-width: var(--size-content-3);
+		overflow: auto;
+	}
+	button {
+		margin-block: var(--size-7);
+		font-weight: var(--font-weight-6);
+	}
+</style>
