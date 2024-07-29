114ec047531b0aa7022c8b33af41297ec8075cb1
cunnellp5 <philip.cunnell@colorado.edu>
2024-07-28 21:01:10 -0600

    use the shadcn component but strip tailwind styles to use my own

---
+++ src/lib/components/ui/shadcn/card/card.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	type $$Props = HTMLAttributes<HTMLDivElement>;
+
+	// eslint-disable-next-line no-undef-init
+	let className: $$Props['class'] = undefined;
+
+	export { className as class };
+</script>
+
+<div class={className} {...$$restProps}>
+	<slot />
+</div>
+
+<style>
+	div {
+		box-shadow: var(--shadow-1);
+		border: 1px solid var(--border);
+		border-radius: var(--radius-2);
+		background: var(--surface-2);
+		color: var(--text-1);
+	}
+</style>
