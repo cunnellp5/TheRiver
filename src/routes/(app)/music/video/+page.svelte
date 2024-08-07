96bc9bd1eaa25fe11118d90303d22d8ceaa128b7
cunnellp5 <philip.cunnell@colorado.edu>
2024-08-07 11:10:17 -0600

    actually render properly styled videos

--- src/routes/(app)/music/video/+page.svelte
+++ src/routes/(app)/music/video/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	// control the iframe: https://developers.google.com/youtube/iframe_api_reference
 	// carousel: https://www.shadcn-svelte.com/docs/components/carousel
+	import * as Card from '$lib/components/ui/shadcn/card';
+	import * as Carousel from '$lib/components/ui/shadcn/carousel';
+
 	export let data;
 
 	const { videos } = data;
@@ -8,23 +11,26 @@
 	const YOUTUBE_BASE_EMBED_URL = 'https://www.youtube.com/embed/';
 </script>
 
-<div class="grid-container">
-	<div class="videos">
-		{#each videos as video}
-			{#if video}
-				<!-- <button
-					type="button"
-					on:click={() => {
-						showModal = true;
-					}}> -->
-				<a href={`${YOUTUBE_BASE_EMBED_URL}${video.videoId}`}>
-					<img src={video.thumbnail} alt={video.title} />
-					<label for="img">{video.title}</label>
-				</a>
-				<!-- </button> -->
-			{/if}
-		{/each}
-	</div>
+<div class="w-full">
+	<Carousel.Root>
+		<Carousel.Content>
+			{#each videos as video, i (i)}
+				<Carousel.Item>
+					<Card.Root>
+						<Card.Content>
+							<span class="text-4xl font-semibold">{i + 1}</span>
+							<a href={`${YOUTUBE_BASE_EMBED_URL}${video.videoId}`}>
+								<img src={video.thumbnail} alt={video.title} />
+								<label for="img">{video.title}</label>
+							</a>
+						</Card.Content>
+					</Card.Root>
+				</Carousel.Item>
+			{/each}
+		</Carousel.Content>
+		<Carousel.Previous />
+		<Carousel.Next />
+	</Carousel.Root>
 </div>
 
 <style>
@@ -41,25 +47,30 @@
 	}
 
 	/* CLASSES */
-	.grid-container {
+	.w-full {
+		display: flex;
+		justify-content: center;
+		/* width: 100%; */
+		/* max-width: var(--size-content-2); */
+	}
+	/* .grid-container {
 		margin-block: var(--size-7);
 	}
 	.videos {
 		display: grid;
-		grid-template-columns: repeat(3, 1fr); /* 3 columns */
-		grid-auto-rows: minmax(200px, auto); /* Set a base row height */
+		grid-template-columns: repeat(3, 1fr);
+		grid-auto-rows: minmax(200px, auto);
 		gap: var(--size-7);
 	}
 
-	/* MEDIA QUERIES */
 	@media (max-width: 1440px) {
 		.videos {
-			grid-template-columns: repeat(2, 1fr); /* 2 columns */
+			grid-template-columns: repeat(2, 1fr);
 		}
 	}
 	@media (max-width: 768px) {
 		.videos {
-			grid-template-columns: repeat(1, 1fr); /* 1 column */
+			grid-template-columns: repeat(1, 1fr);
 		}
-	}
+	} */
 </style>
