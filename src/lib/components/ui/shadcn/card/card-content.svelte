9ee858f268849b989c460e44d625f3a92f2f6d5b
cunnellp5 <philip.cunnell@colorado.edu>
2024-08-08 11:53:03 -0600

    rm unused line lines

--- src/lib/components/ui/shadcn/card/card-content.svelte
+++ src/lib/components/ui/shadcn/card/card-content.svelte
@@ -3,7 +3,6 @@
 
 	type $$Props = HTMLAttributes<HTMLDivElement>;
 
-	// eslint-disable-next-line no-undef-init
 	let className: $$Props['class'] = undefined;
 	export { className as class };
 </script>
