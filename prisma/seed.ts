5a8bf77b25f2c93fbfdef6300812b888bde1ff46
cunnellp5 <philip.cunnell@colorado.edu>
2024-08-12 10:23:18 -0600

    clean up seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,14 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, type Service } from '@prisma/client';
 import { Argon2id } from 'oslo/password';
+import slugify from '$lib/utils/slugify';
+
+import articlesData from './seedData/articlesData';
+import serviceCategories from './seedData/serviceCategories';
+import serviceHairCutData from './seedData/serviceHairCutData';
+import serviceHairColorData from './seedData/serviceHairColorData';
+import serviceHairAddOnData from './seedData/serviceHairAddOnData';
+import serviceNewClientData from './seedData/serviceNewClientData';
+import serviceNailData from './seedData/serviceNailData';
 
 const db = new PrismaClient();
 
@@ -17,127 +26,112 @@ async function getPosts() {
 	return posts as DummyPost[];
 }
 
-function slugify(text: string) {
-	return text
-		.toLowerCase()
-		.replace(/\s/g, '-')
-		.replace(/[^a-zA-Z0-9-]/g, '');
-}
-
 async function main() {
-	const posts = await getPosts();
+	await db.$transaction(async (db) => {
+		// DELETE ALL DATA
+		await db.article.deleteMany();
+		await db.service.deleteMany();
+		await db.serviceCategory.deleteMany();
+		await db.user.deleteMany();
+		await db.post.deleteMany();
 
-	const data = posts.map((post, i) => ({
-		title: post.title,
-		content: post.body,
-		slug: slugify(post.title),
-		tags: post.tags,
-		description: `${post.body.slice(0, 100)}...`,
-		published: i % 2 === 0
-	}));
+		// FETCH POSTS FROM API
+		const posts = await getPosts();
 
-	await db.post.createMany({ data });
+		// BUILDING POST DATA
+		const data = posts.map((post, i) => ({
+			title: post.title,
+			content: post.body,
+			slug: slugify(post.title),
+			tags: post.tags,
+			description: `${post.body.slice(0, 100)}...`,
+			published: i % 2 === 0
+		}));
 
-	await db.service.createMany({
-		data: [
-			{
-				name: 'Haircut',
-				description: 'Get your hair cut and styled',
-				duration: 60,
-				availability: 'AVAILABLE',
-				category: 'HAIR',
-				slug: 'haircut',
-				price: 100
-			},
-			{
-				name: 'Gel nails',
-				description: 'Get your nails did',
-				duration: 90,
-				availability: 'LIMITED_AVAILABILITY',
-				category: 'NAILS',
-				slug: 'gel-nails',
-				price: 69
-			},
-			{
-				name: 'Waxing',
-				description: 'Wax your brow',
-				duration: 30,
-				availability: 'COMING_SOON',
-				category: 'WAX',
-				slug: 'wax',
-				price: 35
-			},
-			{
-				name: 'Rainbow extensions',
-				description: 'Get fully organic extesions',
-				duration: 120,
-				availability: 'LIMITED_AVAILABILITY',
-				category: 'HAIR',
-				slug: 'rainbow-extensions',
-				price: 35
+		// PUT POSTS IN DB
+		await db.post.createMany({ data });
+
+		// 1. Create haircut Category/Services
+		const hairCutCategory = await db.serviceCategory.create({ data: serviceCategories[2] });
+		await db.service.createMany({
+			data: serviceHairCutData.map((service) => ({ ...service, categoryId: hairCutCategory.id }))
+		});
+
+		// 2. create Hair color category/services
+		const hairColorCategory = await db.serviceCategory.create({ data: serviceCategories[1] });
+		await db.service.createMany({
+			data: serviceHairColorData.map((service) => ({
+				...service,
+				categoryId: hairColorCategory.id
+			}))
+		});
+
+		// 3. create Hair Add-on category/services
+		const hairAddOnCategory = await db.serviceCategory.create({ data: serviceCategories[0] });
+		await db.service.createMany({
+			data: serviceHairAddOnData.map((service) => ({
+				...service,
+				categoryId: hairAddOnCategory.id
+			}))
+		});
+
+		// 4. create New Client category/services
+		const newClientCategory = await db.serviceCategory.create({ data: serviceCategories[3] });
+		await db.service.create({
+			data: {
+				...serviceNewClientData,
+				categoryId: newClientCategory.id
 			}
-		]
-	});
+		});
 
-	const hashedPassword = await new Argon2id().hash('12345678');
+		// 5. create Nails category/services
+		const NailsCategory = await db.serviceCategory.create({ data: serviceCategories[4] });
+		await db.service.createMany({
+			data: serviceNailData.map((service) => ({ ...service, categoryId: NailsCategory.id }))
+		});
+		// *************
 
-	await db.user.create({
-		data: {
-			email: 'test@example.com',
-			firstName: 'philly',
-			lastName: 'phil',
-			isSubscribed: true,
-			hashedPassword,
-			isAdmin: true
-		}
-	});
+		// 2. Relate services to categories then create from seedData
+		// await db.service.createMany({
+		// 	data: servicesData
+		// });
 
-	await db.timeSlot.create({
-		data: {
-			startTime: new Date('2024-07-07T13:00:00Z'),
-			endTime: new Date('2024-07-07T14:00:00Z'),
-			day: new Date('2024-07-07T19:00:00.000Z'),
-			available: true,
-			serviceId: 1
-		}
-	});
-
-	await db.article.createMany({
-		data: [
-			{
-				articleTitle: 'The River Delivers Angry Verses On “Hours”',
-				author: 'Victória Santiago',
-				description:
-					'Ever since releasing her first song in June 2021, she has had her foot on the gas.',
-				img: 'https://res.cloudinary.com/dswpu3qez/image/upload/v1707412331/sample.jpg',
-				link: 'https://ratingsgamemusic.com/2022/01/08/the-river-delivers-angry-verses-on-hours/'
-			},
-			{
-				articleTitle: 'The River’s “Hours” digs deep into the dynamics of failed relationships',
-				author: 'TAYO ODUTOLA',
-				description:
-					"On Hours: The production here is sublime and crisp as each instrument stands out with clarity while The River's commanding vocals cut through the mix with ease.",
-				img: 'https://res.cloudinary.com/dswpu3qez/image/upload/v1707412341/samples/people/jazz.jpg',
-				link: 'https://earmilk.com/2022/01/10/the-rivers-hours-digs-deep-into-the-dynamics-of-failed-relationships/'
-			},
-			{
-				articleTitle: 'It aint that deep',
-				author: 'Alyce Lindberg',
-				description:
-					"The vocals are confidently delivered, imbued with the effortless coolness that comes with being insanely naturally gifted. Plus, the arrangement and production create the most infectious groove you'll ever hear.",
-				img: 'https://res.cloudinary.com/dswpu3qez/image/upload/v1707412345/samples/ecommerce/accessories-bag.jpg',
-				link: 'https://musicfashionblog.com/it-aint-that-deep-the-river/'
-			},
-			{
-				articleTitle: 'Soulful and introspective track',
-				author: 'Kreadiv Kesh',
-				description:
-					"Somewhere in the lyrics there is peace and truth. The River's latest single “Hours” facing the reality of love, respect, and emotions. Over and acoustic melody the rain pours over the lyrical expressions of facing your feelings.",
-				img: 'https://res.cloudinary.com/dswpu3qez/image/upload/v1707412364/samples/man-portrait.jpg',
-				link: 'EXPIRED_URL' // https://www.laonlock.com/reviews/1/11/2022/the-river-hours-review
+		// BEGIN CREATEING SEED USER
+		const hashedPassword = await new Argon2id().hash('12345678');
+		await db.user.create({
+			data: {
+				email: 'test@example.com',
+				firstName: 'philly',
+				lastName: 'phil',
+				isSubscribed: true,
+				hashedPassword,
+				isAdmin: true
 			}
-		]
+		});
+
+		// DUMMY TIME SLOT
+		// await db.timeSlot.create({
+		// 	data: {
+		// 		startTime: new Date('2024-07-07T13:00:00Z'),
+		// 		endTime: new Date('2024-07-07T14:00:00Z'),
+		// 		day: new Date('2024-07-07T19:00:00.000Z'),
+		// 		available: true,
+		// 		serviceId: 1
+		// 	}
+		// });
+
+		// ARTICLES SEEDS
+		await db.article.createMany({
+			data: articlesData
+		});
 	});
 }
 
-main();
+main()
+	.catch((e) => {
+		console.error(e);
+		process.exit(1);
+	})
+	.finally(async () => {
+		await db.$disconnect();
+	});
