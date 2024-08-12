afda82a86826ff0204eef8670ee68298553f8dae
cunnellp5 <philip.cunnell@colorado.edu>
2024-08-12 10:23:36 -0600

    separate data for seeding

---
+++ prisma/seedData/serviceCategories.ts
@@ -0,0 +1,27 @@
+export default [
+	{
+		value: 'HAIR_ADD_ON',
+		name: 'Hair Add-on',
+		description: 'Add on to any service'
+	},
+	{
+		value: 'HAIR_COLOR',
+		name: 'Hair Color',
+		description: 'Color your hair any way you like'
+	},
+	{
+		value: 'HAIR_CUT',
+		name: 'Haircut',
+		description: 'Get a haircut'
+	},
+	{
+		value: 'NEW_CLIENT',
+		name: 'New Client',
+		description: 'New client services'
+	},
+	{
+		value: 'NAILS',
+		name: 'Nails',
+		description: 'Nail services'
+	}
+];
---
+++ prisma/seedData/serviceHairAddOnData.ts
@@ -0,0 +1,20 @@
+export default [
+	{
+		name: 'Shampoo + Blow Dry & Style',
+		description: 'Add on to any service',
+		duration: 60,
+		price: 35
+	},
+	{
+		name: 'Deep Conditioning Treatment',
+		description: 'Add on to any service',
+		duration: 15,
+		price: 35
+	},
+	{
+		name: 'Keratin Shot',
+		description: 'Add on to any service',
+		duration: 15,
+		price: 45
+	}
+];
