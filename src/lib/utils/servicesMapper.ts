5b7134e27ac2d147b82f63c502f9b5efe6e92c7c
cunnellp5 <philip.cunnell@colorado.edu>
2024-08-15 06:54:21 -0600

    adding a mapper

---
+++ src/lib/utils/servicesMapper.ts
@@ -0,0 +1,29 @@
+// THIS MAPS SERVICE AND CATEGORY DATA TOGETHER SUCH THAT WE CAN HANDLE IT IN THE TABLES
+import type { Service, ServiceCategory } from '@prisma/client';
+
+// type for the remapped table
+type TableServiceInfo = Record<string, Service[]>;
+
+// combine service and cateogry in one type
+type ServiceWithCategory = Service & { category: ServiceCategory };
+
+export const servicesMapper = (services: ServiceWithCategory[]) => {
+	return services.reduce(
+		(acc: TableServiceInfo, service) => {
+			if (!acc[service.category.name]) {
+				acc[service.category.name] = [];
+			}
+			acc[service.category.name].push({
+				...service
+			});
+			return acc;
+		},
+		{
+			'New Client': [],
+			Haircut: [],
+			'Hair Color': [],
+			'Hair Add-on': [],
+			Nails: []
+		} as TableServiceInfo
+	);
+};
