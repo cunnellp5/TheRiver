9351b6d1090a46fbc4fd4dac45674ccc697da9a9
cunnellp5 <philip.cunnell@colorado.edu>
2024-08-16 13:28:49 -0600

    new model, new seeds, new data

---
+++ prisma/seedData/aboutData.ts
@@ -0,0 +1,38 @@
+export default [
+	{
+		name: 'home',
+		text: 'Welcome to the home page',
+		isShowing: true,
+		url: '/'
+	},
+	{
+		name: 'music',
+		text: 'Welcome to the music page',
+		isShowing: true,
+		url: '/music'
+	},
+	{
+		name: 'services',
+		text: 'Welcome to the services page',
+		isShowing: true,
+		url: '/services'
+	},
+	{
+		name: 'shop',
+		text: 'Welcome to the shop page',
+		isShowing: true,
+		url: '/shop'
+	},
+	{
+		name: 'posts',
+		text: 'Welcome to the blog',
+		isShowing: true,
+		url: '/posts'
+	},
+	{
+		name: 'contact',
+		text: 'Welcome to the contact page',
+		isShowing: true,
+		url: '/contact'
+	}
+];
