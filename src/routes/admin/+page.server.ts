a48cca993d192fd88b0acc781eef4b68d630a364
cunnellp5 <philip.cunnell@colorado.edu>
2024-08-21 10:50:37 -0600

    save data to businessinfo

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -1,5 +1,5 @@
 import db from '$lib/server/database';
-import { error } from '@sveltejs/kit';
+import { error, type Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
@@ -8,13 +8,28 @@ export const load: PageServerLoad = async (event) => {
 	}
 
 	try {
-		const business = await db.businessInfo.findFirst({
-			where: {
-				name: 'The River'
-			}
-		});
+		const business = await db.businessInfo.findFirst();
 		return { business };
 	} catch (err) {
 		return error(500, 'Internal Server Error');
+	}
+};
+
+export const actions: Actions = {
+	updateBusinessInfo: async ({ request }) => {
+		const formData = await request.formData();
+		const id = Number(formData.get('id'));
+		const key = formData.get('key') as string;
+		const value = formData.get('value') as string;
+
+		try {
+			await db.businessInfo.update({
+				where: { id },
+				data: { [key]: value }
+			});
+			return { success: true };
+		} catch (err) {
+			return error(500, 'Internal Server Error');
+		}
 	}
 };
