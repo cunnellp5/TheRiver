47ba8b75964429d13dc9b7ca74e2a58beefe40d8
cunnellp5 <philip.cunnell@colorado.edu>
2024-08-21 10:49:57 -0600

    updating files with new layouts

--- src/routes/admin/market/+page.server.ts
+++ src/routes/admin/market/+page.server.ts
@@ -1,33 +1,16 @@
-import type { Actions } from '@sveltejs/kit';
-import fs from 'fs';
-import path from 'path';
-import market from '$lib/data/json/market.json';
+import db from '$lib/server/database';
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
 
-export const actions: Actions = {
-	default: async ({ request }) => {
-		const formData = await request.formData();
+export const load: PageServerLoad = async (event) => {
+	if (!event.locals.session || !event.locals.user) {
+		return error(404, 'Not found');
+	}
 
-		const inputUrl = formData.get('inputUrl') as string;
-		const inputText = formData.get('inputText') as string;
-
-		market.url = inputUrl;
-		market.text = inputText;
-
-		// Define the path to the file
-		const filePath = path.join(process.cwd(), './src/lib/data/json/market.json');
-
-		// Create the data object to be written
-		const data = JSON.parse(JSON.stringify(market));
-
-		// Write the data to the file
-		fs.writeFile(filePath, JSON.stringify(data, null, 2), 'utf8', (err) => {
-			if (err) {
-				// SHOW ERROR ON THE PAGE
-				console.error('Error writing file:', err);
-			} else {
-				// SHOW TOAST
-				console.log('File has been saved.');
-			}
-		});
+	try {
+		const business = await db.businessInfo.findFirst();
+		return { business };
+	} catch (err) {
+		return error(500, 'Internal Server Error');
 	}
 };
--- src/routes/admin/market/+page.svelte
+++ src/routes/admin/market/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import * as Card from '$lib/components/ui/shadcn/card';
 	import market from '$lib/data/json/market.json';
+
+	export let data;
+	const { business } = data;
 </script>
 
 <div class="adminIntroCardWrapper">
@@ -10,80 +13,35 @@
 			<Card.Description>
 				All the images and data loads from etsys RSS feed, so the UI should update automatically
 			</Card.Description>
+			<Card.Description>
+				Link info comes from /admin section where all the business info is stored
+			</Card.Description>
 		</Card.Header>
 		<Card.Content>
+			<span>Current Link</span>
 			<p>
-				<span> Link: </span>
-				{market.url}
+				<a href={business?.merchUrl}>
+					{business?.merchUrl}
+				</a>
 			</p>
+			<br />
+			<span>Display text</span>
 			<p>
-				<span> Display: </span>
-				{market.text}
+				{business?.merchText}
 			</p>
 		</Card.Content>
 	</Card.Root>
 </div>
 
-<!-- TODO make sure its required and add a warning-->
-<div class="formWrapper">
-	<form method="POST">
-		<div>
-			<div class="inputWrapper">
-				<label for="inputUrl">Link:</label>
-				<input
-					required
-					type="text"
-					name="inputUrl"
-					placeholder="https://put-link-here.com"
-					value={market.url} />
-			</div>
-			<div class="inputWrapper">
-				<label for="inputText">Hyper text:</label>
-				<input required type="text" name="inputText" placeholder="Display" value={market.text} />
-			</div>
-			<button type="submit">Update</button>
-		</div>
-	</form>
-</div>
-
 <style>
-	form {
-		align-self: center;
-		width: 100%;
-		max-width: var(--size-content-2);
-		& button {
-			margin-block-start: var(--size-4);
-		}
-	}
 	div {
 		display: flex;
 		flex-direction: column;
 	}
 	p {
 		color: var(--link);
-		font-weight: var(--font-weight-1);
 	}
 	span {
 		color: var(--text-2);
-		font-size: var(--font-size-1);
-	}
-	/* CLASSES */
-	.formWrapper {
-		width: 100%;
-	}
-	.inputWrapper {
-		display: flex;
-		flex-direction: column;
-		justify-content: space-between;
-		margin-block: var(--size-2);
-		& input {
-			width: 100%;
-			color: var(--link);
-		}
-		& label {
-			margin-block: var(--size-2);
-			color: var(--text-2);
-			font-size: var(--font-size-1);
-		}
 	}
 </style>
