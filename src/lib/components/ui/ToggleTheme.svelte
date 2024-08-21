d4de74bc501ddb4c19c7ef70ae96d554eaf4c3ca
cunnellp5 <philip.cunnell@colorado.edu>
2024-08-21 10:48:08 -0600

    move socials and toggle to lib section

---
+++ src/lib/components/ui/ToggleTheme.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { fly } from 'svelte/transition';
+	import Circle from 'lucide-svelte/icons/circle';
+	import { theme, toggleTheme } from '$lib/stores/theme';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	type $$Props = HTMLAttributes<HTMLDivElement>;
+	let className: $$Props['class'] = '';
+	export { className as class };
+</script>
+
+<button on:click={toggleTheme} aria-label="Toggle Theme" class={className}>
+	{#if $theme === 'dark'}
+		<div in:fly={{ y: 10 }}>
+			<Circle fill="#E6E4C3" />
+		</div>
+	{:else}
+		<div in:fly={{ y: -10 }}>
+			<Circle fill="#343332" />
+		</div>
+	{/if}
+</button>
+
+<style>
+	button {
+		box-shadow: none;
+		border: none;
+		background: none;
+		padding: 0;
+		overflow: hidden;
+		font-weight: inherit;
+	}
+
+	button > * {
+		display: flex;
+		gap: var(--size-2);
+	}
+</style>
