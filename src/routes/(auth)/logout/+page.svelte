1fccf9a11bd28de5e2ca86c6aace0785d38d0704
cunnellp5 <philip.cunnell@colorado.edu>
2024-08-29 17:36:34 -0600

    logout seo

--- src/routes/(auth)/logout/+page.svelte
+++ src/routes/(auth)/logout/+page.svelte
@@ -1,3 +1,11 @@
+<script lang="ts">
+	import Seo from '$lib/components/SEO.svelte';
+</script>
+
+<Seo
+	title={'Logout - See You Next Time!'}
+	description={'You have successfully logged out. Thank you for visiting. We look forward to seeing you again soon!'} />
+
 <main>
 	<h2>Logged out.</h2>
 	<p>🫶 See you later!</p>
