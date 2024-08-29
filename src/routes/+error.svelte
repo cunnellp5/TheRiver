f62b874c8d8e9af5037486e37257feeb2d9e0837
cunnellp5 <philip.cunnell@colorado.edu>
2024-08-29 10:20:00 -0600

    dont reveal anything to the user

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,38 +1,39 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+	// import { page } from '$app/stores';
 </script>
 
-<main class:hype={$page.status === 404} class="app-layout">
-	{#if $page.status === 404}
-		<h1>The River</h1>
-		<section>
-			<a href="/">
-				<p>Home</p>
-			</a>
-			<a href="/music">
-				<p>Music</p>
-			</a>
-			<a href="/services">
-				<p>Services</p>
-			</a>
-			<a href="/shop">
-				<p>Merch</p>
-			</a>
-			<a href="/blog">
-				<p>Blog</p>
-			</a>
-			<a href="/contact">
-				<p>Contact</p>
-			</a>
-		</section>
-	{:else if $page.status === 500}
+<!-- class:hype={$page.status === 404}  -->
+<main class="app-layout hype">
+	<!-- {#if $page.status === 404} -->
+	<h1>The River</h1>
+	<section>
+		<a href="/">
+			<p>Home</p>
+		</a>
+		<a href="/music">
+			<p>Music</p>
+		</a>
+		<a href="/services">
+			<p>Services</p>
+		</a>
+		<a href="/shop">
+			<p>Merch</p>
+		</a>
+		<a href="/blog">
+			<p>Blog</p>
+		</a>
+		<a href="/contact">
+			<p>Contact</p>
+		</a>
+	</section>
+	<!-- {:else if $page.status === 500}
 		<h1>We weren't expecting that</h1>
 		<p>We're investigating the issue.</p>
 	{:else}
 		<h1>{$page.status}</h1>
 		<p>{$page.error?.message}</p>
-		<a href="/">Home</a>
-	{/if}
+		<a href="/">Home</a> -->
+	<!-- {/if} -->
 </main>
 
 <style>
