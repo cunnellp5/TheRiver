1577f807d0955bc221712f5b9926d7a1929bbdbf
cunnellp5 <philip.cunnell@colorado.edu>
2024-08-29 05:28:09 -0600

    dashboard updates

--- src/routes/(userDashboard)/dashboard/+page.svelte
+++ src/routes/(userDashboard)/dashboard/+page.svelte
@@ -5,6 +5,7 @@
 	import market from '$lib/data/json/market.json';
 	import DashboardUserRow from './DashboardUserRow.svelte';
 	import X from 'lucide-svelte/icons/x';
+	import ArrowRight from 'lucide-svelte/icons/arrow-right';
 
 	export let data;
 	export let form;
@@ -44,21 +45,10 @@
 									<DashboardUserRow {key} {value} id={data.user.id} />
 								{/if}
 							{/each}
-							<Table.Row>
-								<Table.Cell colspan={2}>
-									<a href="/reset" class="center"> Reset Password </a>
-								</Table.Cell>
-							</Table.Row>
-							<Table.Row>
-								<Table.Cell colspan={2}>
-									<div class="center">
-										<button class="delete-button"> Delete Account </button>
-									</div>
-								</Table.Cell>
-							</Table.Row>
 						</Table.Body>
 					</Table.Root>
 				</div>
+				<!-- <Card.Footer>Settings</Card.Footer> -->
 			</Card.Content>
 		</Card.Root>
 	</div>
@@ -66,56 +56,131 @@
 	<div class="cardsWrapper">
 		<Card.Root>
 			<Card.Header>
-				<Card.Title>Services</Card.Title>
-				<Card.Description>Nails</Card.Description>
-				<Card.Description>Hair</Card.Description>
+				<Card.Title>Navigation</Card.Title>
+				<Card.Description>Explore the site!</Card.Description>
+				<!-- <Card.Description>Hair</Card.Description> -->
 			</Card.Header>
-			<Card.Footer>
-				<a href="/services">
-					<button class="primary">View all services</button>
-				</a>
-			</Card.Footer>
+			<Card.Content>
+				<ul>
+					<a href="/music">
+						<li>
+							<ArrowRight size={12} />
+							Beats
+						</li>
+					</a>
+					<a href="/services">
+						<li>
+							<ArrowRight size={12} />
+							Services
+						</li>
+					</a>
+					<a href={market.url} target="_blank">
+						<li>
+							<ArrowRight size={12} />
+							Etsy
+						</li>
+					</a>
+					<a href="/blog">
+						<li>
+							<ArrowRight size={12} />
+							Blog
+						</li>
+					</a>
+					<a href="/contact">
+						<li>
+							<ArrowRight size={12} />
+							Contact
+						</li>
+					</a>
+				</ul>
+			</Card.Content>
 		</Card.Root>
 
 		<Card.Root>
 			<Card.Header>
-				<Card.Title>Merch</Card.Title>
-				<Card.Description>Handmade jewelry</Card.Description>
-				<Card.Description>Custom clothes</Card.Description>
+				<Card.Title>Danger Zone</Card.Title>
+				<!-- <Card.Description>Handmade jewelry</Card.Description> -->
+				<!-- <Card.Description>Custom clothes</Card.Description> -->
 			</Card.Header>
-			<Card.Footer>
-				<a href={market.url} target="_blank">
-					<button class="secondary"> Etsy store </button>
-				</a>
-			</Card.Footer>
-		</Card.Root>
-
-		<Card.Root>
-			<Card.Header>
-				<Card.Title>Music</Card.Title>
-				<Card.Description>Videos</Card.Description>
-				<Card.Description>Tracks</Card.Description>
-				<Card.Description>Stems to download</Card.Description>
-			</Card.Header>
-			<Card.Footer>
-				<a href="/music">
-					<button class="primary"> Beats </button>
-				</a>
-			</Card.Footer>
+			<Card.Content>
+				<Table.Root>
+					<Table.Body>
+						<Table.Row>
+							<Table.Cell>
+								<p class="text2">Reset password</p>
+								<span> This cannot be undone. </span>
+							</Table.Cell>
+							<Table.Cell>
+								<a href="/reset" class="center">
+									<button class="update-button"> Reset Password </button>
+								</a>
+							</Table.Cell>
+						</Table.Row>
+						<Table.Row>
+							<Table.Cell>
+								<p class="text2">Account Deletion</p>
+								<span> This is permanent. </span>
+							</Table.Cell>
+							<Table.Cell>
+								<div class="center">
+									<button class="delete-button"> Delete Account </button>
+								</div>
+							</Table.Cell>
+						</Table.Row>
+					</Table.Body>
+				</Table.Root>
+			</Card.Content>
+			<Card.Content></Card.Content>
+			<Card.Footer></Card.Footer>
 		</Card.Root>
 	</div>
 </section>
 
 <style>
+	/* ELEMENTS */
+	li {
+		display: flex;
+		gap: var(--size-2);
+		margin-block: var(--size-2);
+	}
+	a {
+		position: relative;
+		color: var(--text-2);
+		text-decoration: none;
+	}
+
+	ul a::after {
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		transform: scaleX(0);
+		transform-origin: bottom right;
+		transition: transform 0.25s ease-out;
+		background-color: var(--link);
+		width: 100%;
+		height: 1px;
+		content: '';
+	}
+	ul a:hover {
+		color: var(--link);
+	}
+
+	ul a:hover::after {
+		transform: scaleX(1);
+		transform-origin: bottom left;
+	}
+	/* CLASSES */
 	.headWrapper {
 		display: flex;
 		flex-direction: row;
 		justify-content: space-between;
 	}
 	.cardsWrapper {
-		display: grid;
-		grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+		display: flex;
+		flex-direction: column;
 		gap: var(--size-3);
+		margin: 0 auto;
+		width: 70%;
 	}
 	.top-user-card {
 		margin-block: var(--size-7);
@@ -124,7 +189,7 @@
 		display: flex;
 		justify-content: center;
 		margin: 0 auto;
-		width: 50%;
+		width: 70%;
 	}
 	.error-message {
 		color: var(--red-7);
@@ -139,7 +204,16 @@
 	.center {
 		display: flex;
 		justify-content: center;
-		/* gap: var(--size-3); */
-		/* width: 100%; */
+	}
+	.text2 {
+		color: var(--text-2);
+	}
+	@media (max-width: 768px) {
+		.adminIntroCardWrapper {
+			width: 100%;
+		}
+		.cardsWrapper {
+			width: 100%;
+		}
 	}
 </style>
