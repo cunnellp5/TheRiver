2cec0ccc0a080ef96fe80b79752cdc00f12436b9
cunnellp5 <philip.cunnell@colorado.edu>
2024-08-29 16:53:17 -0600

    this hook is throwing an error at a level i dont like

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -40,13 +40,14 @@ export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.user = user;
 	event.locals.session = session;
 
-	if (
-		(!event.locals.session && event.route.id?.includes('/admin')) ||
-		(event.locals.session && !event.locals.user?.isAdmin && event.route.id?.includes('/admin'))
-	) {
-		error(404, 'Not Found');
-		// return resolve(event.locals);
-	}
+	// BELOW RUNS BEFORE THE ADMIN ROUTE LOADS AND FALLS BACK TO ROOT OF APPLICATION
+	// TECHNICALLY DOING THIS WILL CLUE YOU INTO THE FACT THE ROUTE EXISTS
+	// if (
+	// 	(!event.locals.session && event.route.id?.includes('/admin')) ||
+	// 	(event.locals.session && !event.locals.user?.isAdmin && event.route.id?.includes('/admin'))
+	// ) {
+	// 	error(404, 'Not Found');
+	// }
 
 	// generic user logged in and navigates to login page
 	if (event.locals.session && event.route.id?.includes('/login')) {
