6d6bcb322e850fdcc24174411de4073491cbc230
cunnellp5 <philip.cunnell@colorado.edu>
2024-08-30 14:21:23 -0600

    total nav bar revamp

--- src/routes/components/Header.svelte
+++ src/routes/components/Header.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-	import LogoR from '$lib/components/svgs/logos/LogoR2.svelte';
-	import ToggleTheme from '$lib/components/ui/ToggleTheme.svelte';
-	import Waves from 'lucide-svelte/icons/waves';
 	import { onMount } from 'svelte';
 	import { fly } from 'svelte/transition';
-	import { page } from '$app/stores';
 	import { browser } from '$app/environment';
+	import NavDesktop from './NavDesktop.svelte';
+	import NavMobile from './NavMobile.svelte';
+	import NavSheet from './NavSheet.svelte';
 
 	export let isSignedIn = false;
 	export let user;
 	let showAuthLinks = false;
 	let visible = false;
+	let showSheet = false;
 
 	if (browser) {
 		window.addEventListener('click', () => {
@@ -24,92 +24,17 @@
 	onMount(() => {
 		visible = true;
 	});
-
-	$: includesAdmin = $page.url.pathname.includes('/admin');
-	$: includesPosts = $page.url.pathname.includes('/posts');
-	$: includesService = $page.url.pathname.includes('/services');
-	$: includesMusic = $page.url.pathname.includes('/music');
-
-	$: isContact = $page.url.pathname === '/contact';
-	$: isDashboard = $page.url.pathname === '/dashboard';
-	$: isHomePage = $page.url.pathname === '/';
-	$: isLogin = $page.url.pathname === '/login';
-	$: isLogout = $page.url.pathname === '/logout';
-	$: isShop = $page.url.pathname === '/shop';
-	$: isSignup = $page.url.pathname === '/signup';
 </script>
+
+<NavSheet {user} {isSignedIn} show={showSheet} on:handleClose={() => (showSheet = false)} />
 
 <!-- {#if visible} -->
 <nav class="nav-desktop" in:fly={{ duration: 500, y: -400, delay: 500 }}>
-	<div>
-		<a class="logo-link" href="/">
-			<LogoR />
-		</a>
-		<ul class="links" class:homepageText={isHomePage}>
-			<li></li>
-
-			{#if user && user.isAdmin}
-				<li class:current={includesAdmin} aria-current={includesAdmin}>
-					<a href="/admin"> Admin </a>
-				</li>
-			{/if}
-
-			{#if isSignedIn && user}
-				<li class:current={isDashboard} aria-current={isDashboard}>
-					<a href="/dashboard"> Dashboard </a>
-				</li>
-			{/if}
-		</ul>
-	</div>
-
-	<ul class="links" class:homepageText={isHomePage}>
-		<li class:current={includesMusic} aria-current={includesMusic}>
-			<a href="/music"> Music </a>
-		</li>
-
-		<li class:current={includesService} aria-current={includesService}>
-			<a href="/services"> services </a>
-		</li>
-
-		<li class:current={isShop} aria-current={isShop}>
-			<a href="/shop"> Shop </a>
-		</li>
-
-		<li class="border-left"></li>
-
-		<li class:current={includesPosts} aria-current={includesPosts}>
-			<a href="/blog"> Blog </a>
-		</li>
-		<li class:current={isContact} aria-current={isContact}>
-			<a href="/contact"> Contact </a>
-		</li>
-
-		<li class="border-left"></li>
-
-		{#if isSignedIn}
-			<form class="logout-wrapper" id="logoutForm" method="POST" action="/logout">
-				<li class:current={isLogout} class:homepageText={isHomePage}>
-					<button class="logout-button" type="submit">Logout</button>
-				</li>
-			</form>
-		{:else}
-			<li class:current={isSignup} class="signup">
-				<a href="/signup"> Sign up </a>
-			</li>
-			<li class:current={isLogin}>
-				<a href="/login"> Login </a>
-			</li>
-		{/if}
-		<li class="border-left"></li>
-		<ToggleTheme />
-	</ul>
+	<NavDesktop {user} {isSignedIn}></NavDesktop>
 </nav>
 
 <nav class="nav-mobile">
-	<a href="/" class="logo-link">
-		<LogoR />
-	</a>
-	<Waves />
+	<NavMobile on:openSheet={() => (showSheet = true)}></NavMobile>
 </nav>
 
 <!-- {/if} -->
@@ -121,69 +46,8 @@
 		align-items: center;
 		padding-block: var(--size-2);
 	}
-	a {
-		transition: color 0.3s ease;
-		height: 100%;
-		color: var(--text-2);
-		font-weight: var(--font-weight-5);
-		font-size: var(--font-size-0);
-		letter-spacing: var(--font-letterspacing-2);
-		text-decoration: none;
-		text-transform: uppercase;
-	}
 
 	/* CLASSES */
-	.logout-button {
-		box-shadow: none;
-		font-size: var(--font-size-0);
-		text-transform: uppercase;
-	}
-	.links {
-		& * {
-			-webkit-transition: all 0.35s ease;
-			transition: all 0.35s ease;
-			-webkit-box-sizing: border-box;
-			box-sizing: border-box;
-		}
-		& a,
-		.logout-button {
-			display: inline-block;
-			position: relative;
-			padding-block: var(--size-1);
-			color: var(--text-2);
-			font-weight: var(--font-weight-7);
-			text-decoration: none;
-		}
-		& a:before,
-		.logout-button:before {
-			position: absolute;
-			top: 25%;
-			right: 0;
-			bottom: 25%;
-			left: 0;
-			opacity: 0;
-			-webkit-transition: all 0.35s ease;
-			transition: all 0.35s ease;
-			border-top: 1px solid var(--link);
-			border-bottom: 1px solid var(--link);
-			content: '';
-		}
-	}
-	.logout-wrapper .logout-button:hover,
-	.links a:hover,
-	.links .current a,
-	.links-horz a:hover .links-horz .current a {
-		color: var(--text-1);
-	}
-
-	.logout-wrapper .logout-button:hover:before,
-	.links a:hover:before,
-	.links .current a:before {
-		top: 0;
-		bottom: 0;
-		opacity: 1;
-	}
-
 	.nav-desktop {
 		display: none;
 		/* min-height: var(--size-9); */
@@ -194,41 +58,11 @@
 		justify-content: space-between;
 	}
 
-	a.logo-link {
-		display: flex;
-		width: var(--size-8);
-	}
-
-	div {
-		display: flex;
-		align-items: center;
-	}
-
-	.border-left {
-		border-left: 1px solid var(--border);
-		height: var(--size-5);
-	}
-
-	.logout-button {
-		background: inherit;
-		padding: 0;
-	}
-
-	.signup > a {
-		color: var(--link);
-	}
-
 	@media (min-width: 768px) {
 		nav {
 			display: flex;
 			flex-direction: row;
 			justify-content: space-between;
-		}
-
-		.links {
-			display: flex;
-			align-items: center;
-			gap: var(--size-5);
 		}
 
 		.nav-desktop {
---
+++ src/routes/components/NavDesktop.svelte
@@ -0,0 +1,229 @@
+<script lang="ts">
+	import LogoR from '$lib/components/svgs/logos/LogoR2.svelte';
+	import ToggleTheme from '$lib/components/ui/ToggleTheme.svelte';
+	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
+	import { browser } from '$app/environment';
+
+	export let showingSheet: boolean = false;
+	export let isSignedIn: boolean;
+	export let user;
+	let showAuthLinks = false;
+	let visible = false;
+
+	if (browser) {
+		window.addEventListener('click', () => {
+			if (showAuthLinks === true) {
+				showAuthLinks = false;
+			}
+		});
+	}
+
+	// TODO only really want to animate this once, ever
+	onMount(() => {
+		visible = true;
+	});
+
+	$: includesAdmin = $page.url.pathname.includes('/admin');
+	$: includesPosts = $page.url.pathname.includes('/posts');
+	$: includesService = $page.url.pathname.includes('/services');
+	$: includesMusic = $page.url.pathname.includes('/music');
+
+	$: isContact = $page.url.pathname === '/contact';
+	$: isDashboard = $page.url.pathname === '/dashboard';
+	$: isHomePage = $page.url.pathname === '/';
+	$: isLogin = $page.url.pathname === '/login';
+	$: isLogout = $page.url.pathname === '/logout';
+	$: isShop = $page.url.pathname === '/shop';
+	$: isSignup = $page.url.pathname === '/signup';
+</script>
+
+<!-- {#if visible} -->
+
+<div class:showingSheet>
+	<a class="logo-link" href="/">
+		<LogoR />
+	</a>
+	<ul
+		class="links"
+		class:homepageText={isHomePage}
+		class:showingSheet
+		class:showingSheetTopSeparator={showingSheet}>
+		<li></li>
+
+		{#if user && user.isAdmin}
+			<li class:current={includesAdmin} aria-current={includesAdmin}>
+				<a href="/admin"> Admin </a>
+			</li>
+		{/if}
+
+		{#if isSignedIn && user}
+			<li class:current={isDashboard} aria-current={isDashboard}>
+				<a href="/dashboard"> Dashboard </a>
+			</li>
+		{/if}
+	</ul>
+</div>
+
+<ul class="links" class:homepageText={isHomePage} class:showingSheet>
+	{#if showingSheet}
+		<li class:borderBottom={showingSheet}></li>
+	{/if}
+	<li class:current={includesMusic} aria-current={includesMusic}>
+		<a href="/music"> Music </a>
+	</li>
+
+	<li class:current={includesService} aria-current={includesService}>
+		<a href="/services"> services </a>
+	</li>
+
+	<li class:current={isShop} aria-current={isShop}>
+		<a href="/shop"> Shop </a>
+	</li>
+
+	<li class:border-left={!showingSheet} class:borderBottom={showingSheet}></li>
+
+	<li class:current={includesPosts} aria-current={includesPosts}>
+		<a href="/blog"> Blog </a>
+	</li>
+	<li class:current={isContact} aria-current={isContact}>
+		<a href="/contact"> Contact </a>
+	</li>
+
+	<li class:border-left={!showingSheet} class:borderBottom={showingSheet}></li>
+
+	{#if isSignedIn}
+		<form class="logout-wrapper" id="logoutForm" method="POST" action="/logout">
+			<li class:current={isLogout} class:homepageText={isHomePage}>
+				<button class="logout-button" type="submit">Logout</button>
+			</li>
+		</form>
+	{:else}
+		<li class:current={isSignup} class="signup">
+			<a href="/signup"> Sign up </a>
+		</li>
+		<li class:current={isLogin}>
+			<a href="/login"> Login </a>
+		</li>
+	{/if}
+	<li class:border-left={!showingSheet} class:borderBottom={showingSheet}></li>
+	<ToggleTheme />
+</ul>
+
+<style>
+	/* ELEMENTS */
+	a {
+		transition: color 0.3s ease;
+		height: 100%;
+		color: var(--text-2);
+		font-weight: var(--font-weight-5);
+		font-size: var(--font-size-0);
+		letter-spacing: var(--font-letterspacing-2);
+		text-decoration: none;
+		text-transform: uppercase;
+	}
+	/* CLASSES */
+	.logout-button {
+		box-shadow: none;
+		font-size: var(--font-size-0);
+		text-transform: uppercase;
+	}
+	.links {
+		& * {
+			-webkit-transition: all 0.35s ease;
+			transition: all 0.35s ease;
+			-webkit-box-sizing: border-box;
+			box-sizing: border-box;
+		}
+		& a,
+		.logout-button {
+			display: inline-block;
+			position: relative;
+			padding-block: var(--size-1);
+			color: var(--text-2);
+			font-weight: var(--font-weight-7);
+			text-decoration: none;
+		}
+		& a:before,
+		.logout-button:before {
+			position: absolute;
+			top: 25%;
+			right: 0;
+			bottom: 25%;
+			left: 0;
+			opacity: 0;
+			-webkit-transition: all 0.35s ease;
+			transition: all 0.35s ease;
+			border-top: 1px solid var(--link);
+			border-bottom: 1px solid var(--link);
+			content: '';
+		}
+	}
+	.logout-wrapper .logout-button:hover,
+	.links a:hover,
+	.links .current a,
+	.links-horz a:hover .links-horz .current a {
+		color: var(--text-1);
+	}
+
+	.logout-wrapper .logout-button:hover:before,
+	.links a:hover:before,
+	.links .current a:before {
+		top: 0;
+		bottom: 0;
+		opacity: 1;
+	}
+
+	a.logo-link {
+		display: flex;
+		width: var(--size-8);
+	}
+
+	div {
+		display: flex;
+		align-items: center;
+	}
+
+	.border-left {
+		border-left: 1px solid var(--border);
+		height: var(--size-5);
+	}
+
+	.logout-button {
+		background: inherit;
+		padding: 0;
+	}
+
+	.signup > a {
+		color: var(--link);
+	}
+
+	@media (min-width: 768px) {
+		.links {
+			display: flex;
+			align-items: center;
+			gap: var(--size-5);
+		}
+	}
+
+	/* MOBILE SHEET CLASSES */
+	.showingSheet {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: var(--size-5);
+		& a,
+		button {
+			font-size: var(--font-size-2);
+		}
+	}
+	.showingSheetTopSeparator {
+		margin-bottom: var(--size-5);
+	}
+	.borderBottom {
+		border-bottom: 1px solid var(--border);
+		width: 35%;
+		/* height: var(--size-5); */
+	}
+</style>
