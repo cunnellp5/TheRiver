ac1cb2047b4a336f167156286d36a96124db9987
cunnellp5 <philip.cunnell@colorado.edu>
2024-09-04 06:53:35 -0600

    allow media to load

--- svelte.config.js
+++ svelte.config.js
@@ -14,11 +14,11 @@ const config = {
 			directives: {
 				'default-src': ['self'], // Fallback for other directives
 				'script-src': ['self'], // Allow scripts from self and inline scripts with nonce
-				'style-src': ['self', 'https://fonts.googleapis.com', 'unsafe-inline'], // Allow styles from self and Google Fonts
-				'img-src': ['self', 'https://res.cloudinary.com'], // Allow images from self and Cloudinary
+				'style-src': ['self', 'https://fonts.googleapis.com'], // Allow styles from self and Google Fonts
+				'img-src': ['self', 'https://res.cloudinary.com', 'https://i.ytimg.com/'], // Allow images from self and Cloudinary
 				'font-src': ['self', 'https://fonts.gstatic.com'], // Allow fonts from self and Google Fonts
 				'connect-src': ['self'], // Allow connections to self
-				'frame-src': ['none'], // Disallow iframes
+				'frame-src': ['self', 'https://w.soundcloud.com'], // Disallow iframes
 				'media-src': ['self', 'https://res.cloudinary.com'], // Allow media from self and Cloudinary
 				'object-src': ['none'], // Disallow plugins
 				'base-uri': ['self'], // Restrict base URI to self
