d35a665ce48102d4eb6a01ec005fb0352d4415a7
cunnellp5 <philip.cunnell@colorado.edu>
2024-09-04 10:54:38 -0600

    update unsub ui page

--- src/routes/(auth)/unsubscribe/+page.svelte
+++ src/routes/(auth)/unsubscribe/+page.svelte
@@ -1,47 +1,50 @@
-<!-- TODO USE THIS SOME HOW -->
+<section class="app-layout">
+	<section class="breaker">
+		<h1>Un&#8203;sub&#8203;scribe</h1>
+		<p>Enter your email to unsubscribe from The River's newsletter</p>
+	</section>
 
-<section>
-	<h1>Unsubscribe</h1>
-
-	<form method="POST">
-		<div>
-			<label for="email">Email:</label>
-			<input type="email" name="email" id="email" placeholder="your@email.com" required />
+	<form method="POST" class="column">
+		<div class="column">
+			<label for="email">Email</label>
+			<input type="email" name="email" id="email" placeholder="Enter email" required />
 		</div>
-		<button type="submit">Opt out</button>
+		<button class="delete-button" type="submit">Un&#8203;sub&#8203;scribe</button>
 	</form>
 </section>
 
-<!-- 
-  todo 
-  - add toast notification 
-  - add one-time click if successful
-  - add error handling
-  - confirm opt out works
-  - just check if user is logged in, in order to opt out, also verify if the logged in user is the same as the email being sent, then let this work
--->
-
 <style>
 	h1 {
+		margin-block-end: var(--size-2);
+		font-size: var(--font-size-7);
 		text-transform: uppercase;
 	}
+	p {
+		color: var(--text-2);
+	}
 	form {
-		display: flex;
-		flex-direction: column;
+		gap: var(--size-4);
+		/* min-width: var(--size-content-1); */
 	}
-	input {
-		margin: var(--size-2);
-		padding: var(--size-2);
-	}
-	button {
-		margin: var(--size-2);
-		padding: var(--size-2);
+	label {
+		margin-bottom: var(--size-1);
+		color: var(--text-2);
+		font-size: var(--font-size-0);
 	}
 	section {
+		margin-block: var(--size-9);
+		max-width: var(--size-content-2);
+	}
+	.column {
 		display: flex;
 		flex-direction: column;
-		/* justify-content: left; */
-		align-items: center;
-		margin-block: var(--size-9);
+	}
+	@media (max-width: 768px) {
+		h1 {
+			font-size: var(--font-size-5);
+		}
+		p {
+			font-size: var(--font-size-1);
+		}
 	}
 </style>
