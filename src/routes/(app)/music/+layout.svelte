98ef804b684010fcf2002191cc5d9cfb2d20c930
cunnellp5 <philip.cunnell@colorado.edu>
2024-09-19 06:49:37 -0600

    update music section with less obnoxious sub nav - still wip

--- src/routes/(app)/music/+layout.svelte
+++ src/routes/(app)/music/+layout.svelte
@@ -8,13 +8,7 @@
 	const { about } = data;
 </script>
 
-<div class="sidenav">
-	<a href="/music" class:current={$page.url.pathname === '/music'}>Tracks</a>
-	<a href="/music/video" class:current={$page.url.pathname === '/music/video'}>Videos</a>
-	<a href="/music/stems" class:current={$page.url.pathname === '/music/stems'}>Stems</a>
-</div>
-
-<div class="grid-container gutter">
+<main class="grid-container gutter">
 	<section class="title">
 		<h1>The River Sings</h1>
 		{#if about.isShowing}
@@ -22,11 +16,17 @@
 				{about.text}
 			</p>
 		{/if}
+		<nav class="sidenav">
+			<a href="/music" class:current={$page.url.pathname === '/music'}>Tracks</a>
+			<a href="/music/video" class:current={$page.url.pathname === '/music/video'}>Videos</a>
+			<a href="/music/stems" class:current={$page.url.pathname === '/music/stems'}>Stems</a>
+		</nav>
 	</section>
+
 	<section>
 		<slot></slot>
 	</section>
-</div>
+</main>
 
 <style>
 	/* ELEMENTS */
@@ -34,12 +34,6 @@
 		color: var(--text-2);
 	}
 	h1 {
-		/* background: unset;
-		-webkit-background-clip: unset;
-		-webkit-text-fill-color: unset;
-		background-clip: unset; */
-
-		/* color: var(--link); */
 		font-style: normal;
 		font-variant: small-caps;
 		font-weight: var(--font-weight-4);
@@ -47,10 +41,6 @@
 		line-height: 1;
 		font-family: 'Atyp BL Display Medium';
 		letter-spacing: 0em;
-		/* text-transform: uppercase; */
-	}
-	div {
-		margin-block: var(--size-7);
 	}
 	/* CLASSES */
 	.title {
@@ -76,38 +66,10 @@
 		text-decoration: none;
 		text-transform: uppercase;
 	}
-	.sidenav {
-		display: flex;
-		position: fixed;
-		top: 1px;
-		/* top: var(--size-1); */
-		flex-direction: column;
-		z-index: var(--layer-important);
-		margin: unset;
-		width: fit-content;
-	}
-	a:hover {
-		transform: scale(1.1);
-	}
-	.sidenav,
 	.current {
 		color: var(--link);
 	}
 	.gutter {
 		padding-inline: var(--size-7);
-	}
-	@media (max-width: 768px) {
-		.sidenav {
-			/* margin: 0 auto; */
-			top: 1%;
-			left: 50%;
-			flex-direction: row;
-			justify-content: center;
-			transform: translate(-50%);
-			font-size: var(--font-size-0);
-		}
-		.sidenav a {
-			padding: var(--size-3);
-		}
 	}
 </style>
