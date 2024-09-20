3cf30dcb0c5518efcf3c190a69dd7ca65bf41e22
cunnellp5 <philip.cunnell@colorado.edu>
2024-09-20 15:42:28 -0600

    change service buttons a little

--- src/lib/components/ui/button/ServiceButtons.svelte
+++ src/lib/components/ui/button/ServiceButtons.svelte
@@ -1,5 +1,8 @@
 <a class="hairLink" href="https://lazwicky.glossgenius.com/services" target="_blank" rel="noopener">
-	<button class="hair">Book Hair [Alexis]</button>
+	<button class="hair">
+		Book Hair &bull;
+		<span>Alexis</span>
+	</button>
 </a>
 <a class="nailsLink" href="https://the-river-sings.square.site/" target="_blank" rel="noopener">
 	<button class="nails">Book Nails</button>
@@ -13,7 +16,8 @@
 			0 0 0 var(--_highlight-size) var(--_highlight);
 		background-color: unset;
 		min-width: var(--size-content-1);
-		color: var(--gray-2);
+		color: var(--gray-3);
+		font-weight: var(--font-weight-6);
 	}
 	button.hair {
 		background-color: var(--jungle-2);
@@ -21,5 +25,8 @@
 	}
 	button.nails {
 		background-color: var(--brown-8);
+	}
+	span {
+		color: var(--brown-8);
 	}
 </style>
