4d597bc1bc5062638ce57b4e488ad07225898b96
cunnellp5 <philip.cunnell@colorado.edu>
2024-09-23 07:22:27 -0600

    add handler to cell

--- src/lib/components/ui/shadcn/table/table-cell.svelte
+++ src/lib/components/ui/shadcn/table/table-cell.svelte
@@ -7,7 +7,7 @@
 	export { className as class };
 </script>
 
-<td class={`${className}`} {...$$restProps} on:click on:keydown>
+<td on:dblclick class={`${className}`} {...$$restProps} on:click on:keydown>
 	<slot />
 </td>
 
