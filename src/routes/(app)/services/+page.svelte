8288546eb02951ca2e5e564de6005eb0e78cf198
cunnellp5 <philip.cunnell@colorado.edu>
2024-09-23 15:21:05 -0600

    use the components, clean up root, add notes for if i want the commented code

--- src/routes/(app)/services/+page.svelte
+++ src/routes/(app)/services/+page.svelte
@@ -1,41 +1,13 @@
 <script lang="ts">
 	import Seo from '$lib/components/SEO.svelte';
 	import ServiceButtons from '$lib/components/ui/button/ServiceButtons.svelte';
-	import * as Table from '$lib/components/ui/shadcn/table';
-	// import { enhance } from '$app/forms';
+	import CutoutImg from './components/CutoutImg.svelte';
+	import ScrollDots from './components/ScrollDots.svelte';
+	import ServiceTable from './components/ServiceTable.svelte';
 
 	export let data;
 	const { services, about } = data;
-	// const selectedServices = new Set();
-	// let selectedServicesInput: string;
 	let scroll: number;
-	let activeIndex: number | null = null;
-
-	// function setSelected(event) {
-	// 	if (selectedServices.has(event.detail.id)) {
-	// 		selectedServices.delete(event.detail.id);
-	// 		selectedServices = selectedServices;
-	// 		return;
-	// 	}
-	// 	selectedServices = selectedServices.add(event.detail.id);
-	// 	selectedServicesInput = [...selectedServices].join(',');
-	// }
-
-	// TODO table mapper function?
-	// const remappedServices = services.reduce((acc, service) => {
-	// 	if (!acc[service.category]) {
-	// 		acc[service.category] = [];
-	// 	}
-	// 	acc[service.category].push({
-	// 		id: service.id,
-	// 		Service: service.name,
-	// 		Description: service.description,
-	// 		Duration: `${service.duration} min`,
-	// 		Availability: service.availability,
-	// 		Price: `$${service.price}`
-	// 	});
-	// 	return acc;
-	// }, {});
 </script>
 
 <Seo
@@ -45,20 +17,11 @@
 <svelte:window bind:scrollY={scroll} />
 
 <div class="cutoutImg" style:transform={`translate3d(0, ${scroll / 5}px, 0)`}>
-	<svg width="0" height="0">
-		<defs>
-			<clipPath id="waveClip" clipPathUnits="objectBoundingBox">
-				<path d="M0,0 H1 V0.85 C0.75,1 0.25,0.85 0,1 Z" />
-			</clipPath>
-		</defs>
-	</svg>
-	<img
-		src="https://res.cloudinary.com/dswpu3qez/image/upload/f_auto/q_auto/v1714083850/TheRiver/alexisCutout.png"
-		alt="alexis cutout" />
+	<CutoutImg />
 </div>
 
-<main class="app-layout">
-	<section class="top">
+<main>
+	<section class="top" style:background-position={`center ${scroll / 55}%`}>
 		<h1 class="headingReset">The River Beauty</h1>
 		{#if about?.isShowing}
 			<p class="about">
@@ -66,107 +29,23 @@
 			</p>
 		{/if}
 	</section>
-	<!-- {#each Object.entries(remappedServices) as [category, listOfServices]}
-		<section class="service-table">
-			<div>
-				<h2>{category}</h2>
-				<Table data={listOfServices} {selectedServices} on:selectService={setSelected} />
-			</div>
-		</section>
-	{/each} -->
 </main>
 <main class="app-layout">
 	{#each Object.entries(services) as [category, data]}
-		<span class="stupid" id={category}></span>
-		<section class="tables">
-			<Table.Root>
-				<Table.Caption>{data[0].category.description}</Table.Caption>
-				<Table.Header>
-					<Table.Row>
-						<div class="table-row-header">
-							<h6>
-								{category.toUpperCase()}
-							</h6>
-						</div>
-					</Table.Row>
-				</Table.Header>
-				<Table.Body>
-					{#each data as service}
-						<Table.Row>
-							<Table.Cell>
-								<p class="serviceName">
-									{service.name}
-								</p>
-							</Table.Cell>
-							<Table.Cell>
-								<div class="price-duration">
-									<span class="price">
-										${service.price}.00
-									</span>
-									<span class="time">
-										{service.duration} min
-									</span>
-								</div>
-							</Table.Cell>
-						</Table.Row>
-					{/each}
-				</Table.Body>
-			</Table.Root>
-			<!-- <hr /> -->
-		</section>
+		<ServiceTable {category} {data} />
 	{/each}
-
-	<!-- {#if selectedServices.size > 0}
-		<aside class="surface-3 aside-left">
-			<h4>Selected service{selectedServices.size > 1 ? 's' : ''}</h4>
-			<ul>
-				{#each Array.from(selectedServices) as serviceId}
-					<li>{services.find((service) => service.id === serviceId).name}</li>
-				{/each}
-			</ul>
-		</aside>
-	{/if} -->
 	<aside class="aside-right">
-		<!-- <h4 class="categoryTitle">Categories</h4> -->
-		<ul class="dots">
-			{#each Object.entries(services) as [category], index}
-				<!-- svelte-ignore a11y-click-events-have-key-events -->
-				<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-				<li
-					class:active={index === activeIndex}
-					on:click={() => (activeIndex = index)}
-					title={category}>
-					<a href={`#${category}`}>
-						{category}
-					</a>
-				</li>
-			{/each}
-		</ul>
+		<ScrollDots {services} />
 	</aside>
 	<div class="fakeform">
 		<div class="serviceButtons">
 			<ServiceButtons />
 		</div>
-		<!-- <a href="https://lazwicky.glossgenius.com/services" target="_blank" rel="noopener"> -->
-		<!-- <button class="primary"> Book Now </button> -->
-		<!-- </a> -->
 	</div>
-	<!-- <form method="POST" use:enhance>
-		<input
-			name="selectedServices"
-			id="selectedServices"
-			type="hidden"
-			bind:value={selectedServicesInput} />
-		<button
-			class:disabled={selectedServices.size === 0}
-			disabled={selectedServices.size === 0}
-			class="primary">
-			{selectedServices.size === 0 ? 'Pick a service' : 'BOOK NOW'} ({selectedServices.size})
-		</button>
-	</form> -->
 </main>
 
 <style>
+	/* ELEMENTS */
 	main {
 		display: flex;
 		grid-column: 2;
@@ -175,94 +54,44 @@
 		line-height: var(--font-lineheight-4);
 		letter-spacing: var(--font-letterspacing-3);
 	}
-	/* button {
-		align-self: end;
-		margin: 0 auto;
-		width: 100%;
-		font-weight: var(--font-weight-8);
-	} */
-
 	h1 {
-		color: var(--text-2);
+		color: var(--gray-7);
 		font-style: normal;
 		font-variant: small-caps;
-		/* font-weight: var(--font-weight-6); */
 		font-size: var(--font-size-8);
 		line-height: 1;
 		font-family: 'Atyp BL Display Medium';
 		letter-spacing: 0em;
 	}
-
 	/* CLASSES */
-	.serviceName {
-		font-size: var(--font-size-2);
+	.aside-right {
+		position: sticky;
+		bottom: 70%;
+		left: 100%;
+		width: max-content;
+	}
+	.about {
+		color: var(--gray-7);
+		font-family: 'Ambit Light';
+		text-align: center;
 	}
 	.serviceButtons {
 		display: flex;
 		flex-direction: row;
 		gap: var(--size-3);
 	}
-	/* .page-indicator {
-		display: flex;
-		position: fixed;
-		bottom: 0;
-		justify-content: center;
-		align-items: center;
-		margin-bottom: var(--size-4);
-		border-right: 1px solid var(--link);
-		padding-right: var(--size-2);
-		height: var(--size-7);
-		color: var(--link);
-	} */
-	/* .aside-left {
-		position: sticky;
-		bottom: var(--size-7);
-		box-shadow: var(--shadow-3);
-		border-radius: var(--radius-2);
-		padding: var(--size-4);
-		width: max-content;
-	} */
-	/* Your existing styles */
-	.aside-right {
-		position: sticky;
-		bottom: 70%;
-		left: 100%;
-		/* transform: rotate(90deg); */
-		/* box-shadow: var(--shadow-3); */
-		/* border-radius: var(--radius-2); */
-		/* padding: var(--size-3); */
-		width: max-content;
-	}
-	.about {
-		/* margin: 0 auto; */
-		/* margin-block-end: var(--size-12); */
-		color: var(--text-2);
-		font-family: 'Ambit Light';
-		/* font-family: sans-serif; */
-		text-align: center;
-	}
 	.fakeform {
 		position: sticky;
 		bottom: var(--size-7);
 		align-self: center;
-		/* width: var(--size-content-1); */
-	}
-	.tables {
-		align-items: center;
-		align-self: center;
-		width: var(--size-content-3);
-		& tr {
-			font-size: var(--font-size-1);
-		}
+		z-index: var(--layer-4);
 	}
 	.cutoutImg {
 		position: absolute;
-		top: var(--size-9);
+		top: var(--size-content-2);
 		left: 0;
-		z-index: -1;
-		/* clip-path: polygon(0 0, 100% 0, 100% 75%, 0% 100%); */
+		z-index: var(--layer-2);
 		clip-path: url(#waveClip);
-
 		transition: opacity 2.5s;
 	}
 	.top {
@@ -271,164 +100,30 @@
 		justify-content: center;
 		align-items: center;
 		gap: var(--size-4);
+		z-index: var(--layer-1);
+		margin-block-end: var(--size-7);
+		background-image: url('https://res.cloudinary.com/dswpu3qez/image/upload/c_fill/backgrounds/service-background-river_ev06lj.webp');
+		background-size: cover;
 		height: var(--size-content-2);
 	}
-	.price-duration {
-		display: flex;
-		flex-direction: column;
+	::selection {
+		background-color: var(--highlight-background-color, #b3d4fc); /* Default highlight color */
+		color: var(--highlight-text-color, #000); /* Default text color */
 	}
-	.time {
-		color: var(--text-2);
-		font-size: var(--font-size-0);
-	}
-	.price {
-		font-size: var(--font-size-1);
-	}
-	.table-row-header {
-		display: flex;
-		flex-direction: row;
-		align-items: center;
-		gap: var(--size-3);
-	}
-	/* KEEPING BELOW FOR REFERENCE */
-	/* .service-table {
-		display: flex;
-		flex-direction: column;
-		justify-content: start;
-		align-items: center;
-		min-height: 30vh;
-	}
-	.service-table h2 {
-		align-self: flex-start;
-	} */
-	.stupid {
-		/* border: 1px solid red; */
-		/* display: none; */
-		height: var(--size-10);
-	}
-
-	/* DOT STYLES */
-	.dots {
-		display: flex;
-		/* position: relative; */
-		flex-direction: column;
-		gap: var(--size-5);
-		/* cursor: default; */
-		/* margin: 0; */
-		/* padding: 0; */
-		/* list-style: none; */
-
-		& .active {
-			& a {
-				-webkit-transform: scale3d(1.3, 1.3, 1.3);
-				transform: scale3d(1.3, 1.3, 1.3);
-
-				&:after {
-					height: 100%;
-				}
-			}
-		}
-
-		& li {
-			display: block;
-			position: relative;
-			float: left;
-			cursor: pointer;
-			margin-inline: var(--size-3);
-			width: 20px;
-			height: 20px;
-
-			& a {
-				position: absolute;
-				top: 0;
-				left: 0;
-				-webkit-transform: scale3d(1, 1, 1);
-				transform: scale3d(1, 1, 1);
-				-webkit-transition: all 0.3s ease;
-				transition: all 0.3s ease;
-				cursor: pointer;
-				outline: none;
-				box-shadow: inset 0 0 0 2px var(--link);
-				border-radius: 50%;
-				background-color: transparent;
-				width: 100%;
-				height: 100%;
-				overflow: hidden;
-				text-indent: -999em;
-
-				&:after {
-					position: absolute;
-					bottom: 0;
-					left: 0;
-					-webkit-transition: height 0.3s ease;
-					transition: height 0.3s ease;
-					box-shadow: 0 0 1px var(--link);
-					background-color: var(--link);
-					width: 100%;
-					height: 0;
-					content: '';
-				}
-			}
-		}
-	}
-
+	/* MEDIA */
 	@media (max-width: 768px) {
-		/* .page-indicator {
-			display: none;
-		} */
 		.top {
 			height: 30vh;
 		}
-		.tables {
-			width: 100%;
+		.serviceButtons {
+			flex-direction: column;
+			align-items: center;
 		}
 		.fakeform {
 			width: 100%;
 		}
-		/* .service-table {
-			min-height: 20vh;
-		} */
-		/* .aside-right { */
-		/* bottom: var(--size-10); */
-		/* } */
 		.cutoutImg {
 			opacity: 0.1;
 		}
-
-		.dots {
-			position: absolute;
-			right: -40px;
-		}
-
-		.serviceName {
-			font-size: var(--font-size-0);
-			text-wrap: break-word;
-			word-break: break-word; /* Ensures words break at the edge of the container */
-			overflow-wrap: break-word; /* Allows breaking within words to prevent overflow */
-		}
-	}
-
-	/* FOR THE DOTS */
-	[title] {
-		position: relative;
-	}
-	[title]:after {
-		position: absolute;
-		right: 150%;
-		bottom: 0%;
-		opacity: 0;
-		-webkit-transition: opacity 0.1s ease-in-out;
-		transition: opacity 0.1s ease-in-out;
-		border-radius: var(--radius-round);
-		background-color: var(--surface-3);
-		padding-inline: var(--size-2);
-		width: max-content;
-		content: attr(title);
-		color: var(--text-2);
-		font-size: var(--font-size-0);
-		text-transform: uppercase;
-	}
-	[title]:hover:after {
-		opacity: 1;
 	}
 </style>
