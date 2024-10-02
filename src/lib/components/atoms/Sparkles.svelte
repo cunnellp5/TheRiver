56b1e6de2cb56f97acdcaa0d4eb7e4de87b89410
cunnellp5 <philip.cunnell@colorado.edu>
2024-10-01 18:35:27 -0600

    add sparkle component

---
+++ src/lib/components/atoms/Sparkles.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+	import Sparkle from '$lib/components/atoms/SingleSparkle.svelte';
+	import type { SparkleType } from '$lib/types';
+	import { onDestroy, onMount } from 'svelte';
+
+	const random = (min: number, max: number) => Math.floor(Math.random() * (max - min)) + min;
+
+	export let color: 'default' | 'primary' | 'secondary' = 'default';
+
+	const generateSparkle = (): SparkleType => {
+		return {
+			id: String(random(10000, 99999)),
+			createdAt: Date.now(),
+			color:
+				color === 'primary'
+					? 'var(--cyan-3)'
+					: color === 'secondary'
+						? 'var(--indigo-7)'
+						: 'var(--yellow-4)',
+			size: random(10, 20),
+			style: {
+				// Pick a random spot in the available space
+				top: random(-10, 80) + '%',
+				left: random(0, 100) + '%'
+			}
+		};
+	};
+
+	let sparkles: SparkleType[] = [];
+	let sparklesInterval: ReturnType<typeof setInterval>;
+
+	onMount(() => {
+		sparklesInterval = setInterval(() => {
+			const now = Date.now();
+
+			// Create a new sparkle
+			const sparkle = generateSparkle();
+
+			// Clean up any "expired" sparkles
+			const nextSparkles = sparkles.filter((sparkle) => {
+				const delta = now - sparkle.createdAt;
+				return delta < 1500;
+			});
+
+			// Include new sparkle
+			nextSparkles.push(sparkle);
+			sparkles = nextSparkles;
+		}, 400);
+	});
+
+	onDestroy(() => {
+		clearInterval(sparklesInterval);
+	});
+</script>
+
+<div class="sparkle-wrapper">
+	{#each sparkles as sparkle (sparkle.id)}
+		<Sparkle color={sparkle.color} size={`${sparkle.size}`} style={sparkle.style} />
+	{/each}
+	<span class="slot-wrapper">
+		<slot />
+	</span>
+</div>
+
+<style>
+	.sparkle-wrapper {
+		display: inline-block;
+		position: relative;
+
+		& > .slot-wrapper {
+			position: relative;
+			z-index: 1;
+		}
+	}
+</style>
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,14 @@
+export type NoUndefinedField<T> = { [P in keyof T]-?: NoUndefinedField<NonNullable<T[P]>> };
+
 export type Info = App.Info;
 export type View = App.View;
 export type Resource = App.Resource;
 export type Event = App.Event;
+
+export type SparkleType = {
+	id: string;
+	createdAt: number;
+	color: string;
+	size: number;
+	style: any;
+};
