a295f5481d4adbad7a17cc3184484c356243b771
cunnellp5 <philip.cunnell@colorado.edu>
2024-10-10 12:26:59 -0600

    featured tracks data

---
+++ src/routes/(app)/music/TRACKS_FEATURED.ts
@@ -0,0 +1,26 @@
+export default [
+	{
+		iframeUrl:
+			'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1929051230&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true',
+		soundCloudUrl: 'https://soundcloud.com/theriversings/best-for-me-out-the-mud-volume-1-track-1',
+		title: '&quot;Best for Me&quot; Out the Mud Volume 1 Track 1',
+		width: '100%',
+		height: '400'
+	},
+	{
+		iframeUrl:
+			'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1929045803&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false',
+		soundCloudUrl: 'https://soundcloud.com/theriversings/fall-release-date-100724',
+		title: '&quot;Fall&quot; Out the Mud Volume 1 Track 2',
+		width: '100%',
+		height: '166'
+	},
+	{
+		iframeUrl:
+			'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1826468196&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false',
+		soundCloudUrl: 'https://soundcloud.com/theriversings/reasons',
+		title: '&quot;Reasons&quot; Out the Mud Volume 1 Track 3',
+		width: '100%',
+		height: '166'
+	}
+];
