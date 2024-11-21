5bb244137997ef062846d46f71544d89434e2322
cunnellp5 <philip.cunnell@colorado.edu>
2024-11-20 19:35:18 -0700

    glow stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,7 +18,20 @@
 	// const isPlaying = true;
 	let visible = false;
 
+	// const syncPointer = ({ x, y }: { x: number; y: number }) => {
+	// 	document.documentElement.style.setProperty('--x', x.toFixed(2));
+	// 	document.documentElement.style.setProperty('--xp', (x / window.innerWidth).toFixed(2));
+	// 	document.documentElement.style.setProperty('--y', y.toFixed(2));
+	// 	document.documentElement.style.setProperty('--yp', (y / window.innerHeight).toFixed(2));
+	// };
+
 	onMount(() => {
+		// document.body.addEventListener('pointermove', syncPointer);
+
+		// Cleanup event listener on component destroy
+		// onDestroy(() => {
+		// 	document.body.removeEventListener('pointermove', syncPointer);
+		// });
 		visible = true;
 	});
 </script>
@@ -60,6 +73,14 @@
 	<a href="/music" class="marquee">
 		<Marquee display={'NEW RELEASE - Out The Mud -'} />
 	</a>
+	<!-- <main>
+		<article class="glower" data-glow>
+			<div data-glow></div>
+		</article>
+		<article class="glower" data-glow>
+			<div data-glow></div>
+		</article>
+	</main> -->
 	<section>
 		{#each articles as article, i}
 			<GenericCard.Root class={i % 2 === 0 ? 'option1' : 'option2'}>
@@ -220,4 +241,125 @@
 			font-size: var(--size-9);
 		}
 	}
+
+	/* **** GLOW HSIZZ */
+	/* :root {
+		--backdrop: hsl(0 0% 60% / 0.12);
+		--radius: 14;
+		--border: 3;
+		--backup-border: var(--backdrop);
+		--size: 200;
+	}
+
+	.glower:first-of-type {
+		--base: 80;
+		--spread: 500;
+		--outer: 1;
+	}
+	.glower:last-of-type {
+		--outer: 1;
+		--base: 220;
+		--spread: 200;
+	}
+
+	.glower {
+		display: grid;
+		position: relative;
+		grid-template-rows: 1fr auto;
+		gap: 1rem;
+		backdrop-filter: blur(calc(var(--cardblur, 5) * 1px));
+		box-shadow: 0 1rem 2rem -1rem black;
+		border-radius: calc(var(--radius) * 1px);
+		padding: 1rem;
+		aspect-ratio: 3 / 4;
+		width: 260px;
+	}
+	[data-glow] {
+		--border-size: calc(var(--border, 2) * 1px);
+		--spotlight-size: calc(var(--size, 150) * 1px);
+		--hue: calc(var(--base) + (var(--xp, 0) * var(--spread, 0)));
+		position: relative;
+		border: var(--border-size) solid var(--backup-border);
+		background-image: radial-gradient(
+			var(--spotlight-size) var(--spotlight-size) at calc(var(--x, 0) * 1px) calc(var(--y, 0) * 1px),
+			hsl(
+				var(--hue, 210) calc(var(--saturation, 100) * 1%) calc(var(--lightness, 70) * 1%) /
+					var(--bg-spot-opacity, 0.1)
+			),
+			transparent
+		);
+		background-position: 50% 50%;
+		background-size: calc(100% + (2 * var(--border-size))) calc(100% + (2 * var(--border-size)));
+		background-attachment: fixed;
+		background-color: var(--backdrop, transparent);
+		touch-action: none;
+	}
+
+	[data-glow]::before,
+	[data-glow]::after {
+		position: absolute;
+		mask: linear-gradient(transparent, transparent), linear-gradient(white, white);
+		mask-clip: padding-box, border-box;
+		mask-composite: intersect;
+		inset: calc(var(--border-size) * -1);
+		border: var(--border-size) solid transparent;
+		border-radius: calc(var(--radius) * 1px);
+		background-position: 50% 50%;
+		background-size: calc(100% + (2 * var(--border-size))) calc(100% + (2 * var(--border-size)));
+		background-repeat: no-repeat;
+		background-attachment: fixed;
+		pointer-events: none;
+		content: '';
+	}
+	[data-glow]::before {
+		filter: brightness(2);
+		background-image: radial-gradient(
+			calc(var(--spotlight-size) * 0.75) calc(var(--spotlight-size) * 0.75) at
+				calc(var(--x, 0) * 1px) calc(var(--y, 0) * 1px),
+			hsl(
+				var(--hue, 210) calc(var(--saturation, 100) * 1%) calc(var(--lightness, 50) * 1%) /
+					var(--border-spot-opacity, 1)
+			),
+			transparent 100%
+		);
+	}
+	[data-glow]::after {
+		background-image: radial-gradient(
+			calc(var(--spotlight-size) * 0.5) calc(var(--spotlight-size) * 0.5) at calc(var(--x, 0) * 1px)
+				calc(var(--y, 0) * 1px),
+			hsl(0 100% 100% / var(--border-light-opacity, 1)),
+			transparent 100%
+		);
+	}
+	[data-glow] [data-glow] {
+		position: absolute;
+		opacity: var(--outer, 1);
+		will-change: filter;
+		inset: 0;
+	}
+	[data-glow] > [data-glow] {
+		filter: blur(calc(var(--border-size) * 10));
+		border-width: calc(var(--border-size) * 20);
+		border-radius: calc(var(--radius) * 1px);
+		background: none;
+		pointer-events: none;
+	}
+	[data-glow] > [data-glow]::before {
+		inset: -10px;
+		border-width: 10px;
+	}
+	[data-glow] [data-glow] {
+		border: none;
+	}
+	[data-glow] :is(a, button) {
+		border: var(--border-size) solid transparent;
+		border-radius: calc(var(--radius) * 1px);
+	}
+	[data-glow] :is(a, button) [data-glow] {
+		background: none;
+	}
+	[data-glow] :is(a, button) [data-glow]::before {
+		inset: calc(var(--border-size) * -1);
+		border-width: calc(var(--border-size) * 1);
+	} */
 </style>
