16bece7a75378627d24aea75fa6471c19d1f33ea
cunnellp5 <philip.cunnell@colorado.edu>
2024-11-20 19:19:44 -0700

    build pages on backend

--- src/routes/(app)/music/+page.server.ts
+++ src/routes/(app)/music/+page.server.ts
@@ -1,4 +1,14 @@
 // import type { PageServerLoad } from './$types';
-// import TRACKS from './TRACKS';
+import type { PageServerLoad } from './$types';
+import TRACKS from './TRACKS';
 
-// export const load: PageServerLoad = async () => ({ tracks: TRACKS });
+export const load: PageServerLoad = async () => {
+	let pages = [];
+
+	// break the array of tracks into chunks of pages of 3 elements
+	for (let i = 0; i < TRACKS.length; i += 3) {
+		pages.push(TRACKS.slice(i, i + 3));
+	}
+
+	return { pages };
+};
