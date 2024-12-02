187b35adb04187d9d01840516adaf2a04b61edd5
cunnellp5 <philip.cunnell@colorado.edu>
2024-12-02 16:33:29 -0700

    miss

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,5 +3,4 @@ export const description = 'Portfolio site';
 export const url = 'https://theriversings.com';
 
 export const emailWithBreaks = 'the&#8203;river&#8203;runs&#8203;fast&#8203;@gmail&#8203;.com';
-// export const phoneNumber = '(720) 282-1202';
 export const address = 'Denver, CO';
