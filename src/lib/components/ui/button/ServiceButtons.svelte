45424f460f2bbb9bf24b2a29a9e5ee7521289f0b
cunnellp5 <philip.cunnell@colorado.edu>
2024-12-02 16:33:49 -0700

    use a cool button instead

--- src/lib/components/ui/button/ServiceButtons.svelte
+++ src/lib/components/ui/button/ServiceButtons.svelte
@@ -9,22 +9,40 @@
 </a>
 
 <style>
+	/* https://ryanmulligan.dev/blog/css-property-new-style/ */
 	button {
+		animation: spin 2.5s infinite linear paused;
 		box-shadow:
 			var(--shadow-2),
 			0 0px var(--surface-3),
 			0 0 0 var(--_highlight-size) var(--_highlight);
+		border: 1px solid transparent;
 		background-color: unset;
+		background:
+			linear-gradient(to bottom, #222, #111) padding-box,
+			conic-gradient(from var(--bg-angle) in oklch longer hue, oklch(0.85 0.37 0) 0 0) border-box; /* extends to outer border edges */
 		min-width: var(--size-content-1);
-		color: var(--gray-9);
+		color: var(--stone-1);
 		font-weight: var(--font-weight-6);
+		&:hover {
+			animation-play-state: running;
+		}
 	}
-	/* button.hair {
-		background-color: var(--blue-1);
-		color: var(--gray-7);
-	} */
+
 	button.nails {
 		background-color: var(--yellow-6);
+	}
+
+	@property --bg-angle {
+		inherits: false;
+		initial-value: 0deg;
+		syntax: '<angle>';
+	}
+
+	@keyframes spin {
+		to {
+			--bg-angle: 360deg;
+		}
 	}
 	/* span {
 		color: var(--cyan-12);
