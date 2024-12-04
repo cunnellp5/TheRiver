8267cfda858e47881c61731f317ad448eeba35db
cunnellp5 <philip.cunnell@colorado.edu>
2024-12-03 19:19:26 -0700

    create newsletter endpoint

---
+++ src/routes/api/emails/newsletter/+server.ts
@@ -0,0 +1,37 @@
+import { env } from '$env/dynamic/private';
+import type { Component } from 'svelte';
+import NewsLetterBasic from '$lib/emails/templates/NewsLetterBasic.svelte';
+import transporter from '$lib/utils/transporter';
+import { error, json, type RequestHandler } from '@sveltejs/kit';
+import { render } from '../render';
+
+export const POST: RequestHandler = async ({ request }): Promise<Response> => {
+	const { subject, email, token = null } = await request.json();
+
+	const options = {
+		from: env.EMAIL_USER,
+		to: email,
+		subject,
+		html: render({
+			template: NewsLetterBasic as Component,
+			props: {
+				token: token
+			}
+		})
+	};
+
+	// INTENTIONALLY dont 'await' sendMail
+	try {
+		transporter.sendMail(options, (error, info) => {
+			if (error) {
+				console.error('❌ Error:', error.message);
+			} else {
+				console.log('✅ Email sent:', info.response);
+			}
+		});
+	} catch (err: unknown | Error) {
+		error(500, (err as Error).message);
+	}
+
+	return json({ status: 200, message: 'Email sent successfully' });
+};
