61624aaa779205c74f5b64ecbad107a01d668f68
cunnellp5 <philip.cunnell@colorado.edu>
2024-12-04 15:06:31 -0700

    rename footer file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,9 @@
   import type { PageData } from "./$types";
   import { onNavigate } from "$app/navigation";
   import Toasts from "$lib/components/ui/toasts.svelte";
-  // import Footer from "./components/footer/footer.svelte";
-  // import Header from "./components/header/header.svelte";
-  // import VanishingHeader from "./components/header/vanishing-header.svelte";
+  import Footer from "./components/footer/footer.svelte";
+  import Header from "./components/header/header.svelte";
+  import VanishingHeader from "./components/header/vanishing-header.svelte";
   import "open-props/buttons";
   import "open-props/colors-hsl";
   import "open-props/masks/edges";
@@ -17,10 +17,7 @@
     children?: import("svelte").Snippet;
   }
 
-  const {
-    // data,
-    children,
-  }: Props = $props();
+  const { data, children }: Props = $props();
 
   // Use view transitions if available
   onNavigate(navigation => {
@@ -38,13 +35,13 @@
 <Toasts />
 
 <div>
-  <!-- <VanishingHeader>
+  <VanishingHeader>
     <Header
       isSignedIn={data.isSignedIn}
       user={data.user} />
-  </VanishingHeader> -->
+  </VanishingHeader>
 
   {@render children?.()}
 
-  <!-- <Footer /> -->
+  <Footer />
 </div>
