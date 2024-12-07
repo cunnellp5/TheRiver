27c0b552d641b77ab6cac8a555ab136a62cc0294
cunnellp5 <philip.cunnell@colorado.edu>
2024-12-06 19:46:39 -0700

    rm analytics due to crs

--- src/routes/+layout.ts
+++
@@ -1,5 +0,0 @@
-import { injectAnalytics } from "@vercel/analytics/sveltekit";
-import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
-
-injectSpeedInsights();
-injectAnalytics();
--- svelte.config.js
+++ svelte.config.js
@@ -17,9 +17,6 @@ const config = {
         "default-src": ["self"], // Fallback for other directives
         "script-src": [
           "self",
-          "https://va.vercel-scripts.com/v1/script.debug.js",
-          "https://va.vercel-scripts.com/v1/speed-insights/script.debug.js",
-          "https://vercel.live/_next-live/",
           "http://www.instagram.com",
           "https://www.instagram.com/the.river.beauty/embed/",
         ], // Allow scripts from self and inline scripts with nonce
@@ -33,7 +30,7 @@ const config = {
         ], // Allow images from self and Cloudinary
         "font-src": ["self", "https://fonts.gstatic.com"], // Allow fonts from self and Google Fonts
         "connect-src": ["self"], // Allow connections to self
-        "frame-src": ["self", "https://w.soundcloud.com", "https://www.instagram.com/", "https://vercel.live/"], // Disallow iframes
+        "frame-src": ["self", "https://w.soundcloud.com", "https://www.instagram.com/"], // Disallow iframes
         "media-src": ["self", "https://res.cloudinary.com"], // Allow media from self and Cloudinary
         "object-src": ["none"], // Disallow plugins
         "base-uri": ["self"], // Restrict base URI to self
