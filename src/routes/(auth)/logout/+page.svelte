8bbbc9690a1608a45a1cc30a5677ed25e875d452
cunnellp5 <philip.cunnell@colorado.edu>
2024-12-10 12:58:06 -0700

    manual dependency for load function added

---
+++ src/routes/(auth)/logout/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { invalidate } from "$app/navigation";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    invalidate("home:locals");
+  });
+</script>
+
+<h1>loggedout</h1>
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,10 @@
 import type { LayoutServerLoad } from "./$types";
 
-export const load: LayoutServerLoad = async ({ locals }: { locals: App.Locals }) => ({
-  isSignedIn: locals.session !== null,
-  user: locals.user,
-});
+export const load: LayoutServerLoad = async ({ locals, depends }) => {
+  depends("home:locals");
+
+  return {
+    isSignedIn: locals.session !== null,
+    user: locals.user,
+  };
+};
