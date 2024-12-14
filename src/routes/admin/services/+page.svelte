cf38bdd2861f9e0be49d16d2737c21825bd78df1
cunnellp5 <philip.cunnell@colorado.edu>
2024-12-13 17:28:32 -0700

    add dropdown capabilities

--- src/routes/admin/services/+page.svelte
+++ src/routes/admin/services/+page.svelte
@@ -41,13 +41,12 @@
       <Card.Title>Services</Card.Title>
       <Card.Description>Create, Edit, or delete your services</Card.Description>
     </Card.Header>
-    <Card.Content>
+    <!-- <Card.Content>
       <p>todo</p>
       <ul>
         <li>MVP is create a way to CRUD services</li>
-        <li>dont let users start scheduling, yet</li>
       </ul>
-    </Card.Content>
+    </Card.Content> -->
     <Card.Footer>
       <aside>
         <a
@@ -91,45 +90,73 @@
   </section>
 {/each} -->
 
-{#each Object.entries(services) as [category, data]}
-  <section>
-    <Table.Root>
-      <!-- <Table.Caption>{data[0].category.description}</Table.Caption> -->
-      <Table.Header>
-        <Table.Row>
-          <Table.Head class="gradientHeaders">
-            {category.toUpperCase()}
-          </Table.Head>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {#each data as service}
+<main>
+  {#each Object.entries(services) as [category, data]}
+    <section>
+      <Table.Root>
+        <!-- <Table.Caption>{data[0].category.description}</Table.Caption> -->
+        <Table.Header>
           <Table.Row>
-            <Table.Cell>{service.name}</Table.Cell>
-            <Table.Cell>
-              <div class="price-duration">
-                <span class="price">
-                  ${service?.price}.00
-                </span>
-                {#if service.duration}
-                  <span class="time">
-                    {service.duration} min
-                  </span>
-                {/if}
-              </div>
-            </Table.Cell>
+            <Table.Head class="gradientHeaders">
+              {category.toUpperCase()}
+            </Table.Head>
           </Table.Row>
-        {/each}
-      </Table.Body>
-    </Table.Root>
-  </section>
-{/each}
+        </Table.Header>
+        <Table.Body>
+          {#each data as service}
+            <Table.Row>
+              <details name="service-details">
+                <summary>
+                  {service.name}
+                  <Table.Cell>
+                    <div class="price-duration">
+                      <span class="price">
+                        ${service?.price}.00
+                      </span>
+                      {#if service.duration}
+                        <span class="time">
+                          {service.duration} min
+                        </span>
+                      {/if}
+                    </div>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <div class="table-row-header">
+                      <a href="/admin/services/edit/{service.id}">
+                        <button class="update-button">Edit</button>
+                      </a>
+                      <!-- <a href="/admin/services/delete"> -->
+                      <!-- <button class="delete-button">X</button> -->
+                      <!-- </a> -->
+                    </div>
+                  </Table.Cell>
+                </summary>
+                <p>
+                  {service.description}
+                </p>
+                <!-- <Table.Cell>{service.name}</Table.Cell> -->
+              </details>
+            </Table.Row>
+          {/each}
+        </Table.Body>
+      </Table.Root>
+    </section>
+  {/each}
+</main>
 
 <style>
   /* ELEMENTS */
+  main {
+    display: flex;
+    flex-flow: row wrap;
+    align-items: flex-start;
+    /* justify-content: space-evenly; */
+    /* max-inline-size: calc(var(--size-content-2) * 4); */
+  }
   section {
+    box-shadow: 5px var(--size-2) var(--size-5) var(--stone-10);
     /* margin-block: var(--size-12); */
-    box-shadow: var(--shadow-3);
+    /* box-shadow: var(--shadow-3); */
     padding: var(--size-4);
     max-inline-size: var(--size-content-3);
     margin: var(--size-6) auto;
@@ -137,6 +164,30 @@
   aside {
     display: flex;
     gap: var(--size-4);
+  }
+  summary {
+    display: grid;
+    grid-template-columns: 1fr 1fr auto;
+    align-items: center;
+    /* display: list-item; */
+  }
+  details {
+    background-color: var(--surface-2);
+    & p {
+      padding-inline: var(--size-4);
+      font-size: var(--font-size-0);
+      color: var(--text-2);
+    }
+  }
+  details[open] {
+    & summary {
+      background-color: var(--surface-1);
+    }
+  }
+  details:hover {
+    & summary {
+      background-color: var(--surface-4);
+    }
   }
   /* CLASSES */
   .price-duration {
