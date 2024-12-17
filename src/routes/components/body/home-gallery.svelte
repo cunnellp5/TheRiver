5d7abbdbefe40d83d1959e1d7989ce1c4543f3c1
cunnellp5 <philip.cunnell@colorado.edu>
2024-12-17 14:41:42 -0700

    rm animations and flex

--- src/routes/components/body/home-gallery.svelte
+++ src/routes/components/body/home-gallery.svelte
@@ -1,66 +1,76 @@
 <script lang="ts">
+  import SparkleRainbowButton from "$lib/components/ui/button/sparkle-rainbow-button.svelte";
+
   const images = [
+    // {
+    //   src: "https://res.cloudinary.com/dswpu3qez/image/upload/f_auto/q_auto:eco/TheRiver/AlexisWater1_ji0grg.webp",
+    //   alt: "Water model",
+    // },
     {
       src: "https://res.cloudinary.com/dswpu3qez/image/upload/f_auto/q_auto:eco/TheRiver/alexismodel1_va2axz.webp",
       alt: "fifth element model",
-    },
-    {
-      src: "https://res.cloudinary.com/dswpu3qez/image/upload/f_auto/q_auto:eco/TheRiver/AlexisWater1_ji0grg.webp",
-      alt: "Water model",
+      buttonText: "Merch",
     },
     {
       src: "https://res.cloudinary.com/dswpu3qez/image/upload/f_auto/q_auto:eco/TheRiver/alexis_blue_h6x97p.webp",
       alt: "holding comb model",
-    },
-    {
-      src: "https://res.cloudinary.com/dswpu3qez/image/upload/f_auto/q_auto:eco/TheRiver/alexisArticle2_iqx8vk.webp",
-      alt: "sitting on yellow floor",
+      buttonText: "Book Services",
     },
     {
       src: "https://res.cloudinary.com/dswpu3qez/image/upload/f_auto/q_auto:eco/TheRiver/alexisSing_mmyxg6.webp",
       alt: "singing blue background",
+      buttonText: "Music",
     },
+    // {
+    //   src: "https://res.cloudinary.com/dswpu3qez/image/upload/f_auto/q_auto:eco/TheRiver/alexisArticle2_iqx8vk.webp",
+    //   alt: "sitting on yellow floor",
+    // },
   ];
 </script>
 
-<main>
+<section>
   <article class="gallery">
-    {#each images as { src, alt }, i}
-      <img
-        loading="lazy"
-        height="604.7px"
-        width="254.6px"
-        style="--i: {i}"
-        {src}
-        {alt} />
+    {#each images as { src, alt, buttonText }, i}
+      <div class="image-container">
+        <img
+          loading="lazy"
+          height="604.7px"
+          width="254.6px"
+          style="--i: {i}"
+          {src}
+          {alt} />
+        {#if buttonText}
+          <SparkleRainbowButton
+            {buttonText}
+            link="#"
+            rainbow={true}
+            class="overlay-button" />
+        {/if}
+      </div>
     {/each}
   </article>
-</main>
+</section>
 
 <style>
-  /* ELEMENTS */
-  main {
-    display: grid;
-    place-items: center;
-    margin: 0;
-    /* min-height: 100vh; */
-    height: 100%;
-  }
   img {
     border-radius: 0;
   }
-  /* CLASSES */
+  .image-container {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
   .gallery {
-    --n: 1; /* number of rows*/
-    --m: 5; /* number of columns */
+    --n: 3; /* number of rows*/
+    --m: 3; /* number of columns */
     --g: 0px; /* control the gap */
     --f: 0.8; /* control the scale factor */
-    display: grid;
-    grid-template-columns: repeat(var(--m), auto);
+    display: flex;
+    /* grid-template-columns: repeat(var(--m), auto); */
     gap: var(--g);
     width: 100%;
-    /* height: 80vh; */
-    height: var(--size-content-3);
+    flex-basis: var(--size-content-2);
+    height: 40vh;
   }
   .gallery img {
     filter: grayscale(80%);
@@ -68,14 +78,12 @@
     transition-delay: var(--transition-delay-2);
     transition-timing-function: cubic-bezier(0.14, 0.8, 0.3, 1);
     cursor: pointer;
-    width: 0;
+    min-height: 100%;
     min-width: 100%;
     height: 0;
-    min-height: 100%;
+    width: 0;
     object-fit: cover;
-    animation: grayscaleAnimation 8s infinite;
-    /* Apply increasing delays to each image */
-    animation-delay: calc(var(--i) * 1.5s);
+    object-position: center 15%;
   }
   .gallery img:hover {
     filter: grayscale(0);
@@ -85,8 +93,9 @@
   /* QUERIES */
   @media (max-width: 768px) {
     .gallery {
+      flex-direction: column;
       grid-template-columns: auto;
-      height: 100%;
+      height: 50vh;
     }
     .gallery img {
       grid-column: auto / span 4;
@@ -95,19 +104,5 @@
     /* .gallery img:hover {
       height: calc(100vw * var(--f) * 4 / var(--m));
     } */
-  }
-
-  @keyframes grayscaleAnimation {
-    0% {
-      filter: grayscale(100%);
-    }
-    50% {
-      filter: grayscale(0%);
-      /* width: calc(100vh * var(--f) / var(--n)); */
-      /* height: calc(100vw * var(--f) / var(--m)); */
-    }
-    100% {
-      filter: grayscale(100%);
-    }
   }
 </style>
