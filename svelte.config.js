3cafe4b67735e24210032b1907f4150ef63c360c
cunnellp5 <philip.cunnell@colorado.edu>
2024-12-18 07:39:36 -0700

    added calendar packages

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,14 @@
+import { preprocessMeltUI, sequence } from "@melt-ui/pp";
 import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  preprocess: sequence([
+    vitePreprocess(),
+    preprocessMeltUI(), // add to the end!
+  ]),
 
   kit: {
     adapter: adapter({
