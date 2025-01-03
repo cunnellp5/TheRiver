e32b65e954efea33af836d5e256e6c63d7c54409
cunnellp5 <philip.cunnell@colorado.edu>
2025-01-03 10:19:06 -0700

    dummy components to use

---
+++ src/routes/(app)/services/booking/info/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Separator from "$lib/components/ui/separators/separator.svelte";
+</script>
+
+<h1>
+  information
+  <Separator orientation="horizontal"></Separator>
+</h1>
