fc6396c564a244d895c1d45c1bc28d4aec7b86fc
cunnellp5 <philip.cunnell@colorado.edu>
2025-01-03 22:29:48 -0700

    update endpoint to use selected param

--- src/routes/api/services/availability/[weekday]/+server.ts
+++ src/routes/api/services/availability/[weekday]/+server.ts
@@ -1,11 +1,19 @@
 import db from "$lib/server/database";
-import { error, json, type RequestHandler } from "@sveltejs/kit";
+import { json, type RequestHandler } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async ({ params }): Promise<Response> => {
   const { weekday } = params;
 
-  const availability = await db.serviceProviderAvailability.findUniqueOrThrow({
-    where: { dayOfTheWeek: Number(weekday) },
+  const availability = await db.serviceProviderAvailability.findUnique({
+    where: {
+      dayOfTheWeek: Number(weekday),
+      enabled: true,
+    },
+    select: {
+      startTime: true,
+      endTime: true,
+      dayOfTheWeek: true,
+    },
   });
 
   return json({ status: 200, availability });
